509110eb7d54aeb5eccffbec383f492357e30bc4
Alexander01998 <contact.wurstimperium@gmail.com>
2019-06-06 08:31:27 +0200

    Add ChatUtils

---
+++ src/main/java/net/wurstclient/utils/ChatUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2017 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.utils;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+
+public enum ChatUtils
+{
+	;
+	
+	private static final MinecraftClient MC = MinecraftClient.getInstance();
+	
+	private static final String WURST_PREFIX =
+		"\u00a7c[\u00a76Wurst\u00a7c]\u00a7r ";
+	private static final String WARNING_PREFIX =
+		"\u00a7c[\u00a76\u00a7lWARNING\u00a7c]\u00a7r ";
+	private static final String ERROR_PREFIX =
+		"\u00a7c[\u00a74\u00a7lERROR\u00a7c]\u00a7r ";
+	
+	private static boolean enabled = true;
+	
+	public static void setEnabled(boolean enabled)
+	{
+		ChatUtils.enabled = enabled;
+	}
+	
+	public static void component(Component component)
+	{
+		if(!enabled)
+			return;
+		
+		ChatHud chatHud = MC.inGameHud.getChatHud();
+		TextComponent prefix = new TextComponent(WURST_PREFIX);
+		chatHud.addMessage(prefix.append(component));
+	}
+	
+	public static void message(String message)
+	{
+		component(new TextComponent(message));
+	}
+	
+	public static void warning(String message)
+	{
+		message(WARNING_PREFIX + message);
+	}
+	
+	public static void error(String message)
+	{
+		message(ERROR_PREFIX + message);
+	}
+}
