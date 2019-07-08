1136c3bb7b20a5a5a5e089b9071e99282f3de4e3
Alexander <Alexander@Leuchtbook3000>
2019-07-08 20:25:16 +0200

    Add ChatInputEvent

---
+++ src/main/java/net/wurstclient/events/ChatInputListener.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.events;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.gui.hud.ChatHudLine;
+import net.minecraft.network.chat.Component;
+import net.wurstclient.event.CancellableEvent;
+import net.wurstclient.event.Listener;
+
+public interface ChatInputListener extends Listener
+{
+	public void onReceivedMessage(ChatInputEvent event);
+	
+	public static class ChatInputEvent
+		extends CancellableEvent<ChatInputListener>
+	{
+		private Component component;
+		private List<ChatHudLine> chatLines;
+		
+		public ChatInputEvent(Component component, List<ChatHudLine> chatLines)
+		{
+			this.component = component;
+			this.chatLines = chatLines;
+		}
+		
+		public Component getComponent()
+		{
+			return component;
+		}
+		
+		public void setComponent(Component component)
+		{
+			this.component = component;
+		}
+		
+		public List<ChatHudLine> getChatLines()
+		{
+			return chatLines;
+		}
+		
+		@Override
+		public void fire(ArrayList<ChatInputListener> listeners)
+		{
+			for(ChatInputListener listener : listeners)
+			{
+				listener.onReceivedMessage(this);
+				
+				if(isCancelled())
+					break;
+			}
+		}
+		
+		@Override
+		public Class<ChatInputListener> getListenerType()
+		{
+			return ChatInputListener.class;
+		}
+	}
+}
---
+++ src/main/java/net/wurstclient/mixin/ChatHudMixin.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import net.minecraft.network.chat.Component;
+import net.wurstclient.WurstClient;
+import net.wurstclient.events.ChatInputListener.ChatInputEvent;
+
+@Mixin(ChatHud.class)
+public class ChatHudMixin extends DrawableHelper
+{
+	@Shadow
+	private List<ChatHudLine> messages;
+	@Shadow
+	private static Logger LOGGER;
+	@Shadow
+	private MinecraftClient client;
+	
+	@Inject(at = @At("HEAD"),
+		method = "addMessage(Lnet/minecraft/network/chat/Component;I)V",
+		cancellable = true)
+	private void onAddMessage(Component chatComponent, int chatLineId,
+		CallbackInfo ci)
+	{
+		ChatInputEvent event = new ChatInputEvent(chatComponent, messages);
+		
+		WurstClient.INSTANCE.getEventManager().fire(event);
+		if(event.isCancelled())
+			ci.cancel();
+		
+		chatComponent = event.getComponent();
+		shadow$addMessage(chatComponent, chatLineId,
+			client.inGameHud.getTicks(), false);
+		
+		LOGGER.info("[CHAT] {}", chatComponent.getString()
+			.replaceAll("\r", "\\\\r").replaceAll("\n", "\\\\n"));
+		ci.cancel();
+	}
+	
+	@Shadow
+	private void shadow$addMessage(Component component_1, int int_1, int int_2,
+		boolean boolean_1)
+	{
+		
+	}
+}
