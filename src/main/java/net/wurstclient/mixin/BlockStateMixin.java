298509247e1647664481d7dde02d286c26fb85e8
Alexander01998 <contact.wurstimperium@gmail.com>
2019-07-22 00:09:22 +0200

    Add IsNormalCubeListener

---
+++ src/main/java/net/wurstclient/mixin/BlockStateMixin.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.google.common.collect.ImmutableMap;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.AbstractPropertyContainer;
+import net.minecraft.state.PropertyContainer;
+import net.minecraft.state.property.Property;
+import net.wurstclient.WurstClient;
+import net.wurstclient.events.IsNormalCubeListener.IsNormalCubeEvent;
+
+@Mixin(BlockState.class)
+public class BlockStateMixin
+	extends AbstractPropertyContainer<Block, BlockState>
+	implements PropertyContainer<BlockState>
+{
+	private BlockStateMixin(WurstClient wurst, Block object_1,
+		ImmutableMap<Property<?>, Comparable<?>> immutableMap_1)
+	{
+		super(object_1, immutableMap_1);
+	}
+	
+	@Inject(at = {@At("TAIL")},
+		method = {
+			"isSimpleFullBlock(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z"},
+		cancellable = true)
+	private void onIsSimpleFullBlock(CallbackInfoReturnable<Boolean> cir)
+	{
+		IsNormalCubeEvent event = new IsNormalCubeEvent();
+		WurstClient.INSTANCE.getEventManager().fire(event);
+		
+		cir.setReturnValue(cir.getReturnValue() && !event.isCancelled());
+	}
+}
