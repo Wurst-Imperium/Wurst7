fba9da7e133afd05194b386a0dbfe4a7382e04ae
Alexander01998 <contact.wurstimperium@gmail.com>
2019-07-22 21:46:04 +0200

    Add IKeyBinding interface

---
+++ src/main/java/net/wurstclient/mixin/KeyBindingMixin.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.wurstclient.WurstClient;
+import net.wurstclient.mixinterface.IKeyBinding;
+
+@Mixin(KeyBinding.class)
+public class KeyBindingMixin implements IKeyBinding
+{
+	@Shadow
+	private boolean pressed;
+	@Shadow
+	private InputUtil.KeyCode keyCode;
+	
+	@Override
+	public void setPressed(boolean pressed)
+	{
+		this.pressed = pressed;
+	}
+	
+	@Override
+	public boolean isActallyPressed()
+	{
+		long handle = WurstClient.MC.window.getHandle();
+		int code = keyCode.getKeyCode();
+		return InputUtil.isKeyPressed(handle, code);
+	}
+}
