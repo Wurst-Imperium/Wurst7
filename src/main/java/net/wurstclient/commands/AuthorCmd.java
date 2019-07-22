6fe0956282393d989c529da30744177b540313f1
Alexander01998 <contact.wurstimperium@gmail.com>
2019-07-23 00:37:45 +0200

    Add AuthorCmd

---
+++ src/main/java/net/wurstclient/commands/AuthorCmd.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.StringTag;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+
+public final class AuthorCmd extends Command
+{
+	public AuthorCmd()
+	{
+		super("author", "Changes the held written book's author.",
+			".author <author>");
+	}
+	
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		if(args.length == 0)
+			throw new CmdSyntaxError(this);
+		
+		if(!MC.player.abilities.creativeMode)
+			throw new CmdError(this, "Creative mode only.");
+		
+		ItemStack heldItem = MC.player.inventory.getMainHandStack();
+		int heldItemID = Item.getRawId(heldItem.getItem());
+		int writtenBookID = Item.getRawId(Items.WRITTEN_BOOK);
+		
+		if(heldItemID != writtenBookID)
+			throw new CmdError(this,
+				"You must hold a written book in your main hand.");
+		
+		String author = String.join(" ", args);
+		heldItem.putSubTag("author", new StringTag(author));
+	}
+}
