71ccbf0f5a93db42978663295abbe02c1344fbcf
Alexander01998 <contact.wurstimperium@gmail.com>
2019-07-24 15:18:27 +0200

    Add PacketInputListener

---
+++ src/main/java/net/wurstclient/mixin/ClientConnectionMixin.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.Packet;
+import net.wurstclient.WurstClient;
+import net.wurstclient.events.PacketInputListener.PacketInputEvent;
+
+@Mixin(ClientConnection.class)
+public class ClientConnectionMixin
+	extends SimpleChannelInboundHandler<Packet<?>>
+{
+	@Inject(at = {@At(value = "INVOKE",
+		target = "Lnet/minecraft/network/ClientConnection;handlePacket(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;)V",
+		ordinal = 0)},
+		method = {
+			"method_10770(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/Packet;)V"},
+		cancellable = true)
+	private void onMethod_10770(ChannelHandlerContext channelHandlerContext,
+		Packet<?> packet, CallbackInfo ci)
+	{
+		PacketInputEvent event = new PacketInputEvent(packet);
+		WurstClient.INSTANCE.getEventManager().fire(event);
+		
+		if(event.isCancelled())
+			ci.cancel();
+	}
+	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Packet<?> msg)
+		throws Exception
+	{
+		throw new RuntimeException();
+	}
+}
