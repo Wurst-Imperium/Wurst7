00d57ccdd6bf15fcbfc29d6c49fb9933d5742410
Alexander01998 <contact.wurstimperium@gmail.com>
2019-07-31 05:08:34 +0200

    Add BlockUtils

---
+++ src/main/java/net/wurstclient/util/BlockUtils.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.util;
+
+import java.util.ArrayList;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShapes;
+import net.wurstclient.WurstClient;
+
+public final class BlockUtils
+{
+	private static final MinecraftClient MC = WurstClient.MC;
+	
+	public static BlockState getState(BlockPos pos)
+	{
+		return MC.world.getBlockState(pos);
+	}
+	
+	public static Block getBlock(BlockPos pos)
+	{
+		return getState(pos).getBlock();
+	}
+	
+	public static int getId(BlockPos pos)
+	{
+		return Block.getRawIdFromState(getState(pos));
+	}
+	
+	public static String getName(BlockPos pos)
+	{
+		Block block = BlockUtils.getBlock(pos);
+		return Registry.BLOCK.getId(block).toString();
+	}
+	
+	public static float getHardness(BlockPos pos)
+	{
+		return getState(pos).calcBlockBreakingDelta(MC.player, MC.world, pos);
+	}
+	
+	public static boolean canBeClicked(BlockPos pos)
+	{
+		return getState(pos).getOutlineShape(MC.world, pos) != VoxelShapes
+			.empty();
+	}
+	
+	public static ArrayList<BlockPos> getAllInBox(BlockPos min, BlockPos max)
+	{
+		ArrayList<BlockPos> blocks = new ArrayList<>();
+		
+		for(int x = min.getX(); x <= max.getX(); x++)
+			for(int y = min.getY(); y <= max.getY(); y++)
+				for(int z = min.getZ(); z <= max.getZ(); z++)
+					blocks.add(new BlockPos(x, y, z));
+				
+		return blocks;
+	}
+}
