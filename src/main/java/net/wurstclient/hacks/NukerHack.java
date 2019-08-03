9fff459b86099205da4a9faba9e9951249d29b15
Alexander01998 <contact.wurstimperium@gmail.com>
2019-08-03 23:20:08 +0200

    Create BlockBreaker enum for block breaking methods

--- src/main/java/net/wurstclient/hacks/NukerHack.java
+++ src/main/java/net/wurstclient/hacks/NukerHack.java
@@ -23,18 +23,11 @@ import org.lwjgl.opengl.GL11;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.server.network.packet.HandSwingC2SPacket;
-import net.minecraft.server.network.packet.PlayerActionC2SPacket;
-import net.minecraft.server.network.packet.PlayerActionC2SPacket.Action;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.wurstclient.Category;
@@ -45,6 +38,7 @@ import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.EnumSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
+import net.wurstclient.util.BlockBreaker;
 import net.wurstclient.util.BlockUtils;
 import net.wurstclient.util.RenderUtils;
 import net.wurstclient.util.RotationUtils;
@@ -157,12 +151,12 @@ public final class NukerHack extends Hack
 			MC.interactionManager.cancelBlockBreaking();
 			progress = 1;
 			prevProgress = 1;
-			breakBlocksWithPacketSpam(blocks3);
+			BlockBreaker.breakBlocksWithPacketSpam(blocks3);
 			return;
 		}
 		
 		for(BlockPos pos : blocks2)
-			if(breakOneBlock(pos))
+			if(BlockBreaker.breakOneBlock(pos))
 			{
 				currentBlock = pos;
 				break;
@@ -247,99 +241,6 @@ public final class NukerHack extends Hack
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glDisable(GL11.GL_LINE_SMOOTH);
-	}
-	
-	private boolean breakOneBlock(BlockPos pos)
-	{
-		Direction side = null;
-		Direction[] sides = Direction.values();
-		
-		Vec3d eyesPos = RotationUtils.getEyesPos();
-		Vec3d relCenter = BlockUtils.getState(pos)
-			.getOutlineShape(MC.world, pos).getBoundingBox().getCenter();
-		Vec3d center = new Vec3d(pos).add(relCenter);
-		
-		Vec3d[] hitVecs = new Vec3d[sides.length];
-		for(int i = 0; i < sides.length; i++)
-		{
-			Vec3i dirVec = sides[i].getVector();
-			Vec3d relHitVec = new Vec3d(relCenter.x * dirVec.getX(),
-				relCenter.y * dirVec.getY(), relCenter.z * dirVec.getZ());
-			hitVecs[i] = center.add(relHitVec);
-		}
-		
-		BlockState state = BlockUtils.getState(pos);
-		for(int i = 0; i < sides.length; i++)
-		{
-			// check line of sight
-			if(MC.world.rayTraceBlock(eyesPos, hitVecs[i], pos,
-				state.getOutlineShape(MC.world, pos), state) != null)
-				continue;
-			
-			side = sides[i];
-			break;
-		}
-		
-		if(side == null)
-		{
-			double distanceSqToCenter = eyesPos.squaredDistanceTo(center);
-			for(int i = 0; i < sides.length; i++)
-			{
-				// check if side is facing towards player
-				if(eyesPos.squaredDistanceTo(hitVecs[i]) >= distanceSqToCenter)
-					continue;
-				
-				side = sides[i];
-				break;
-			}
-		}
-		
-		// player is inside of block, side doesn't matter
-		if(side == null)
-			side = sides[0];
-		
-		// face block
-		WURST.getRotationFaker().faceVectorPacket(hitVecs[side.ordinal()]);
-		
-		// damage block
-		if(!MC.interactionManager.method_2902(pos, side))
-			return false;
-		
-		// swing arm
-		MC.player.networkHandler
-			.sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
-		
-		return true;
-	}
-	
-	private void breakBlocksWithPacketSpam(Iterable<BlockPos> blocks)
-	{
-		Vec3d eyesPos = RotationUtils.getEyesPos();
-		ClientPlayNetworkHandler netHandler = MC.player.networkHandler;
-		
-		for(BlockPos pos : blocks)
-		{
-			Vec3d posVec = new Vec3d(pos).add(0.5, 0.5, 0.5);
-			double distanceSqPosVec = eyesPos.squaredDistanceTo(posVec);
-			
-			for(Direction side : Direction.values())
-			{
-				Vec3d hitVec =
-					posVec.add(new Vec3d(side.getVector()).multiply(0.5));
-				
-				// check if side is facing towards player
-				if(eyesPos.squaredDistanceTo(hitVec) >= distanceSqPosVec)
-					continue;
-				
-				// break block
-				netHandler.sendPacket(new PlayerActionC2SPacket(
-					Action.START_DESTROY_BLOCK, pos, side));
-				netHandler.sendPacket(new PlayerActionC2SPacket(
-					Action.STOP_DESTROY_BLOCK, pos, side));
-				
-				break;
-			}
-		}
 	}
 	
 	private enum Mode
---
+++ src/main/java/net/wurstclient/util/BlockBreaker.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.util;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.server.network.packet.HandSwingC2SPacket;
+import net.minecraft.server.network.packet.PlayerActionC2SPacket;
+import net.minecraft.server.network.packet.PlayerActionC2SPacket.Action;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+import net.wurstclient.WurstClient;
+
+public enum BlockBreaker
+{
+	;
+	
+	private static final WurstClient WURST = WurstClient.INSTANCE;
+	private static final MinecraftClient MC = WurstClient.MC;
+	
+	public static boolean breakOneBlock(BlockPos pos)
+	{
+		Direction side = null;
+		Direction[] sides = Direction.values();
+		
+		Vec3d eyesPos = RotationUtils.getEyesPos();
+		Vec3d relCenter = BlockUtils.getState(pos)
+			.getOutlineShape(MC.world, pos).getBoundingBox().getCenter();
+		Vec3d center = new Vec3d(pos).add(relCenter);
+		
+		Vec3d[] hitVecs = new Vec3d[sides.length];
+		for(int i = 0; i < sides.length; i++)
+		{
+			Vec3i dirVec = sides[i].getVector();
+			Vec3d relHitVec = new Vec3d(relCenter.x * dirVec.getX(),
+				relCenter.y * dirVec.getY(), relCenter.z * dirVec.getZ());
+			hitVecs[i] = center.add(relHitVec);
+		}
+		
+		BlockState state = BlockUtils.getState(pos);
+		for(int i = 0; i < sides.length; i++)
+		{
+			// check line of sight
+			if(MC.world.rayTraceBlock(eyesPos, hitVecs[i], pos,
+				state.getOutlineShape(MC.world, pos), state) != null)
+				continue;
+			
+			side = sides[i];
+			break;
+		}
+		
+		if(side == null)
+		{
+			double distanceSqToCenter = eyesPos.squaredDistanceTo(center);
+			for(int i = 0; i < sides.length; i++)
+			{
+				// check if side is facing towards player
+				if(eyesPos.squaredDistanceTo(hitVecs[i]) >= distanceSqToCenter)
+					continue;
+				
+				side = sides[i];
+				break;
+			}
+		}
+		
+		// player is inside of block, side doesn't matter
+		if(side == null)
+			side = sides[0];
+		
+		// face block
+		WURST.getRotationFaker().faceVectorPacket(hitVecs[side.ordinal()]);
+		
+		// damage block
+		if(!MC.interactionManager.method_2902(pos, side))
+			return false;
+		
+		// swing arm
+		MC.player.networkHandler
+			.sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
+		
+		return true;
+	}
+	
+	public static void breakBlocksWithPacketSpam(Iterable<BlockPos> blocks)
+	{
+		Vec3d eyesPos = RotationUtils.getEyesPos();
+		ClientPlayNetworkHandler netHandler = MC.player.networkHandler;
+		
+		for(BlockPos pos : blocks)
+		{
+			Vec3d posVec = new Vec3d(pos).add(0.5, 0.5, 0.5);
+			double distanceSqPosVec = eyesPos.squaredDistanceTo(posVec);
+			
+			for(Direction side : Direction.values())
+			{
+				Vec3d hitVec =
+					posVec.add(new Vec3d(side.getVector()).multiply(0.5));
+				
+				// check if side is facing towards player
+				if(eyesPos.squaredDistanceTo(hitVec) >= distanceSqPosVec)
+					continue;
+				
+				// break block
+				netHandler.sendPacket(new PlayerActionC2SPacket(
+					Action.START_DESTROY_BLOCK, pos, side));
+				netHandler.sendPacket(new PlayerActionC2SPacket(
+					Action.STOP_DESTROY_BLOCK, pos, side));
+				
+				break;
+			}
+		}
+	}
+}
