75e8c05a5d9e07da58c906116157c259cb6bc842
Alexander01998 <contact.wurstimperium@gmail.com>
2019-08-19 21:59:49 +0200

    Add RenderBlockEntityListener

---
+++ src/main/java/net/wurstclient/events/RenderBlockEntityListener.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.events;
+
+import java.util.ArrayList;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.wurstclient.event.CancellableEvent;
+import net.wurstclient.event.Listener;
+
+public interface RenderBlockEntityListener extends Listener
+{
+	public void onRenderBlockEntity(RenderBlockEntityEvent event);
+	
+	public static class RenderBlockEntityEvent
+		extends CancellableEvent<RenderBlockEntityListener>
+	{
+		private final BlockEntity blockEntity;
+		
+		public RenderBlockEntityEvent(BlockEntity blockEntity)
+		{
+			this.blockEntity = blockEntity;
+		}
+		
+		public BlockEntity getBlockEntity()
+		{
+			return blockEntity;
+		}
+		
+		@Override
+		public void fire(ArrayList<RenderBlockEntityListener> listeners)
+		{
+			for(RenderBlockEntityListener listener : listeners)
+			{
+				listener.onRenderBlockEntity(this);
+				
+				if(isCancelled())
+					break;
+			}
+		}
+		
+		@Override
+		public Class<RenderBlockEntityListener> getListenerType()
+		{
+			return RenderBlockEntityListener.class;
+		}
+	}
+}
---
+++ src/main/java/net/wurstclient/mixin/BlockEntityRenderDispatcherMixin.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.wurstclient.WurstClient;
+import net.wurstclient.events.RenderBlockEntityListener.RenderBlockEntityEvent;
+
+@Mixin(BlockEntityRenderDispatcher.class)
+public class BlockEntityRenderDispatcherMixin
+{
+	@Inject(at = {@At("HEAD")},
+		method = {"render(Lnet/minecraft/block/entity/BlockEntity;FI)V"},
+		cancellable = true)
+	private void onRender(BlockEntity blockEntity, float partialTicks,
+		int destroyStage, CallbackInfo ci)
+	{
+		RenderBlockEntityEvent event = new RenderBlockEntityEvent(blockEntity);
+		WurstClient.INSTANCE.getEventManager().fire(event);
+		
+		if(event.isCancelled())
+			ci.cancel();
+	}
+}
