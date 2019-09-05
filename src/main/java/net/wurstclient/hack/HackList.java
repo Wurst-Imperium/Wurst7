41ae1acfa24737e12326c477d5a77b29d493133a
Alexander01998 <contact.wurstimperium@gmail.com>
2019-09-05 18:54:14 +0200

    Add SneakHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -136,7 +136,7 @@ public final class HackList implements UpdateListener
 	// public final ScaffoldWalkHack scaffoldWalkHack = new ScaffoldWalkHack();
 	// public final SearchHack searchHack = new SearchHack();
 	// public final SkinDerpHack skinDerpHack = new SkinDerpHack();
-	// public final SneakHack sneakHack = new SneakHack();
+	public final SneakHack sneakHack = new SneakHack();
 	// public final SpammerHack spammerHack = new SpammerHack();
 	// public final SpeedHackHack speedHackHack = new SpeedHackHack();
 	// public final SpeedNukerHack speedNukerHack = new SpeedNukerHack();
---
+++ src/main/java/net/wurstclient/hacks/SneakHack.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2014 - 2019 | Wurst-Imperium | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.hacks;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.server.network.packet.ClientCommandC2SPacket;
+import net.minecraft.server.network.packet.ClientCommandC2SPacket.Mode;
+import net.wurstclient.Category;
+import net.wurstclient.SearchTags;
+import net.wurstclient.events.PostMotionListener;
+import net.wurstclient.events.PreMotionListener;
+import net.wurstclient.hack.Hack;
+import net.wurstclient.mixinterface.IKeyBinding;
+import net.wurstclient.settings.EnumSetting;
+
+@SearchTags({"AutoSneaking"})
+public final class SneakHack extends Hack
+	implements PreMotionListener, PostMotionListener
+{
+	private final EnumSetting<SneakMode> mode = new EnumSetting<>("Mode",
+		"§lPacket§r mode makes it look like you're\n"
+			+ "sneaking without slowing you down.\n"
+			+ "§lLegit§r mode actually makes you sneak.",
+		SneakMode.values(), SneakMode.LEGIT);
+	
+	public SneakHack()
+	{
+		super("Sneak", "Makes you sneak automatically.");
+		setCategory(Category.MOVEMENT);
+		addSetting(mode);
+	}
+	
+	@Override
+	public String getRenderName()
+	{
+		return getName() + " [" + mode.getSelected() + "]";
+	}
+	
+	@Override
+	public void onEnable()
+	{
+		WURST.getEventManager().add(PreMotionListener.class, this);
+		WURST.getEventManager().add(PostMotionListener.class, this);
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		WURST.getEventManager().remove(PreMotionListener.class, this);
+		WURST.getEventManager().remove(PostMotionListener.class, this);
+		
+		switch(mode.getSelected())
+		{
+			case LEGIT:
+			IKeyBinding sneakKey = (IKeyBinding)MC.options.keySneak;
+			sneakKey.setPressed(sneakKey.isActallyPressed());
+			break;
+			
+			case PACKET:
+			sendSneakPacket(Mode.STOP_SNEAKING);
+			break;
+		}
+	}
+	
+	@Override
+	public void onPreMotion()
+	{
+		IKeyBinding sneakKey = (IKeyBinding)MC.options.keySneak;
+		
+		switch(mode.getSelected())
+		{
+			case LEGIT:
+			sneakKey.setPressed(true);
+			break;
+			
+			case PACKET:
+			sneakKey.setPressed(sneakKey.isActallyPressed());
+			sendSneakPacket(Mode.START_SNEAKING);
+			sendSneakPacket(Mode.STOP_SNEAKING);
+			break;
+		}
+	}
+	
+	@Override
+	public void onPostMotion()
+	{
+		if(mode.getSelected() != SneakMode.PACKET)
+			return;
+		
+		sendSneakPacket(Mode.STOP_SNEAKING);
+		sendSneakPacket(Mode.START_SNEAKING);
+	}
+	
+	private void sendSneakPacket(Mode mode)
+	{
+		ClientPlayerEntity player = MC.player;
+		ClientCommandC2SPacket packet =
+			new ClientCommandC2SPacket(player, mode);
+		player.networkHandler.sendPacket(packet);
+	}
+	
+	private enum SneakMode
+	{
+		PACKET("Packet"),
+		LEGIT("Legit");
+		
+		private final String name;
+		
+		private SneakMode(String name)
+		{
+			this.name = name;
+		}
+		
+		@Override
+		public String toString()
+		{
+			return name;
+		}
+	}
+}
