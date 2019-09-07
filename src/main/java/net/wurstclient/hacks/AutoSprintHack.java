2fc26cfc3ac1bb1b02f3a575ece809edd501d58b
Alexander01998 <contact.wurstimperium@gmail.com>
2019-09-07 17:55:24 +0200

    Fix AutoSprintHack again

--- src/main/java/net/wurstclient/hacks/AutoSprintHack.java
+++ src/main/java/net/wurstclient/hacks/AutoSprintHack.java
@@ -42,7 +42,7 @@ public final class AutoSprintHack extends Hack implements UpdateListener
 		if(player.horizontalCollision || player.isSneaking())
 			return;
 		
-		if(player.isTouchingWater())
+		if(player.isTouchingWater() || player.isInWater())
 			return;
 		
 		if(player.forwardSpeed > 0)
