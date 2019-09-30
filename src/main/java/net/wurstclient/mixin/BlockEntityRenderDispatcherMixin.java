37e5eaeab85734d1ab88e0784804675a45c0396a
Alexander01998 <contact.wurstimperium@gmail.com>
2019-09-30 19:05:17 +0200

    Fix BlockEntityRenderDispatcherMixin

--- src/main/java/net/wurstclient/mixin/BlockEntityRenderDispatcherMixin.java
+++ src/main/java/net/wurstclient/mixin/BlockEntityRenderDispatcherMixin.java
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.block.BlockRenderLayer;
+import net.minecraft.class_4587;
+import net.minecraft.class_4597;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.wurstclient.WurstClient;
 import net.wurstclient.events.RenderBlockEntityListener.RenderBlockEntityEvent;
@@ -24,11 +24,11 @@ public class BlockEntityRenderDispatcherMixin
 {
 	@Inject(at = {@At("HEAD")},
 		method = {
-			"render(Lnet/minecraft/block/entity/BlockEntity;FILnet/minecraft/block/BlockRenderLayer;Lnet/minecraft/client/render/BufferBuilder;)V"},
+			"render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/class_4587;Lnet/minecraft/class_4597;DDD)V"},
 		cancellable = true)
-	private void onRender(BlockEntity blockEntity, float partialTicks,
-		int destroyStage, BlockRenderLayer blockRenderLayer_1,
-		BufferBuilder bufferBuilder_1, CallbackInfo ci)
+	private <E extends BlockEntity> void onRender(E blockEntity,
+		float partialTicks, class_4587 class_4587_1, class_4597 class_4597_1,
+		double double_1, double double_2, double double_3, CallbackInfo ci)
 	{
 		RenderBlockEntityEvent event = new RenderBlockEntityEvent(blockEntity);
 		WurstClient.INSTANCE.getEventManager().fire(event);
