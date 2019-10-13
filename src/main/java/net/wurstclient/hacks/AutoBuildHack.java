0eecc76383a0ee91c516e8b114398f26a9384c19
Alexander01998 <contact.wurstimperium@gmail.com>
2019-10-13 23:38:07 +0200

    Add default AutoBuild templates

--- src/main/java/net/wurstclient/hacks/AutoBuildHack.java
+++ src/main/java/net/wurstclient/hacks/AutoBuildHack.java
@@ -8,13 +8,17 @@
 package net.wurstclient.hacks;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import net.wurstclient.Category;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.FileSetting;
+import net.wurstclient.util.json.JsonException;
+import net.wurstclient.util.json.JsonUtils;
 
 public final class AutoBuildHack extends Hack implements UpdateListener
 {
@@ -49,13 +53,84 @@ public final class AutoBuildHack extends Hack implements UpdateListener
 	
 	private void createDefaultTemplates(Path folder)
 	{
-		try
+		for(DefaultTemplate template : DefaultTemplate.values())
 		{
-			Files.createFile(folder.resolve("test.json"));
+			JsonObject json = createJson(template);
 			
-		}catch(IOException e)
+			Path path = folder.resolve(template.name + ".json");
+			
+			try
+			{
+				JsonUtils.toJson(json, path);
+				
+			}catch(IOException | JsonException e)
+			{
+				System.out.println("Couldn't save " + path.getFileName());
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private JsonObject createJson(DefaultTemplate template)
+	{
+		JsonObject json = new JsonObject();
+		JsonElement blocks = JsonUtils.GSON.toJsonTree(template.data);
+		json.add("blocks", blocks);
+		
+		return json;
+	}
+	
+	private static enum DefaultTemplate
+	{
+		BRIDGE("Bridge",
+			new int[][]{{0, 0, 0}, {1, 0, 0}, {1, 0, -1}, {0, 0, -1},
+				{-1, 0, -1}, {-1, 0, 0}, {-1, 0, -2}, {0, 0, -2}, {1, 0, -2},
+				{1, 0, -3}, {0, 0, -3}, {-1, 0, -3}, {-1, 0, -4}, {0, 0, -4},
+				{1, 0, -4}, {1, 0, -5}, {0, 0, -5}, {-1, 0, -5}}),
+		
+		FLOOR("Floor",
+			new int[][]{{0, 0, 0}, {0, 0, 1}, {1, 0, 1}, {1, 0, 0}, {1, 0, -1},
+				{0, 0, -1}, {-1, 0, -1}, {-1, 0, 0}, {-1, 0, 1}, {-1, 0, 2},
+				{0, 0, 2}, {1, 0, 2}, {2, 0, 2}, {2, 0, 1}, {2, 0, 0},
+				{2, 0, -1}, {2, 0, -2}, {1, 0, -2}, {0, 0, -2}, {-1, 0, -2},
+				{-2, 0, -2}, {-2, 0, -1}, {-2, 0, 0}, {-2, 0, 1}, {-2, 0, 2},
+				{-2, 0, 3}, {-1, 0, 3}, {0, 0, 3}, {1, 0, 3}, {2, 0, 3},
+				{3, 0, 3}, {3, 0, 2}, {3, 0, 1}, {3, 0, 0}, {3, 0, -1},
+				{3, 0, -2}, {3, 0, -3}, {2, 0, -3}, {1, 0, -3}, {0, 0, -3},
+				{-1, 0, -3}, {-2, 0, -3}, {-3, 0, -3}, {-3, 0, -2}, {-3, 0, -1},
+				{-3, 0, 0}, {-3, 0, 1}, {-3, 0, 2}, {-3, 0, 3}}),
+		
+		PILLAR("Pillar",
+			new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 2, 0}, {0, 3, 0}, {0, 4, 0},
+				{0, 5, 0}, {0, 6, 0}}),
+		
+		WALL("Wall",
+			new int[][]{{0, 0, 0}, {1, 0, 0}, {1, 1, 0}, {0, 1, 0}, {-1, 1, 0},
+				{-1, 0, 0}, {-2, 0, 0}, {-2, 1, 0}, {-2, 2, 0}, {-1, 2, 0},
+				{0, 2, 0}, {1, 2, 0}, {2, 2, 0}, {2, 1, 0}, {2, 0, 0},
+				{3, 0, 0}, {3, 1, 0}, {3, 2, 0}, {3, 3, 0}, {2, 3, 0},
+				{1, 3, 0}, {0, 3, 0}, {-1, 3, 0}, {-2, 3, 0}, {-3, 3, 0},
+				{-3, 2, 0}, {-3, 1, 0}, {-3, 0, 0}, {-3, 4, 0}, {-2, 4, 0},
+				{-1, 4, 0}, {0, 4, 0}, {1, 4, 0}, {2, 4, 0}, {3, 4, 0},
+				{3, 5, 0}, {2, 5, 0}, {1, 5, 0}, {0, 5, 0}, {-1, 5, 0},
+				{-2, 5, 0}, {-3, 5, 0}, {-3, 6, 0}, {-2, 6, 0}, {-1, 6, 0},
+				{0, 6, 0}, {1, 6, 0}, {2, 6, 0}, {3, 6, 0}}),
+		
+		WURST("Wurst",
+			new int[][]{{0, 0, 0}, {1, 0, 0}, {1, 1, 0}, {0, 1, 0}, {0, 1, 1},
+				{1, 1, 1}, {2, 1, 1}, {2, 1, 0}, {2, 0, 0}, {2, 1, -1},
+				{1, 1, -1}, {0, 1, -1}, {-1, 1, -1}, {-1, 1, 0}, {-1, 0, 0},
+				{-2, 0, 0}, {-2, 1, 0}, {-2, 1, 1}, {-1, 1, 1}, {-1, 2, 0},
+				{0, 2, 0}, {1, 2, 0}, {2, 2, 0}, {3, 1, 0}, {-2, 1, -1},
+				{-2, 2, 0}, {-3, 1, 0}});
+		
+		private final String name;
+		private final int[][] data;
+		
+		private DefaultTemplate(String name, int[][] data)
 		{
-			throw new RuntimeException(e);
+			this.name = name;
+			this.data = data;
 		}
 	}
 }
