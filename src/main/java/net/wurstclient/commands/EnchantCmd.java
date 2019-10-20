eea9e1cf5cd9beec86584ba1662ee6316f444aa6
Alexander01998 <contact.wurstimperium@gmail.com>
2019-10-20 10:02:59 +0200

    Clean up EnchantCmd

--- src/main/java/net/wurstclient/commands/EnchantCmd.java
+++ src/main/java/net/wurstclient/commands/EnchantCmd.java
@@ -1,8 +1,9 @@
 package net.wurstclient.commands;
 
-import net.minecraft.item.ItemStack;
-import net.minecraft.enchantment.*;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.registry.Registry;
 import net.wurstclient.command.CmdError;
 import net.wurstclient.command.CmdException;
 import net.wurstclient.command.CmdSyntaxError;
@@ -13,64 +14,64 @@ public final class EnchantCmd extends Command
 {
 	public EnchantCmd()
 	{
-		super("enchant", "Enchants an item with everything, up to level 127.", ".enchant <slot> <level>");
+		super("enchant", "Enchants an item with everything,\n"
+			+ "except for silk touch and curses.", ".enchant");
 	}
 	
 	@Override
-	public void call (String[] args) throws CmdException
+	public void call(String[] args) throws CmdException
 	{
-		if(args.length != 2)
-			throw new CmdSyntaxError();
-		
 		if(!MC.player.abilities.creativeMode)
 			throw new CmdError("Creative mode only.");
 		
-		ItemStack item = getItem(args[0]);
+		if(args.length > 1)
+			throw new CmdSyntaxError();
 		
-		Enchantment[] ench = new Enchantment[] {Enchantments.AQUA_AFFINITY, Enchantments.BANE_OF_ARTHROPODS, Enchantments.BLAST_PROTECTION, Enchantments.CHANNELING, Enchantments.DEPTH_STRIDER, Enchantments.EFFICIENCY, Enchantments.FEATHER_FALLING, Enchantments.FIRE_ASPECT, Enchantments.FIRE_PROTECTION, Enchantments.FLAME, Enchantments.FORTUNE, Enchantments.FROST_WALKER,Enchantments.IMPALING, Enchantments.INFINITY, Enchantments.KNOCKBACK, Enchantments.LOOTING, Enchantments.LOYALTY, Enchantments.LUCK_OF_THE_SEA, Enchantments.LURE, Enchantments.MENDING, Enchantments.MULTISHOT, Enchantments.PIERCING, Enchantments.POWER, Enchantments.PROJECTILE_PROTECTION, Enchantments.PROTECTION, Enchantments.PUNCH, Enchantments.QUICK_CHARGE, Enchantments.RESPIRATION, Enchantments.SHARPNESS, Enchantments.SMITE, Enchantments.SILK_TOUCH, Enchantments.SWEEPING, Enchantments.THORNS, Enchantments.UNBREAKING};
-		
-		if(item == null)
-			throw new CmdError("There is no item in this slot");
-		
-		int level = Integer.parseInt(args[1]);
-		
-		for(Enchantment enchantment : ench)
-			try
-			{
-				if(enchantment == Enchantments.QUICK_CHARGE)
-					item.addEnchantment(enchantment, 5);
-				else
-					item.addEnchantment(enchantment, level);
-			}catch(Exception e)
-		{
-				
-		}
+		ItemStack stack = getHeldItem();
+		enchant(stack);
 		
 		ChatUtils.message("Item enchanted.");
 	}
 	
-	private ItemStack getItem(String slot)
-			throws CmdSyntaxError
+	private ItemStack getHeldItem() throws CmdError
+	{
+		ItemStack stack = MC.player.inventory.getMainHandStack();
+		
+		if(stack.isEmpty())
+			throw new CmdError("There is no item in your hand.");
+		
+		return stack;
+	}
+	
+	private void enchant(ItemStack stack)
+	{
+		for(Enchantment enchantment : Registry.ENCHANTMENT)
 		{
-			switch(slot.toLowerCase())
+			if(enchantment == Enchantments.SILK_TOUCH)
+				continue;
+			
+			if(enchantment.isCursed())
+				continue;
+			
+			if(enchantment == Enchantments.QUICK_CHARGE)
 			{
-				case "hand":
-				return MC.player.inventory.getMainHandStack();
-				
-				case "head":
-				return MC.player.inventory.getArmorStack(3);
-				
-				case "chest":
-				return MC.player.inventory.getArmorStack(2);
-				
-				case "legs":
-				return MC.player.inventory.getArmorStack(1);
-				
-				case "feet":
-				return MC.player.inventory.getArmorStack(0);
-				
-				default:
-				throw new CmdSyntaxError();
+				stack.addEnchantment(enchantment, 5);
+				continue;
 			}
+			
+			stack.addEnchantment(enchantment, 127);
 		}
+	}
+	
+	@Override
+	public String getPrimaryAction()
+	{
+		return "Enchant Held Item";
+	}
+	
+	@Override
+	public void doPrimaryAction()
+	{
+		WURST.getCmdProcessor().process("enchant");
+	}
 }
