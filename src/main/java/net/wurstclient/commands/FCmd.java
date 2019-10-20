e62619f5e3584220f25def964ad7696c615dbaef
Mersid <mersid_email@yahoo.com>
2019-10-20 12:41:10 -0700

    Added way to prevent duplicates.

--- src/main/java/net/wurstclient/commands/FCmd.java
+++ src/main/java/net/wurstclient/commands/FCmd.java
@@ -3,7 +3,9 @@ package net.wurstclient.commands;
 import net.wurstclient.WurstClient;
 import net.wurstclient.command.CmdError;
 import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
 import net.wurstclient.command.Command;
+import net.wurstclient.util.ChatUtils;
 
 public class FCmd extends Command {
 	public FCmd() {
@@ -12,19 +14,27 @@ public class FCmd extends Command {
 
 	@Override
 	public void call(String[] args) throws CmdException {
-		if (args.length < 1 || args.length > 2) throw new CmdError("Incorrect number of parameters passed!");
-
+		if (args.length < 1 || args.length > 2)
+		{
+			throw new CmdSyntaxError();
+		}
 
 		switch (args[0].toLowerCase())
 		{
 			case "list":
-				System.out.println("LIST");
+				ChatUtils.message("These are your friends:");
+				for (String friend : WurstClient.INSTANCE.getFriendsList().getAllFriends())
+				{
+					ChatUtils.message(friend);
+				}
 				break;
 			case "add":
+				if (args.length < 2) throw new CmdError("No player supplied!");
 				WurstClient.INSTANCE.getFriendsList().addFriend(args[1]);
 				break;
 			case "remove":
-				System.out.println("REMOVE");
+				if (args.length < 2) throw new CmdError("No player supplied!");
+				WurstClient.INSTANCE.getFriendsList().removeFriend(args[1]);
 				break;
 			default:
 				throw new CmdError("First parameter must be \"list\", \"add\", or \"remove\".");
--- src/main/java/net/wurstclient/util/FriendsList.java
+++ src/main/java/net/wurstclient/util/FriendsList.java
@@ -2,6 +2,7 @@ package net.wurstclient.util;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,13 +19,44 @@ public class FriendsList {
 	}
 
 	/**
-	 * Adds a friend, and saves them to a list.
+	 * Adds a friend, and saves them to a list. If the player already exists, they will not be re-added, and a message will be printed to chat.
 	 * @param playerName The name of the player to add as friend.
 	 */
 	public void addFriend(String playerName)
 	{
-		friends.add(playerName);
-		save();
+		if (friends.contains(playerName))
+		{
+			ChatUtils.message(playerName + " is already a friend!");
+		}
+		else
+		{
+			friends.add(playerName);
+			save();
+		}
+	}
+
+	/**
+	 * Removes a friend and then save the list. If the player does not exist, nothing will happen and a message will be printed.
+	 * @param playerName The name of the player to remove from the friends list.
+	 */
+	public void removeFriend(String playerName) {
+		if (friends.contains(playerName))
+		{
+			friends.remove(playerName);
+		}
+		else
+		{
+			ChatUtils.message(playerName + " is not a friend.");
+		}
+	}
+
+	/**
+	 * Gets a list of all friends. Attempts to add or remove from this will cause a crash.
+	 * @return A unmodifiable list of friends.
+	 */
+	public List<String> getAllFriends()
+	{
+		return Collections.unmodifiableList(friends);
 	}
 
 	/**
@@ -32,7 +64,7 @@ public class FriendsList {
 	 */
 	private void save()
 	{
-		System.out.println("TEST: Saved!");
+
 	}
 
 	/**
