83caa0e559e44d9c524fa4831fab1b83a5027015
Alexander01998 <contact.wurstimperium@gmail.com>
2019-11-10 00:45:46 +0200

    Add PanicHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -123,7 +123,7 @@ public final class HackList implements UpdateListener
 	public final NukerHack nukerHack = new NukerHack();
 	// public final NukerLegitHack nukerLegitHack = new NukerLegitHack();
 	// public final OverlayHack overlayHack = new OverlayHack();
-	// public final PanicHack panicHack = new PanicHack();
+	public final PanicHack panicHack = new PanicHack();
 	// public final ParkourHack parkourHack = new ParkourHack();
 	// public final PhaseHack phaseHack = new PhaseHack();
 	public final PlayerEspHack playerEspHack = new PlayerEspHack();
