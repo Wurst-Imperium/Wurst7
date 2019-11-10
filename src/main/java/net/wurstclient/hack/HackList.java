774ad83e277dd34dece829ce8b6b73c32112fae1
Alexander01998 <contact.wurstimperium@gmail.com>
2019-11-10 15:28:55 +0200

    Add AutoSignHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -39,7 +39,7 @@ public final class HackList implements UpdateListener
 	public final AutoFishHack autoFishHack = new AutoFishHack();
 	public final AutoMineHack autoMineHack = new AutoMineHack();
 	public final AutoRespawnHack autoRespawnHack = new AutoRespawnHack();
-	// public final AutoSignHack autoSignHack = new AutoSignHack();
+	public final AutoSignHack autoSignHack = new AutoSignHack();
 	// public final AutoSplashPotHack autoSplashPotHack = new
 	// AutoSplashPotHack();
 	// public final AutoSoupHack autoSoupHack = new AutoSoupHack();
