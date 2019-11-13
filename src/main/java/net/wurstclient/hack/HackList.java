784614a17a92f5ceef17180e6b37131473d08fb4
Alexander01998 <contact.wurstimperium@gmail.com>
2019-11-13 16:38:02 +0200

    Add BlinkHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -51,7 +51,7 @@ public final class HackList implements UpdateListener
 	public final AutoToolHack autoToolHack = new AutoToolHack();
 	public final AutoWalkHack autoWalkHack = new AutoWalkHack();
 	// public final BaseFinderHack baseFinderHack = new BaseFinderHack();
-	// public final BlinkHack blinkHack = new BlinkHack();
+	public final BlinkHack blinkHack = new BlinkHack();
 	// public final BoatFlyHack boatFlyHack = new BoatFlyHack();
 	// public final BonemealAuraHack bonemealAuraHack = new BonemealAuraHack();
 	// public final BowAimbotHack bowAimbotHack = new BowAimbotHack();
