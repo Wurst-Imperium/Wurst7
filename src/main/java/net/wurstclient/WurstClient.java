013ba112ce53f052b177ae4d4238b0a95155319c
Alexander01998 <contact.wurstimperium@gmail.com>
2019-12-08 19:04:36 +0100

    Update to 1.15-pre5

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -47,7 +47,7 @@ public enum WurstClient
 	public static final IMinecraftClient IMC = (IMinecraftClient)MC;
 	
 	public static final String VERSION = "7.0pre18";
-	public static final String MC_VERSION = "1.15-pre4";
+	public static final String MC_VERSION = "1.15-pre5";
 	
 	private WurstAnalytics analytics;
 	private EventManager eventManager;
