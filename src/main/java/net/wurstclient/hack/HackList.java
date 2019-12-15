f2e90f8f134c114bb0684269cf3de9e4998ffdbb
Alexander01998 <contact.wurstimperium@gmail.com>
2019-12-15 02:35:13 +0100

    Add AutoBuild to HackList

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -31,7 +31,7 @@ public final class HackList implements UpdateListener
 	public final AntiSpamHack antiSpamHack = new AntiSpamHack();
 	public final AntiWaterPushHack antiWaterPushHack = new AntiWaterPushHack();
 	public final AutoArmorHack autoArmorHack = new AutoArmorHack();
-	// public final AutoBuildHack autoBuildHack = new AutoBuildHack();
+	public final AutoBuildHack autoBuildHack = new AutoBuildHack();
 	public final AutoDropHack autoDropHack = new AutoDropHack();
 	// public final AutoLeaveHack autoLeaveHack = new AutoLeaveHack();
 	// public final AutoEatHack autoEatHack = new AutoEatHack();
