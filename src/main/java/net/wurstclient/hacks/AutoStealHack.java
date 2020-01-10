fc50093957372422bddf4336081e58c73a48ad5e
Mersid <mersid_email@yahoo.com>
2020-01-09 16:21:39 -0800

    Update AutoStealHack.java

--- src/main/java/net/wurstclient/hacks/AutoStealHack.java
+++ src/main/java/net/wurstclient/hacks/AutoStealHack.java
@@ -13,6 +13,8 @@ import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.SliderSetting;
 
+import java.util.Random;
+
 @SearchTags({"auto steal", "ChestStealer", "chest stealer",
 	"steal store buttons", "Steal/Store buttons"})
 public final class AutoStealHack extends Hack
@@ -21,9 +23,16 @@ public final class AutoStealHack extends Hack
 		"Delay between moving stacks of items.\n"
 			+ "Should be at least 70ms for NoCheat+ servers.",
 		100, 0, 500, 10, v -> (int)v + "ms");
+
+	private final SliderSetting randomDelay = new SliderSetting("Delay random",
+		"Randomly delay moving stacks of items by +/- value to help avoid anti-cheats.\n"
+			+ "Do not set this value greater than delay.",
+		20, 0, 200, 10, v -> (int)v + "ms");
 	
 	private final CheckboxSetting buttons =
 		new CheckboxSetting("Steal/Store buttons", true);
+
+	private final Random random = new Random();
 	
 	public AutoStealHack()
 	{
@@ -32,6 +41,7 @@ public final class AutoStealHack extends Hack
 		setCategory(Category.ITEMS);
 		addSetting(buttons);
 		addSetting(delay);
+		addSetting(randomDelay);
 	}
 	
 	public boolean areButtonsVisible()
@@ -41,6 +51,11 @@ public final class AutoStealHack extends Hack
 	
 	public long getDelay()
 	{
-		return delay.getValueI();
+		return delay.getValueI() + getRandom();
+	}
+
+	private long getRandom()
+	{
+		return random.nextInt(randomDelay.getValueI()) * 2 - (randomDelay.getValueI());
 	}
 }
