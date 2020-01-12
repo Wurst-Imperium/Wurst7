7a08d008c99a00c1c84272b293a4288c7e8a0ffe
Jakub Staroń <kubastaron@hotmail.com>
2020-01-12 16:22:00 +0100

    Fix Prospector/ModMenu compatibility

--- src/main/java/net/wurstclient/mixin/TitleScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/TitleScreenMixin.java
@@ -7,6 +7,7 @@
  */
 package net.wurstclient.mixin;
 
+import net.minecraft.client.resource.language.I18n;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,17 +30,15 @@ public abstract class TitleScreenMixin extends Screen
 	}
 	
 	@Inject(at = {@At("RETURN")}, method = {"initWidgetsNormal(II)V"})
-	private void onInitWidgetsNormal(int int_1, int int_2, CallbackInfo ci)
+	private void onInitWidgetsNormal(int y, int spacingY, CallbackInfo ci)
 	{
-		addButton(new ButtonWidget(width / 2 + 2, int_1 + int_2 * 2, 98, 20,
+		addButton(new ButtonWidget(width / 2 + 2, y + spacingY * 2, 98, 20,
 			"Alt Manager", b -> minecraft.openScreen(new AltManagerScreen(this,
 				WurstClient.INSTANCE.getAltManager()))));
 		
 		for(AbstractButtonWidget button : buttons)
 		{
-			if(button.x != width / 2 - 100)
-				continue;
-			if(button.y != int_1 + int_2 * 2)
+			if(!button.getMessage().equals(I18n.translate("menu.online")))
 				continue;
 			
 			button.setWidth(98);
