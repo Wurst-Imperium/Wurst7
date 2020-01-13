efe4b94748ecc88cd2805d611da41e19619b921b
Alexander01998 <contact.wurstimperium@gmail.com>
2020-01-13 13:50:23 +0100

    Eclipse clean up

--- src/main/java/net/wurstclient/hacks/TrajectoriesHack.java
+++ src/main/java/net/wurstclient/hacks/TrajectoriesHack.java
@@ -1,5 +1,9 @@
 package net.wurstclient.hacks;
 
+import java.util.ArrayList;
+
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
@@ -12,35 +16,35 @@ import net.wurstclient.Category;
 import net.wurstclient.events.RenderListener;
 import net.wurstclient.hack.Hack;
 import net.wurstclient.util.TrajectoryPath;
-import org.lwjgl.opengl.GL11;
 
-import java.util.ArrayList;
-
-public class TrajectoriesHack extends Hack implements RenderListener {
-
-	private static final int MAX_POINTS = 1000; // Max points in a trajectory. Bigger means longer traces in air, but takes longer to calculate.
+public class TrajectoriesHack extends Hack implements RenderListener
+{
+	
+	private static final int MAX_POINTS = 1000;
+	// Max points in a trajectory. Bigger means longer traces in air, but takes
+	// longer to calculate.
 	private static final long DEFAULT_COLOR = 0x00FF007FL;
-
+	
 	private final ArrayList<PlayerEntity> livingEntities = new ArrayList<>();
-
+	
 	public TrajectoriesHack()
 	{
 		super("Trajectories", "Shows the trajectory of ranged items");
 		setCategory(Category.RENDER);
 	}
-
+	
 	@Override
 	public void onEnable()
 	{
 		EVENTS.add(RenderListener.class, this);
 	}
-
+	
 	@Override
 	public void onDisable()
 	{
 		EVENTS.remove(RenderListener.class, this);
 	}
-
+	
 	@Override
 	public void onRender(float partialTicks)
 	{
@@ -48,54 +52,65 @@ public class TrajectoriesHack extends Hack implements RenderListener {
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		//GL11.glDisable(GL11.GL_DEPTH_TEST);
+		// GL11.glDisable(GL11.GL_DEPTH_TEST);
 		GL11.glDepthMask(false);
 		GL11.glEnable(GL11.GL_LINE_SMOOTH);
 		GL11.glLineWidth(2.0f);
 		Camera camera = MinecraftClient.getInstance().gameRenderer.getCamera();
-
-		// Minecraft 1.15+ handles rotations and transforms relative to the camera position.
-		// This code "undoes" the rotation and places it relative to Minecraftian x/y/z coords.
+		
+		// Minecraft 1.15+ handles rotations and transforms relative to the
+		// camera position.
+		// This code "undoes" the rotation and places it relative to
+		// Minecraftian x/y/z coords.
 		// Fun fact - it took dozens of hours to come up with this. :facepalm:
 		GL11.glRotated(camera.getPitch(), 1, 0, 0);
 		GL11.glRotated(camera.getYaw() + 180, 0, 1, 0);
-
-		for (Entity e : MC.world.getEntities())
+		
+		for(Entity e : MC.world.getEntities())
 		{
-			if (!(e instanceof LivingEntity)) continue;
+			if(!(e instanceof LivingEntity))
+				continue;
 			LivingEntity entity = (LivingEntity)e;
-
+			
 			TrajectoryPath path = getPath(entity);
-
+			
 			double defaultred = ((DEFAULT_COLOR & 0xFF000000) >> 24) / 255d;
 			double defaultgreen = ((DEFAULT_COLOR & 0x00FF0000) >> 16) / 255d;
 			double defaultblue = ((DEFAULT_COLOR & 0x0000FF00) >> 8) / 255d;
 			double defaultalpha = (DEFAULT_COLOR & 0x000000FF) / 255d;
-
+			
 			GL11.glBegin(GL11.GL_LINE_STRIP);
-
-			for (Vec3d point : path)
+			
+			for(Vec3d point : path)
 			{
-				//System.out.println("R: " + defaultred + ", G: " + defaultgreen + ", B: " + defaultblue + ", A: " + defaultalpha);
-				GL11.glColor4d(defaultred, defaultgreen, defaultblue, defaultalpha);
-				GL11.glVertex3d(point.x - BlockEntityRenderDispatcher.INSTANCE.camera.getPos().x, point.y - BlockEntityRenderDispatcher.INSTANCE.camera.getPos().y , point.z - BlockEntityRenderDispatcher.INSTANCE.camera.getPos().z);
+				// System.out.println("R: " + defaultred + ", G: " +
+				// defaultgreen + ", B: " + defaultblue + ", A: " +
+				// defaultalpha);
+				GL11.glColor4d(defaultred, defaultgreen, defaultblue,
+					defaultalpha);
+				GL11.glVertex3d(point.x
+					- BlockEntityRenderDispatcher.INSTANCE.camera.getPos().x,
+					point.y - BlockEntityRenderDispatcher.INSTANCE.camera
+						.getPos().y,
+					point.z - BlockEntityRenderDispatcher.INSTANCE.camera
+						.getPos().z);
 			}
 			GL11.glEnd();
 		}
-
+		
 		GL11.glColor4d(1, 1, 1, 1);
 		GL11.glColor4f(1, 1, 1, 1);
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		//GL11.glEnable(GL11.GL_DEPTH_TEST);
+		// GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glDepthMask(true);
 		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 		GL11.glPopMatrix();
 	}
-
-
+	
 	/**
 	 * Returns the path the entity's held item will take through the air.
+	 *
 	 * @param entity
 	 * @return
 	 */
@@ -103,126 +118,161 @@ public class TrajectoriesHack extends Hack implements RenderListener {
 	{
 		return getPath(entity, false);
 	}
-
+	
 	/**
 	 * Returns the path the entity's held item will take through the air.
 	 * if maxPitch is true, return the value of the path at 45°.
+	 *
 	 * @param entity
 	 * @return
 	 */
 	private static TrajectoryPath getPath(LivingEntity entity, boolean maxPitch)
 	{
 		TrajectoryPath path = new TrajectoryPath();
-		Item item = entity.getMainHandStack() == null ? null : entity.getMainHandStack().getItem();
-
-		// If not holding anything or held item is not ranged weapon, return empty path (not null).
-		if (item == null || !isRangedWeapon(item))
-		{
+		Item item = entity.getMainHandStack() == null ? null
+			: entity.getMainHandStack().getItem();
+		
+		// If not holding anything or held item is not ranged weapon, return
+		// empty path (not null).
+		if(item == null || !isRangedWeapon(item))
 			return path;
-		}
-
-		// The three values indicate the arrow's position in space, using standard Minecraft coordinates.
+			
+		// The three values indicate the arrow's position in space, using
+		// standard Minecraft coordinates.
 		// They will be updated each cycle until arrow impacts.
-		// Obviously, at this point, these values are very close to the firing entity's pos.
-		double arrowPosX = entity.lastRenderX + (entity.getX() - entity.lastRenderX) * MinecraftClient.getInstance().getTickDelta() - Math.cos((float)Math.toRadians(entity.yaw)) * 0.16f;
-		double arrowPosY = entity.lastRenderY + (entity.getY() - entity.lastRenderY) * MinecraftClient.getInstance().getTickDelta() + entity.getStandingEyeHeight() - 0.1;
-		double arrowPosZ = entity.lastRenderZ + (entity.getZ() - entity.lastRenderZ) * MinecraftClient.getInstance().getTickDelta() - Math.sin((float)Math.toRadians(entity.yaw)) * 0.16f;
-
+		// Obviously, at this point, these values are very close to the firing
+		// entity's pos.
+		double arrowPosX = entity.lastRenderX
+			+ (entity.getX() - entity.lastRenderX)
+				* MinecraftClient.getInstance().getTickDelta()
+			- Math.cos((float)Math.toRadians(entity.yaw)) * 0.16f;
+		double arrowPosY = entity.lastRenderY
+			+ (entity.getY() - entity.lastRenderY)
+				* MinecraftClient.getInstance().getTickDelta()
+			+ entity.getStandingEyeHeight() - 0.1;
+		double arrowPosZ = entity.lastRenderZ
+			+ (entity.getZ() - entity.lastRenderZ)
+				* MinecraftClient.getInstance().getTickDelta()
+			- Math.sin((float)Math.toRadians(entity.yaw)) * 0.16f;
+		
 		// Motion factor. Arrows go faster than snowballs and all that...
-		double projectileMotionFactor = (item instanceof BowItem) ? 1.0 : 0.4;
-
-		// Pitch and yaw of the entity's facing, in radians. This is because the Java Math library expects values in radians.
-		double yaw = Math.toRadians(entity.yaw); // Left/right. Right is positive, left is negative. Does not wrap.
-		double pitch = maxPitch ? (-0.25 * Math.PI) : Math.toRadians(entity.pitch); // Up/down. Up is negative, down is positive.
-
-		// The motion of the projectile. These values are small doubles that will be added to the arrowPos values
-		// each iteration of the projection in order to form a list of points that show the motion of a projectile.
+		double projectileMotionFactor = item instanceof BowItem ? 1.0 : 0.4;
+		
+		// Pitch and yaw of the entity's facing, in radians. This is because the
+		// Java Math library expects values in radians.
+		double yaw = Math.toRadians(entity.yaw); // Left/right. Right is
+													// positive, left is
+													// negative. Does not wrap.
+		double pitch =
+			maxPitch ? -0.25 * Math.PI : Math.toRadians(entity.pitch); // Up/down.
+																		// Up
+																		// is
+																		// negative,
+																		// down
+																		// is
+																		// positive.
+		
+		// The motion of the projectile. These values are small doubles that
+		// will be added to the arrowPos values
+		// each iteration of the projection in order to form a list of points
+		// that show the motion of a projectile.
 		// Right now, this is the value added after the first iteration.
 		// Bigger values likely means faster projectiles.
-		double projectileMotionX = (float) (-Math.sin(yaw) * Math.cos(pitch) * projectileMotionFactor);
-		double projectileMotionY = (float) (-Math.sin(pitch) * projectileMotionFactor);
-		double projectileMotionZ = (float) (Math.cos(yaw) * Math.cos(pitch) * projectileMotionFactor);
-
-		// 3D Pythagorean theorem. Returns the length of the vector representing the distance between the two points used to render a path.
+		double projectileMotionX =
+			(float)(-Math.sin(yaw) * Math.cos(pitch) * projectileMotionFactor);
+		double projectileMotionY =
+			(float)(-Math.sin(pitch) * projectileMotionFactor);
+		double projectileMotionZ =
+			(float)(Math.cos(yaw) * Math.cos(pitch) * projectileMotionFactor);
+		
+		// 3D Pythagorean theorem. Returns the length of the vector representing
+		// the distance between the two points used to render a path.
 		// Last point + projectileMotion = next point's distance in 3D space.
-		double projectileMotion = Math.sqrt(projectileMotionX * projectileMotionX + projectileMotionY * projectileMotionY + projectileMotionZ * projectileMotionZ);
-
+		double projectileMotion =
+			Math.sqrt(projectileMotionX * projectileMotionX
+				+ projectileMotionY * projectileMotionY
+				+ projectileMotionZ * projectileMotionZ);
+		
 		// Divide the motion in each axis by the length. Not really sure why.
 		projectileMotionX /= projectileMotion;
 		projectileMotionY /= projectileMotion;
 		projectileMotionZ /= projectileMotion;
-
-		// Calculation that modifies the projectilemotion. No idea how this works.
-		if (item instanceof BowItem || item instanceof CrossbowItem) {
+		
+		// Calculation that modifies the projectilemotion. No idea how this
+		// works.
+		if(item instanceof BowItem || item instanceof CrossbowItem)
+		{
 			float bowPower = (72000 - entity.getItemUseTimeLeft()) / 20.0f;
 			bowPower = (bowPower * bowPower + bowPower * 2.0f) / 3.0f;
-			if (bowPower > 1.0f || bowPower <= 0.1f) {
+			if(bowPower > 1.0f || bowPower <= 0.1f)
 				bowPower = 1.0f;
-			}
 			bowPower *= 3.0f;
 			projectileMotionX *= bowPower;
 			projectileMotionY *= bowPower;
 			projectileMotionZ *= bowPower;
-
-		}
-		else {
+			
+		}else
+		{
 			projectileMotionX *= 1.5;
 			projectileMotionY *= 1.5;
 			projectileMotionZ *= 1.5;
 		}
-
-		// Specifies the ballistic drop for various items. Smaller values means less drop, so greater range.
-		double gravity = (item instanceof BowItem || item instanceof CrossbowItem) ? 0.05 :
-				((item instanceof PotionItem) ? 0.4 : ((item instanceof FishingRodItem) ? 0.15 : (item instanceof TridentItem) ? 0.015 :0.03));
-
-		for (int i = 0; i < MAX_POINTS; i++)
+		
+		// Specifies the ballistic drop for various items. Smaller values means
+		// less drop, so greater range.
+		double gravity =
+			item instanceof BowItem || item instanceof CrossbowItem ? 0.05
+				: item instanceof PotionItem ? 0.4
+					: item instanceof FishingRodItem ? 0.15
+						: item instanceof TridentItem ? 0.015 : 0.03;
+		
+		for(int i = 0; i < MAX_POINTS; i++)
 		{
 			// Add this point (in absolute space) to the path.
 			path.addPoint(new Vec3d(arrowPosX, arrowPosY, arrowPosZ));
-
-			// Add the projectile's motion this iteration to its absolute position, thereby updating it for the next iteration.
+			
+			// Add the projectile's motion this iteration to its absolute
+			// position, thereby updating it for the next iteration.
 			// Though, I'm not really sure what the 0.1x multiplier is doing.
 			arrowPosX += projectileMotionX * 0.1;
 			arrowPosY += projectileMotionY * 0.1;
 			arrowPosZ += projectileMotionZ * 0.1;
-
+			
 			// Projectile gets slower due to friction in air or something.
 			projectileMotionX *= 0.999;
 			projectileMotionY *= 0.999;
 			projectileMotionZ *= 0.999;
-
+			
 			// Apply gravity drop to the motion.
 			projectileMotionY -= gravity * 0.1;
 		}
-
+		
 		return path;
 	}
-
-	private static boolean isRangedWeapon(Item item) {
-		return
-				item instanceof BowItem ||
-						item instanceof CrossbowItem ||
-						item instanceof SnowballItem ||
-						item instanceof EggItem ||
-						item instanceof EnderPearlItem ||
-						item instanceof SplashPotionItem ||
-						item instanceof LingeringPotionItem ||
-						item instanceof FishingRodItem ||
-						item instanceof TridentItem;
+	
+	private static boolean isRangedWeapon(Item item)
+	{
+		return item instanceof BowItem || item instanceof CrossbowItem
+			|| item instanceof SnowballItem || item instanceof EggItem
+			|| item instanceof EnderPearlItem
+			|| item instanceof SplashPotionItem
+			|| item instanceof LingeringPotionItem
+			|| item instanceof FishingRodItem || item instanceof TridentItem;
 	}
-
+	
 	public enum Style
 	{
 		BOXES("Boxes only", true, false),
 		LINES("Lines only", false, true),
 		LINES_AND_BOXES("Lines and boxes", true, true);
-
+		
 		private final String name;
+		
 		private Style(String name, boolean boxes, boolean lines)
 		{
 			this.name = name;
 		}
-
+		
 		@Override
 		public String toString()
 		{
--- src/main/java/net/wurstclient/util/TrajectoryPath.java
+++ src/main/java/net/wurstclient/util/TrajectoryPath.java
@@ -7,13 +7,16 @@ import java.util.List;
 import net.minecraft.util.math.Vec3d;
 
 /**
- * The path a projectile will take through the air. This consists solely of vectors depicting the coordinates the projectile will likely pass through.
+ * The path a projectile will take through the air. This consists solely of
+ * vectors depicting the coordinates the projectile will likely pass through.
  * Positions are in standard Minecraft coordinates.
+ * 
  * @author Admin
  *
  */
-public class TrajectoryPath implements Iterable<Vec3d> {
-	private final List<Vec3d> points = new LinkedList<Vec3d>();
+public class TrajectoryPath implements Iterable<Vec3d>
+{
+	private final List<Vec3d> points = new LinkedList<>();
 	
 	public void addPoint(Vec3d point)
 	{
@@ -24,39 +27,40 @@ public class TrajectoryPath implements Iterable<Vec3d> {
 	{
 		return points.size() == 0;
 	}
-
+	
 	@Override
-	public Iterator<Vec3d> iterator() {
+	public Iterator<Vec3d> iterator()
+	{
 		return points.iterator();
 	}
 	
 	/**
 	 * Returns pairs of numbers in the form (x, y).
+	 * 
 	 * @return
 	 */
 	public String plotX()
 	{
 		StringBuilder sb = new StringBuilder();
-		for (Vec3d point : points)
-		{
+		for(Vec3d point : points)
 			sb.append("(" + point.x + "," + point.y + "),");
-		}
-		if (points.size() > 0) sb.deleteCharAt(sb.length() - 1);
+		if(points.size() > 0)
+			sb.deleteCharAt(sb.length() - 1);
 		return sb.toString();
 	}
 	
 	/**
 	 * Returns pairs of numbers in the form (z, y).
+	 * 
 	 * @return
 	 */
 	public String plotZ()
 	{
 		StringBuilder sb = new StringBuilder();
-		for (Vec3d point : points)
-		{
+		for(Vec3d point : points)
 			sb.append("(" + point.z + "," + point.y + "),");
-		}
-		if (points.size() > 0) sb.deleteCharAt(sb.length() - 1);
+		if(points.size() > 0)
+			sb.deleteCharAt(sb.length() - 1);
 		return sb.toString();
 	}
 }
