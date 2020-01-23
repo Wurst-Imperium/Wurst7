fb8f868f84f9f26b025f26e79e301de7a35738d0
Alexander01998 <contact.wurstimperium@gmail.com>
2020-01-23 01:00:37 +0100

    Add SkinDerpHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -134,7 +134,7 @@ public final class HackList implements UpdateListener
 	public final ScaffoldWalkHack scaffoldWalkHack = new ScaffoldWalkHack();
 	public final SearchHack searchHack = new SearchHack();
 	public final ServerCrasherHack serverCrasherHack = new ServerCrasherHack();
-	// public final SkinDerpHack skinDerpHack = new SkinDerpHack();
+	public final SkinDerpHack skinDerpHack = new SkinDerpHack();
 	public final SneakHack sneakHack = new SneakHack();
 	// public final SpammerHack spammerHack = new SpammerHack();
 	// public final SpeedHackHack speedHackHack = new SpeedHackHack();
