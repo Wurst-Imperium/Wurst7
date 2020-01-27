e8d73d7f44275ca1322e364fca3ac06d33dcb693
Alexander01998 <contact.wurstimperium@gmail.com>
2020-01-27 01:49:07 +0100

    Add ParkourHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -118,7 +118,7 @@ public final class HackList implements UpdateListener
 	// public final NukerLegitHack nukerLegitHack = new NukerLegitHack();
 	// public final OverlayHack overlayHack = new OverlayHack();
 	public final PanicHack panicHack = new PanicHack();
-	// public final ParkourHack parkourHack = new ParkourHack();
+	public final ParkourHack parkourHack = new ParkourHack();
 	public final PlayerEspHack playerEspHack = new PlayerEspHack();
 	// public final PlayerFinderHack playerFinderHack = new PlayerFinderHack();
 	// public final PotionSaverHack potionSaverHack = new PotionSaverHack();
