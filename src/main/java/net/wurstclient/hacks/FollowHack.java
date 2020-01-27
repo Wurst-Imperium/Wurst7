597cb64357039a031d85b5e7254438058035c90a
Alexander01998 <contact.wurstimperium@gmail.com>
2020-01-27 01:07:58 +0100

    Add filters to FollowHack

--- src/main/java/net/wurstclient/hacks/FollowHack.java
+++ src/main/java/net/wurstclient/hacks/FollowHack.java
@@ -9,11 +9,25 @@ package net.wurstclient.hacks;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.AmbientEntity;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.entity.mob.WaterCreatureEntity;
+import net.minecraft.entity.mob.ZombiePigmanEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.GolemEntity;
+import net.minecraft.entity.passive.HorseBaseEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.wurstclient.Category;
 import net.wurstclient.ai.PathFinder;
@@ -46,6 +60,49 @@ public final class FollowHack extends Hack
 	private final CheckboxSetting useAi =
 		new CheckboxSetting("Use AI (experimental)", false);
 	
+	private final CheckboxSetting filterPlayers = new CheckboxSetting(
+		"Filter players", "Won't follow other players.", false);
+	
+	private final CheckboxSetting filterSleeping = new CheckboxSetting(
+		"Filter sleeping", "Won't follow sleeping players.", false);
+	
+	private final SliderSetting filterFlying =
+		new SliderSetting("Filter flying",
+			"Won't follow players that\n" + "are at least the given\n"
+				+ "distance above ground.",
+			0, 0, 2, 0.05,
+			v -> v == 0 ? "off" : ValueDisplay.DECIMAL.getValueString(v));
+	
+	private final CheckboxSetting filterMonsters = new CheckboxSetting(
+		"Filter monsters", "Won't follow zombies, creepers, etc.", true);
+	
+	private final CheckboxSetting filterPigmen = new CheckboxSetting(
+		"Filter pigmen", "Won't follow zombie pigmen.", true);
+	
+	private final CheckboxSetting filterEndermen =
+		new CheckboxSetting("Filter endermen", "Won't follow endermen.", true);
+	
+	private final CheckboxSetting filterAnimals = new CheckboxSetting(
+		"Filter animals", "Won't follow pigs, cows, etc.", true);
+	
+	private final CheckboxSetting filterBabies =
+		new CheckboxSetting("Filter babies",
+			"Won't follow baby pigs,\n" + "baby villagers, etc.", true);
+	
+	private final CheckboxSetting filterPets =
+		new CheckboxSetting("Filter pets",
+			"Won't follow tamed wolves,\n" + "tamed horses, etc.", true);
+	
+	private final CheckboxSetting filterVillagers = new CheckboxSetting(
+		"Filter villagers", "Won't follow villagers.", true);
+	
+	private final CheckboxSetting filterGolems =
+		new CheckboxSetting("Filter golems",
+			"Won't follow iron golems,\n" + "snow golems and shulkers.", true);
+	
+	private final CheckboxSetting filterInvisible = new CheckboxSetting(
+		"Filter invisible", "Won't follow invisible entities.", false);
+	
 	public FollowHack()
 	{
 		super("Follow",
@@ -54,6 +111,19 @@ public final class FollowHack extends Hack
 		setCategory(Category.MOVEMENT);
 		addSetting(distance);
 		addSetting(useAi);
+		
+		addSetting(filterPlayers);
+		addSetting(filterSleeping);
+		addSetting(filterFlying);
+		addSetting(filterMonsters);
+		addSetting(filterPigmen);
+		addSetting(filterEndermen);
+		addSetting(filterAnimals);
+		addSetting(filterBabies);
+		addSetting(filterPets);
+		addSetting(filterVillagers);
+		addSetting(filterGolems);
+		addSetting(filterInvisible);
 	}
 	
 	@Override
@@ -70,12 +140,66 @@ public final class FollowHack extends Hack
 	{
 		if(entity == null)
 		{
-			entity = StreamSupport
+			Stream<Entity> stream = StreamSupport
 				.stream(MC.world.getEntities().spliterator(), true)
 				.filter(e -> e instanceof LivingEntity)
 				.filter(e -> !e.removed && ((LivingEntity)e).getHealth() > 0)
 				.filter(e -> e != MC.player)
-				.filter(e -> !(e instanceof FakePlayerEntity))
+				.filter(e -> !(e instanceof FakePlayerEntity));
+			
+			if(filterPlayers.isChecked())
+				stream = stream.filter(e -> !(e instanceof PlayerEntity));
+			
+			if(filterSleeping.isChecked())
+				stream = stream.filter(e -> !(e instanceof PlayerEntity
+					&& ((PlayerEntity)e).isSleeping()));
+			
+			if(filterFlying.getValue() > 0)
+				stream = stream.filter(e -> {
+					
+					if(!(e instanceof PlayerEntity))
+						return true;
+					
+					Box box = e.getBoundingBox();
+					box = box.union(box.offset(0, -filterFlying.getValue(), 0));
+					return MC.world.doesNotCollide(box);
+				});
+			
+			if(filterMonsters.isChecked())
+				stream = stream.filter(e -> !(e instanceof Monster));
+			
+			if(filterPigmen.isChecked())
+				stream = stream.filter(e -> !(e instanceof ZombiePigmanEntity));
+			
+			if(filterEndermen.isChecked())
+				stream = stream.filter(e -> !(e instanceof EndermanEntity));
+			
+			if(filterAnimals.isChecked())
+				stream = stream.filter(e -> !(e instanceof AnimalEntity
+					|| e instanceof AmbientEntity
+					|| e instanceof WaterCreatureEntity));
+			
+			if(filterBabies.isChecked())
+				stream = stream.filter(e -> !(e instanceof PassiveEntity
+					&& ((PassiveEntity)e).isBaby()));
+			
+			if(filterPets.isChecked())
+				stream = stream
+					.filter(e -> !(e instanceof TameableEntity
+						&& ((TameableEntity)e).isTamed()))
+					.filter(e -> !(e instanceof HorseBaseEntity
+						&& ((HorseBaseEntity)e).isTame()));
+			
+			if(filterVillagers.isChecked())
+				stream = stream.filter(e -> !(e instanceof VillagerEntity));
+			
+			if(filterGolems.isChecked())
+				stream = stream.filter(e -> !(e instanceof GolemEntity));
+			
+			if(filterInvisible.isChecked())
+				stream = stream.filter(e -> !e.isInvisible());
+			
+			entity = stream
 				.min(Comparator
 					.comparingDouble(e -> MC.player.squaredDistanceTo(e)))
 				.orElse(null);
