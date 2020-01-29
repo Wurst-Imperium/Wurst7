b10915c1f2df87162b00ebb89c07a68245689d63
Daleth Darko <Daleth@Dalethium.com>
2020-01-28 18:10:27 -0800

    Fixed issue with passwords in altmgr

--- src/main/java/net/wurstclient/altmanager/screens/AltEditorScreen.java
+++ src/main/java/net/wurstclient/altmanager/screens/AltEditorScreen.java
@@ -83,6 +83,7 @@ public abstract class AltEditorScreen extends Screen
 				stars += "*";
 			return stars;
 		});
+		passwordBox.setMaxLength(64);
 		children.add(passwordBox);
 		
 		setInitialFocus(emailBox);
