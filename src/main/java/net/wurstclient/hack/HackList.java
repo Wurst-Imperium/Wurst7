5de54a8b252a30d77896af1ce08691cdbd50d133
Alexander01998 <contact.wurstimperium@gmail.com>
2020-01-30 18:21:37 +0100

    Add MultiAuraHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -103,7 +103,7 @@ public final class HackList implements UpdateListener
 	public final MileyCyrusHack mileyCyrusHack = new MileyCyrusHack();
 	public final MobEspHack mobEspHack = new MobEspHack();
 	public final MobSpawnEspHack mobSpawnEspHack = new MobSpawnEspHack();
-	// public final MultiAuraHack multiAuraHack = new MultiAuraHack();
+	public final MultiAuraHack multiAuraHack = new MultiAuraHack();
 	// public final NameProtectHack nameProtectHack = new NameProtectHack();
 	// public final NameTagsHack nameTagsHack = new NameTagsHack();
 	public final NavigatorHack navigatorHack = new NavigatorHack();
