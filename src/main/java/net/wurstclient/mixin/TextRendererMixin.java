8ebbdd97a3b4e6f7f70e9227291a643d5a158f98
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-05 10:46:36 +0100

    Fix TextRendererMixin

--- src/main/java/net/wurstclient/mixin/TextRendererMixin.java
+++ src/main/java/net/wurstclient/mixin/TextRendererMixin.java
@@ -18,11 +18,8 @@ import net.wurstclient.WurstClient;
 public abstract class TextRendererMixin implements AutoCloseable
 {
 	@ModifyArg(at = @At(value = "INVOKE",
-		target = "Lnet/minecraft/client/font/TextRenderer;drawInternal(Ljava/lang/String;FFIZLnet/minecraft/client/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I",
-		ordinal = 0),
-		method = {
-			"draw(Ljava/lang/String;FFIZLnet/minecraft/client/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"},
-		index = 0)
+		target = "Lnet/minecraft/client/font/TextRenderer;drawLayer(Ljava/lang/String;FFIZ)F",
+		ordinal = 0), method = {"draw(Ljava/lang/String;FFIZ)I"}, index = 0)
 	private String adjustText(String text)
 	{
 		return WurstClient.INSTANCE.getHax().nameProtectHack.protect(text);
