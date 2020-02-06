41ed95dc36664e52ebc8f843fdf47cf94e31c8c3
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-06 21:14:16 +0100

    Adjust StepHack for 1.15

--- src/main/java/net/wurstclient/hacks/StepHack.java
+++ src/main/java/net/wurstclient/hacks/StepHack.java
@@ -25,13 +25,14 @@ import net.wurstclient.settings.SliderSetting.ValueDisplay;
 public final class StepHack extends Hack implements UpdateListener
 {
 	private final EnumSetting<Mode> mode = new EnumSetting<>("Mode",
-		"\u00a7lSimple\u00a7r mode can step up multiple blocks (enables Height slider).\n"
+		"\u00a7lSimple\u00a7r mode can step up multiple\n"
+			+ "blocks (enables Height slider).\n"
 			+ "\u00a7lLegit\u00a7r mode can bypass NoCheat+.",
 		Mode.values(), Mode.LEGIT);
 	
 	private final SliderSetting height =
 		new SliderSetting("Height", "Only works in \u00a7lSimple\u00a7r mode.",
-			1, 1, 100, 1, ValueDisplay.INTEGER);
+			1, 1, 10, 1, ValueDisplay.INTEGER);
 	
 	public StepHack()
 	{
