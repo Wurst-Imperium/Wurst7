3e52f363717bc0c34183008dab4b40eeaa9e9a8d
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-06 21:26:29 +0100

    Add RemoteViewHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -121,7 +121,7 @@ public final class HackList implements UpdateListener
 	public final RadarHack radarHack = new RadarHack();
 	public final RainbowUiHack rainbowUiHack = new RainbowUiHack();
 	public final ReachHack reachHack = new ReachHack();
-	// public final RemoteViewHack remoteViewHack = new RemoteViewHack();
+	public final RemoteViewHack remoteViewHack = new RemoteViewHack();
 	public final SafeWalkHack safeWalkHack = new SafeWalkHack();
 	public final ScaffoldWalkHack scaffoldWalkHack = new ScaffoldWalkHack();
 	public final SearchHack searchHack = new SearchHack();
