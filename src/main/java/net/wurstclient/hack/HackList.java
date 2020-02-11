143df1c5b8b730e699dcba2916a1cf8c1ef6c26f
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-11 20:14:02 +0100

    Add NoFireOverlayHack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -103,6 +103,7 @@ public final class HackList implements UpdateListener
 	public final NavigatorHack navigatorHack = new NavigatorHack();
 	public final NoClipHack noClipHack = new NoClipHack();
 	public final NoFallHack noFallHack = new NoFallHack();
+	public final NoFireOverlayHack noFireOverlayHack = new NoFireOverlayHack();
 	public final NoHurtcamHack noHurtcamHack = new NoHurtcamHack();
 	// public final NoOverlayHack noOverlayHack = new NoOverlayHack();
 	public final NoPumpkinHack noPumpkinHack = new NoPumpkinHack();
