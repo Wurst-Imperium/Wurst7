016812e71af34c633924d2edecab2dbf7131d956
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-12 22:45:55 +0100

    Add SvCmd

--- src/main/java/net/wurstclient/command/CmdList.java
+++ src/main/java/net/wurstclient/command/CmdList.java
@@ -47,7 +47,7 @@ public final class CmdList
 	public final RenameCmd renameCmd = new RenameCmd();
 	public final RepairCmd repairCmd = new RepairCmd();
 	public final RvCmd rvCmd = new RvCmd();
-	// public final SvCmd svCmd = new SvCmd();
+	public final SvCmd svCmd = new SvCmd();
 	public final SayCmd sayCmd = new SayCmd();
 	public final SetCheckboxCmd setCheckboxCmd = new SetCheckboxCmd();
 	public final SetModeCmd setModeCmd = new SetModeCmd();
---
+++ src/main/java/net/wurstclient/commands/SvCmd.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 - 2020 | Alexander01998 | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
+
+import net.minecraft.client.network.ServerInfo;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+import net.wurstclient.util.ChatUtils;
+import net.wurstclient.util.LastServerRememberer;
+
+public final class SvCmd extends Command
+{
+	public SvCmd()
+	{
+		super("sv", "Shows the version of the server\n"
+			+ "you are currently connected to.", ".sv");
+	}
+	
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		if(args.length != 0)
+			throw new CmdSyntaxError();
+		
+		ChatUtils.message("Server version: " + getVersion());
+	}
+	
+	private String getVersion() throws CmdError
+	{
+		if(MC.isIntegratedServerRunning())
+			throw new CmdError("Can't check server version in singleplayer.");
+		
+		ServerInfo lastServer = LastServerRememberer.getLastServer();
+		if(lastServer == null)
+			throw new IllegalStateException(
+				"LastServerRememberer doesn't remember the last server!");
+		
+		return lastServer.version;
+	}
+	
+	@Override
+	public String getPrimaryAction()
+	{
+		return "Get Server Version";
+	}
+	
+	@Override
+	public void doPrimaryAction()
+	{
+		WURST.getCmdProcessor().process("sv");
+	}
+}
