989f2e886ad801aa4f21b575693c1998f1a05d9b
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-15 21:34:30 +0100

    Improve components list

--- src/main/java/net/wurstclient/util/ForceOpDialog.java
+++ src/main/java/net/wurstclient/util/ForceOpDialog.java
@@ -7,6 +7,7 @@
  */
 package net.wurstclient.util;
 
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -30,7 +31,7 @@ public class ForceOpDialog extends JDialog
 		new ForceOpDialog();
 	}
 	
-	private final ArrayList<JComponent> components = new ArrayList<>();
+	private final ArrayList<Component> components = new ArrayList<>();
 	
 	public ForceOpDialog()
 	{
@@ -56,7 +57,6 @@ public class ForceOpDialog extends JDialog
 		sepListSpeed.setLocation(4, 56);
 		sepListSpeed.setSize(498, 4);
 		add(sepListSpeed);
-		components.add(sepListSpeed);
 		
 		addSpeedSection();
 		
@@ -64,7 +64,6 @@ public class ForceOpDialog extends JDialog
 		sepSpeedStart.setLocation(4, 132);
 		sepSpeedStart.setSize(498, 4);
 		add(sepSpeedStart);
-		components.add(sepSpeedStart);
 		
 		addStartSection();
 		
@@ -80,39 +79,33 @@ public class ForceOpDialog extends JDialog
 		lPWList.setLocation(4, 4);
 		lPWList.setSize(lPWList.getPreferredSize());
 		add(lPWList);
-		components.add(lPWList);
 		
 		JRadioButton rbDefaultList = new JRadioButton("default", true);
 		rbDefaultList.setLocation(4, 24);
 		rbDefaultList.setSize(rbDefaultList.getPreferredSize());
 		add(rbDefaultList);
-		components.add(rbDefaultList);
 		
 		JRadioButton rbTXTList = new JRadioButton("TXT file", false);
 		rbTXTList.setLocation(
 			rbDefaultList.getX() + rbDefaultList.getWidth() + 4, 24);
 		rbTXTList.setSize(rbTXTList.getPreferredSize());
 		add(rbTXTList);
-		components.add(rbTXTList);
 		
 		ButtonGroup bgList = new ButtonGroup();
 		bgList.add(rbDefaultList);
 		bgList.add(rbTXTList);
-		components.add(rbTXTList);
 		
 		JButton bTXTList = new JButton("browse");
 		bTXTList.setLocation(rbTXTList.getX() + rbTXTList.getWidth() + 4, 24);
 		bTXTList.setSize(bTXTList.getPreferredSize());
 		bTXTList.setEnabled(rbTXTList.isSelected());
 		add(bTXTList);
-		components.add(bTXTList);
 		
 		JButton bHowTo = new JButton("How to use");
 		bHowTo.setFont(new Font(bHowTo.getFont().getName(), Font.BOLD, 16));
 		bHowTo.setSize(bHowTo.getPreferredSize());
 		bHowTo.setLocation(506 - bHowTo.getWidth() - 32, 12);
 		add(bHowTo);
-		components.add(bHowTo);
 	}
 	
 	private void addSpeedSection()
@@ -121,13 +114,11 @@ public class ForceOpDialog extends JDialog
 		lSpeed.setLocation(4, 64);
 		lSpeed.setSize(lSpeed.getPreferredSize());
 		add(lSpeed);
-		components.add(lSpeed);
 		
 		JLabel lDelay1 = new JLabel("Delay between attempts:");
 		lDelay1.setLocation(4, 84);
 		lDelay1.setSize(lDelay1.getPreferredSize());
 		add(lDelay1);
-		components.add(lDelay1);
 		
 		JSpinner spDelay = new JSpinner();
 		spDelay.setToolTipText("<html>"
@@ -138,13 +129,11 @@ public class ForceOpDialog extends JDialog
 		spDelay.setLocation(lDelay1.getX() + lDelay1.getWidth() + 4, 84);
 		spDelay.setSize(60, (int)spDelay.getPreferredSize().getHeight());
 		add(spDelay);
-		components.add(spDelay);
 		
 		JLabel lDelay2 = new JLabel("ms");
 		lDelay2.setLocation(spDelay.getX() + spDelay.getWidth() + 4, 84);
 		lDelay2.setSize(lDelay2.getPreferredSize());
 		add(lDelay2);
-		components.add(lDelay2);
 		
 		JCheckBox cbDontWait = new JCheckBox(
 			"<html>Don't wait for \"<span style=\"color: red;\"><b>Wrong password!</b></span>\" messages</html>",
@@ -154,7 +143,6 @@ public class ForceOpDialog extends JDialog
 		cbDontWait.setLocation(4, 104);
 		cbDontWait.setSize(cbDontWait.getPreferredSize());
 		add(cbDontWait);
-		components.add(cbDontWait);
 	}
 	
 	private void addStartSection()
@@ -163,25 +151,21 @@ public class ForceOpDialog extends JDialog
 		lName.setLocation(4, 140);
 		lName.setSize(lName.getPreferredSize());
 		add(lName);
-		components.add(lName);
 		
 		JLabel lPasswords = new JLabel("Passwords: error");
 		lPasswords.setLocation(4, 160);
 		lPasswords.setSize(lPasswords.getPreferredSize());
 		add(lPasswords);
-		components.add(lPasswords);
 		
 		JLabel lTime = new JLabel("Estimated time: error");
 		lTime.setLocation(4, 180);
 		lTime.setSize(lTime.getPreferredSize());
 		add(lTime);
-		components.add(lTime);
 		
 		JLabel lAttempts = new JLabel("Attempts: error");
 		lAttempts.setLocation(4, 200);
 		lAttempts.setSize(lAttempts.getPreferredSize());
 		add(lAttempts);
-		components.add(lAttempts);
 		
 		JButton bStart = new JButton("Start");
 		bStart.setFont(new Font(bStart.getFont().getName(), Font.BOLD, 18));
@@ -189,7 +173,13 @@ public class ForceOpDialog extends JDialog
 		bStart.setSize(192, 66);
 		bStart.addActionListener(e -> startForceOP());
 		add(bStart);
-		components.add(bStart);
+	}
+	
+	@Override
+	public Component add(Component comp)
+	{
+		components.add(comp);
+		return super.add(comp);
 	}
 	
 	private void loadPWList()
