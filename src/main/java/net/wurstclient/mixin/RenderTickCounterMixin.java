f142f47c67d6bfad72c90209586f9bd746df77e0
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-19 19:41:33 +0100

    Fix RenderTickCounterMixin

--- src/main/java/net/wurstclient/mixin/RenderTickCounterMixin.java
+++ src/main/java/net/wurstclient/mixin/RenderTickCounterMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.render.RenderTickCounter;
 import net.wurstclient.WurstClient;
@@ -27,8 +27,9 @@ public abstract class RenderTickCounterMixin
 	@Inject(at = {@At(value = "FIELD",
 		target = "Lnet/minecraft/client/render/RenderTickCounter;prevTimeMillis:J",
 		opcode = Opcodes.PUTFIELD,
-		ordinal = 0)}, method = {"beginRenderTick(J)V"})
-	public void onBeginRenderTick(long long_1, CallbackInfo ci)
+		ordinal = 0)}, method = {"beginRenderTick(J)I"})
+	public void onBeginRenderTick(long long_1,
+		CallbackInfoReturnable<Integer> cir)
 	{
 		TimerHack timerHack = WurstClient.INSTANCE.getHax().timerHack;
 		lastFrameDuration *= timerHack.getTimerSpeed();
