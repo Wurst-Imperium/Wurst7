ced50c2deefe8bccf822077545dc6f9ea56eaad1
Alexander01998 <contact.wurstimperium@gmail.com>
2020-02-26 14:35:57 +0100

    Improve TooManyHaxHack description

--- src/main/java/net/wurstclient/hacks/TooManyHaxHack.java
+++ src/main/java/net/wurstclient/hacks/TooManyHaxHack.java
@@ -16,10 +16,12 @@ public final class TooManyHaxHack extends Hack
 {
 	public TooManyHaxHack()
 	{
-		super("TooManyHax", "Hides and disables features that you don't want.\n"
-			+ "For those who want to \"only hack a little bit\".\n\n"
-			+ "Use the \u00a7l.toomanyhax\u00a7r command to choose which features to hide.\n"
-			+ "Type \u00a7l.help toomanyhax\u00a7r for more info.");
+		super("TooManyHax",
+			"Hides and disables features that you don't want.\n"
+				+ "For those who want to \"only hack a little bit\".\n\n"
+				+ "Use the \u00a76.toomanyhax\u00a7r command to choose\n"
+				+ "which features to hide.\n"
+				+ "Type \u00a76.help toomanyhax\u00a7r for more info.");
 		setCategory(Category.OTHER);
 	}
 }
