a09739ea982cc1ec5fd943a0a36188421fb79afb
Alexander01998 <contact.wurstimperium@gmail.com>
2020-04-22 19:36:45 +0200

    Fix AltEditorScreen

--- src/main/java/net/wurstclient/altmanager/screens/AltEditorScreen.java
+++ src/main/java/net/wurstclient/altmanager/screens/AltEditorScreen.java
@@ -19,6 +19,8 @@ import org.lwjgl.opengl.GL11;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Util;
 import net.wurstclient.WurstClient;
@@ -50,33 +52,34 @@ public abstract class AltEditorScreen extends Screen
 	@Override
 	public final void init()
 	{
-		addButton(
-			doneButton = new ButtonWidget(width / 2 - 100, height / 4 + 72 + 12,
-				200, 20, getDoneButtonText(), b -> pressDoneButton()));
+		addButton(doneButton =
+			new ButtonWidget(width / 2 - 100, height / 4 + 72 + 12, 200, 20,
+				new LiteralText(getDoneButtonText()), b -> pressDoneButton()));
 		
 		addButton(new ButtonWidget(width / 2 - 100, height / 4 + 120 + 12, 200,
-			20, "Cancel", b -> client.openScreen(prevScreen)));
+			20, new LiteralText("Cancel"), b -> client.openScreen(prevScreen)));
 		
 		addButton(new ButtonWidget(width / 2 - 100, height / 4 + 96 + 12, 200,
-			20, "Random Name",
+			20, new LiteralText("Random Name"),
 			b -> emailBox.setText(NameGenerator.generateName())));
 		
 		addButton(stealSkinButton =
 			new ButtonWidget(width - (width / 2 - 100) / 2 - 64, height - 32,
-				128, 20, "Steal Skin", b -> message = stealSkin(getEmail())));
+				128, 20, new LiteralText("Steal Skin"),
+				b -> message = stealSkin(getEmail())));
 		
 		addButton(new ButtonWidget((width / 2 - 100) / 2 - 64, height - 32, 128,
-			20, "Open Skin Folder", b -> openSkinFolder()));
+			20, new LiteralText("Open Skin Folder"), b -> openSkinFolder()));
 		
-		emailBox =
-			new TextFieldWidget(textRenderer, width / 2 - 100, 60, 200, 20, "");
+		emailBox = new TextFieldWidget(textRenderer, width / 2 - 100, 60, 200,
+			20, new LiteralText(""));
 		emailBox.setMaxLength(48);
 		emailBox.setSelected(true);
 		emailBox.setText(getDefaultEmail());
 		children.add(emailBox);
 		
 		passwordBox = new TextFieldWidget(textRenderer, width / 2 - 100, 100,
-			200, 20, "");
+			200, 20, new LiteralText(""));
 		passwordBox.setText(getDefaultPassword());
 		passwordBox.setRenderTextProvider((text, int_1) -> {
 			String stars = "";
@@ -196,9 +199,10 @@ public abstract class AltEditorScreen extends Screen
 	}
 	
 	@Override
-	public void render(int mouseX, int mouseY, float partialTicks)
+	public void render(MatrixStack matrixStack, int mouseX, int mouseY,
+		float partialTicks)
 	{
-		renderBackground();
+		renderBackground(matrixStack);
 		
 		// skin preview
 		AltRenderer.drawAltBack(emailBox.getText(), (width / 2 - 100) / 2 - 64,
@@ -207,14 +211,16 @@ public abstract class AltEditorScreen extends Screen
 			width - (width / 2 - 100) / 2 - 64, height / 2 - 128, 128, 256);
 		
 		// text
-		drawString(textRenderer, "Name or E-Mail", width / 2 - 100, 47,
+		drawString(matrixStack, textRenderer, "Name or E-Mail", width / 2 - 100,
+			47, 10526880);
+		drawString(matrixStack, textRenderer, "Password", width / 2 - 100, 87,
 			10526880);
-		drawString(textRenderer, "Password", width / 2 - 100, 87, 10526880);
-		drawCenteredString(textRenderer, message, width / 2, 142, 16777215);
+		drawCenteredString(matrixStack, textRenderer, message, width / 2, 142,
+			16777215);
 		
 		// text boxes
-		emailBox.render(mouseX, mouseY, partialTicks);
-		passwordBox.render(mouseX, mouseY, partialTicks);
+		emailBox.render(matrixStack, mouseX, mouseY, partialTicks);
+		passwordBox.render(matrixStack, mouseX, mouseY, partialTicks);
 		
 		// red flash for errors
 		if(errorTimer > 0)
@@ -240,6 +246,6 @@ public abstract class AltEditorScreen extends Screen
 			errorTimer--;
 		}
 		
-		super.render(mouseX, mouseY, partialTicks);
+		super.render(matrixStack, mouseX, mouseY, partialTicks);
 	}
 }
