46748d6f863cf0f8b7b87857e7d2c329502dc997
Tyler <22119302+novo52@users.noreply.github.com>
2020-05-21 21:03:07 -0600

    trying to fix whatever change broke freecam

--- src/main/java/net/wurstclient/hack/Hack.java
+++ src/main/java/net/wurstclient/hack/Hack.java
@@ -19,105 +19,99 @@ public abstract class Hack extends Feature
 	private final String name;
 	private final String description;
 	private Category category;
-	
+
 	private boolean enabled;
 	private final boolean stateSaved =
-		!getClass().isAnnotationPresent(DontSaveState.class);
+			!getClass().isAnnotationPresent(DontSaveState.class);
 
 	public Hack(String name, String description)
 	{
-		this(name, description, true);
-	}
-
-	public Hack(String name, String description, boolean addKeybind)
-	{
 		this.name = Objects.requireNonNull(name);
 		this.description = Objects.requireNonNull(description);
-		if (addKeybind)
-			addPossibleKeybind(name, "Toggle " + name);
+		addPossibleKeybind(name, "Toggle " + name);
 	}
-	
+
 	@Override
 	public final String getName()
 	{
 		return name;
 	}
-	
+
 	public String getRenderName()
 	{
 		return name;
 	}
-	
+
 	@Override
 	public final String getDescription()
 	{
 		return description;
 	}
-	
+
 	@Override
 	public final Category getCategory()
 	{
 		return category;
 	}
-	
+
 	protected final void setCategory(Category category)
 	{
 		this.category = category;
 	}
-	
+
 	@Override
 	public final boolean isEnabled()
 	{
 		return enabled;
 	}
-	
+
 	public final void setEnabled(boolean enabled)
 	{
 		if(this.enabled == enabled)
 			return;
-		
+
 		TooManyHaxHack tooManyHax = WURST.getHax().tooManyHaxHack;
 		if(enabled && tooManyHax.isEnabled() && tooManyHax.isBlocked(this))
 			return;
-		
+
 		this.enabled = enabled;
-		
+
 		if(!(this instanceof NavigatorHack))
 			WURST.getHud().getHackList().updateState(this);
-		
+
 		if(enabled)
 			onEnable();
 		else
 			onDisable();
-		
+
 		if(stateSaved)
 			WURST.getHax().saveEnabledHax();
 	}
-	
+
 	@Override
-	public String getPrimaryAction()
+	public final String getPrimaryAction()
 	{
 		return enabled ? "Disable" : "Enable";
 	}
-	
+
 	@Override
-	public void doPrimaryAction()
+	public final void doPrimaryAction()
 	{
 		setEnabled(!enabled);
 	}
-	
+
 	public final boolean isStateSaved()
 	{
 		return stateSaved;
 	}
-	
+
 	protected void onEnable()
 	{
-		
+
 	}
-	
+
 	protected void onDisable()
 	{
-		
+
 	}
 }
