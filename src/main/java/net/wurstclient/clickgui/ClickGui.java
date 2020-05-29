5410e2ea79035dd7729a2f1afff04aacaf35fd5b
Alexander01998 <contact.wurstimperium@gmail.com>
2020-05-29 20:19:18 +0200

    Fix ClickGui

--- src/main/java/net/wurstclient/clickgui/ClickGui.java
+++ src/main/java/net/wurstclient/clickgui/ClickGui.java
@@ -27,6 +27,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.wurstclient.Category;
 import net.wurstclient.Feature;
 import net.wurstclient.WurstClient;
@@ -839,7 +840,7 @@ public final class ClickGui
 		GL11.glColor4f(1, 1, 1, 1);
 		TextRenderer fr = MC.textRenderer;
 		String title =
-			fr.trimToWidth(new LiteralText(window.getTitle()), x3 - x1)
+			((Text)fr.trimToWidth(new LiteralText(window.getTitle()), x3 - x1))
 				.getString();
 		fr.draw(matrixStack, title, x1 + 2, y1 + 3, 0xf0f0f0);
 		GL11.glEnable(GL11.GL_BLEND);
