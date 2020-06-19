6571fc8ba7affe0aa50107f3197266894c08e9b5
Mersid <mersid_email@yahoo.com>
2020-06-19 15:47:54 -0700

    Update HealthTagsHack.java

--- src/main/java/net/wurstclient/hacks/HealthTagsHack.java
+++ src/main/java/net/wurstclient/hacks/HealthTagsHack.java
@@ -11,13 +11,18 @@ import net.minecraft.entity.LivingEntity;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
 
 @SearchTags({"health tags"})
 public final class HealthTagsHack extends Hack
 {
+	CheckboxSetting showMaxHealth = new CheckboxSetting("Show max health", "Shows players' max health.", false);
+	CheckboxSetting colorByPercentage = new CheckboxSetting("Color by percentage", "Color health display based on percentage of max health instead of fixed values.", false);
+
 	public HealthTagsHack()
 	{
 		super("HealthTags", "Shows the health of players in their nametags.");
+
 		setCategory(Category.RENDER);
 	}
 	
@@ -25,22 +30,50 @@ public final class HealthTagsHack extends Hack
 	{
 		if(!isEnabled())
 			return nametag;
-		
-		int health = (int)entity.getHealth();
-		return nametag + " " + getColor(health) + health;
+
+		float health = entity.getHealth();
+		float maxHealth = entity.getMaximumHealth();
+
+		if (showMaxHealth.isChecked())
+			return nametag + " " + getColor(health, maxHealth) + (int)health + " " + (int)maxHealth;
+
+		return nametag + " " + getColor(health, maxHealth) + health;
 	}
 	
-	private String getColor(int health)
+	private String getColor(float health, float maxHealth)
 	{
-		if(health <= 5)
-			return "\u00a74";
-		
-		if(health <= 10)
-			return "\u00a76";
-		
-		if(health <= 15)
-			return "\u00a7e";
-		
-		return "\u00a7a";
+		final String colorDarkRed = "\u00a74";
+		final String colorGold = "\u00a76";
+		final String colorYellow = "\u00a7e";
+		final String colorGreen = "\u00a7a";
+
+		if (colorByPercentage.isChecked())
+		{
+			float percentage = health / maxHealth;
+
+			if (percentage <= 0.25)
+				return colorDarkRed;
+
+			if (percentage <= 0.5)
+				return colorGold;
+
+			if (percentage <= 0.75)
+				return colorYellow;
+
+			return colorGreen;
+		}
+		else
+		{
+			if (health <= 5)
+				return "\u00a74";
+
+			if (health <= 10)
+				return "\u00a76";
+
+			if (health <= 15)
+				return "\u00a7e";
+
+			return "\u00a7a";
+		}
 	}
 }
