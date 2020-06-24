92f637f21d5436ee9b7af68967d2c8c4883fafe5
Alexander01998 <contact.wurstimperium@gmail.com>
2020-06-24 17:13:23 +0200

    Clean up GoogleTranslateService

--- src/main/java/net/wurstclient/util/GoogleTranslateService.java
+++ src/main/java/net/wurstclient/util/GoogleTranslateService.java
@@ -8,9 +8,12 @@
 package net.wurstclient.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,64 +22,84 @@ import org.apache.commons.lang3.StringEscapeUtils;
 
 public class GoogleTranslateService
 {
-	public String translate(String textToTranslate, String translateFrom,
-		String translateTo) throws Exception
+	public String translate(String text, String langFrom, String langTo)
 	{
+		String html = getHTML(text, langFrom, langTo);
+		String translated = parseHTML(html);
+		
+		if(text.equalsIgnoreCase(translated))
+			return null;
+		
+		return translated;
+	}
+	
+	private String getHTML(String text, String langFrom, String langTo)
+	{
+		URL url = createURL(text, langFrom, langTo);
+		
 		try
 		{
-			String pageSource =
-				getPageSource(textToTranslate, translateFrom, translateTo);
-			final String regex = "class=\"t0\">([^<]*)<\\/div>";
-			final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-			final Matcher matcher = pattern.matcher(pageSource);
-			matcher.find();
-			String match = matcher.group(1);
-			if(match != null && !match.isEmpty() && !StringEscapeUtils
-				.unescapeHtml4(match).contains(textToTranslate))
-				return match;
-			else
-				return null;
-		}catch(Exception e)
+			URLConnection connection = setupConnection(url);
+			
+			try(BufferedReader br = new BufferedReader(
+				new InputStreamReader(connection.getInputStream(), "UTF-8")))
+			{
+				StringBuilder html = new StringBuilder();
+				
+				String line;
+				while((line = br.readLine()) != null)
+					html.append(line + "\n");
+				
+				return html.toString();
+			}
+			
+		}catch(IOException e)
 		{
 			return null;
 		}
 	}
 	
-	private static String getPageSource(String textToTranslate,
-		String translateFrom, String translateTo) throws Exception
+	private URL createURL(String text, String langFrom, String langTo)
 	{
-		String pageUrl = String.format(
-			"https://translate.google.com/m?hl=en&sl=%s&tl=%s&ie=UTF-8&prev=_m&q=%s",
-			translateFrom, translateTo,
-			URLEncoder.encode(textToTranslate.trim(), "UTF-8"));
-		URL url = new URL(pageUrl);
-		HttpURLConnection connection = null;
-		BufferedReader bufferedReader = null;
-		StringBuilder pageSource = new StringBuilder();
 		try
 		{
-			connection = (HttpURLConnection)url.openConnection();
-			connection.setConnectTimeout(5000);
-			connection.setRequestProperty("User-Agent",
-				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-			bufferedReader = new BufferedReader(
-				new InputStreamReader(connection.getInputStream(), "UTF-8"));
-			String line;
-			while((line = bufferedReader.readLine()) != null)
-				pageSource.append(line + System.lineSeparator());
-			String clean = pageSource.toString();
-			return clean;
-		}catch(Exception e)
+			String encodedText = URLEncoder.encode(text.trim(), "UTF-8");
+			
+			String urlString = String.format(
+				"https://translate.google.com/m?hl=en&sl=%s&tl=%s&ie=UTF-8&prev=_m&q=%s",
+				langFrom, langTo, encodedText);
+			
+			return new URL(urlString);
+			
+		}catch(MalformedURLException | UnsupportedEncodingException e)
 		{
-			// e.printStackTrace();
-		}finally
-		{
-			if(connection != null)
-				connection.disconnect();
-			if(bufferedReader != null)
-				bufferedReader.close();
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 	
+	private URLConnection setupConnection(URL url) throws IOException
+	{
+		URLConnection connection = url.openConnection();
+		
+		connection.setConnectTimeout(5000);
+		connection.setRequestProperty("User-Agent",
+			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		
+		return connection;
+	}
+	
+	private String parseHTML(String html)
+	{
+		String regex = "class=\"t0\">([^<]*)<\\/div>";
+		Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		
+		Matcher matcher = pattern.matcher(html);
+		matcher.find();
+		String match = matcher.group(1);
+		
+		if(match == null || match.isEmpty())
+			return null;
+		
+		return StringEscapeUtils.unescapeHtml4(match);
+	}
 }
