3db9f9b3424cdcd0de37265e7d5d79a29a6f7515
Alexander01998 <contact.wurstimperium@gmail.com>
2020-06-27 18:14:29 +0200

    Manual cleanup

--- src/main/java/net/wurstclient/hacks/MountBypassHack.java
+++ src/main/java/net/wurstclient/hacks/MountBypassHack.java
@@ -1,22 +1,29 @@
+/*
+ * Copyright (C) 2014 - 2020 | Alexander01998 | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
 package net.wurstclient.hacks;
 
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket.InteractionType;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.PacketOutputListener;
 import net.wurstclient.hack.Hack;
 
 @SearchTags({"mount bypass", "donkey chest"})
-
 public final class MountBypassHack extends Hack implements PacketOutputListener
 {
 	public MountBypassHack()
 	{
 		super("MountBypass",
-			"Allows you to mount chests on donkeys\n"
-				+ "on servers that disable it, allowing\n"
-				+ "donkey chest duplication glitch.");
+			"Allows you to mount chests on donkeys, llamas\n"
+				+ "and mules on servers that disable it,\n"
+				+ "allowing for a donkey chest duplication glitch.");
 		setCategory(Category.OTHER);
 	}
 	
@@ -35,14 +42,18 @@ public final class MountBypassHack extends Hack implements PacketOutputListener
 	@Override
 	public void onSentPacket(PacketOutputEvent event)
 	{
-		if(event.getPacket() instanceof PlayerInteractEntityC2SPacket)
-		{
-			PlayerInteractEntityC2SPacket packet =
-				(PlayerInteractEntityC2SPacket)event.getPacket();
-			if(packet.getEntity(MC.world) instanceof AbstractDonkeyEntity)
-				if(packet
-					.getType() == PlayerInteractEntityC2SPacket.InteractionType.INTERACT_AT)
-					event.cancel();
-		}
+		if(!(event.getPacket() instanceof PlayerInteractEntityC2SPacket))
+			return;
+		
+		PlayerInteractEntityC2SPacket packet =
+			(PlayerInteractEntityC2SPacket)event.getPacket();
+		
+		if(!(packet.getEntity(MC.world) instanceof AbstractDonkeyEntity))
+			return;
+		
+		if(packet.getType() != InteractionType.INTERACT_AT)
+			return;
+		
+		event.cancel();
 	}
 }
