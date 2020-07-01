d45087e794bb85c59e48ac462283503ec7ad1e05
Codedot <48249438+code1dot@users.noreply.github.com>
2020-07-01 16:25:17 -0400

    Add criticals hack support

--- src/main/java/net/wurstclient/hacks/MultiAuraHack.java
+++ src/main/java/net/wurstclient/hacks/MultiAuraHack.java
@@ -230,6 +230,7 @@ public final class MultiAuraHack extends Hack implements UpdateListener
 		// attack entities
 		for(Entity entity : entities)
 		{
+			if (WURST.getHax().criticalsHack.isEnabled()) WURST.getHax().criticalsHack.doCritical()
 			RotationUtils.Rotation rotations = RotationUtils
 				.getNeededRotations(entity.getBoundingBox().getCenter());
 			
