cc5e374c144a60664124342ce7caeb6da5755485
Alexander01998 <contact.wurstimperium@gmail.com>
2020-07-08 21:10:43 +0200

    Update to 20w28a

--- src/main/java/net/wurstclient/altmanager/screens/AltEditorScreen.java
+++ src/main/java/net/wurstclient/altmanager/screens/AltEditorScreen.java
@@ -30,6 +30,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.StringRenderable;
 import net.minecraft.text.Text;
 import net.minecraft.util.Util;
 import net.wurstclient.WurstClient;
@@ -94,7 +95,7 @@ public abstract class AltEditorScreen extends Screen
 			String stars = "";
 			for(int i = 0; i < text.length(); i++)
 				stars += "*";
-			return stars;
+			return StringRenderable.plain(stars);
 		});
 		children.add(passwordBox);
 		
