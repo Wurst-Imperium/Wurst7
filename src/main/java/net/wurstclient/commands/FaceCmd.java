d3f6ba529081f5ec26e232fc7df5f8ac5297e26d
Jakob Kirsch <jakob.kirsch@web.de>
2020-07-16 20:17:19 +0200

    Add .face command and improve BoatFly

---
+++ src/main/java/net/wurstclient/commands/FaceCmd.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2014 - 2020 | Jakiki6 | All rights reserved.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.commands;
+
+import java.util.Comparator;
+import java.util.stream.StreamSupport;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.wurstclient.command.CmdError;
+import net.wurstclient.command.CmdException;
+import net.wurstclient.command.CmdSyntaxError;
+import net.wurstclient.command.Command;
+import net.wurstclient.util.FakePlayerEntity;
+import net.wurstclient.util.MathUtils;
+import net.wurstclient.util.RotationUtils;
+import net.wurstclient.util.RotationUtils.Rotation;
+
+public final class FaceCmd extends Command
+{
+	public FaceCmd()
+	{
+		super("face", "Rotates you to a block", ".face <x> <y> <z>",
+			".face <entity>");
+	}
+	
+	@Override
+	public void call(String[] args) throws CmdException
+	{
+		Rotation pos = argsToPos(args);
+		
+		ClientPlayerEntity player = MC.player;
+		player.yaw = pos.getYaw();
+		player.pitch = pos.getPitch();
+	}
+	
+	private Rotation argsToPos(String... args) throws CmdException
+	{
+		switch(args.length)
+		{
+			default:
+			throw new CmdSyntaxError("Invalid coordinates.");
+			
+			case 1:
+			return argsToEntityPos(args[0]);
+			
+			case 3:
+			return argsToXyzPos(args);
+		}
+	}
+	
+	private Rotation argsToEntityPos(String name) throws CmdError
+	{
+		LivingEntity entity = StreamSupport
+			.stream(MC.world.getEntities().spliterator(), true)
+			.filter(e -> e instanceof LivingEntity).map(e -> (LivingEntity)e)
+			.filter(e -> !e.removed && e.getHealth() > 0)
+			.filter(e -> e != MC.player)
+			.filter(e -> !(e instanceof FakePlayerEntity))
+			.filter(e -> name.equalsIgnoreCase(e.getDisplayName().asString()))
+			.min(
+				Comparator.comparingDouble(e -> MC.player.squaredDistanceTo(e)))
+			.orElse(null);
+		
+		if(entity == null)
+			throw new CmdError("Entity \"" + name + "\" could not be found.");
+		
+		BlockPos pos = new BlockPos(entity);
+		return RotationUtils.getNeededRotations(new Vec3d(pos));
+	}
+	
+	private Rotation argsToXyzPos(String... xyz) throws CmdSyntaxError
+	{
+		BlockPos playerPos = new BlockPos(MC.player);
+		int[] player =
+			new int[]{playerPos.getX(), playerPos.getY(), playerPos.getZ()};
+		int[] pos = new int[3];
+		
+		for(int i = 0; i < 3; i++)
+			if(MathUtils.isInteger(xyz[i]))
+				pos[i] = Integer.parseInt(xyz[i]);
+			else if(xyz[i].equals("~"))
+				pos[i] = player[i];
+			else if(xyz[i].startsWith("~")
+				&& MathUtils.isInteger(xyz[i].substring(1)))
+				pos[i] = player[i] + Integer.parseInt(xyz[i].substring(1));
+			else
+				throw new CmdSyntaxError("Invalid coordinates.");
+			
+		return RotationUtils.getNeededRotations(new Vec3d(pos[0], pos[1], pos[2]));
+	}
+}
--- src/main/java/net/wurstclient/hacks/BoatFlyHack.java
+++ src/main/java/net/wurstclient/hacks/BoatFlyHack.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 - 2020 | Alexander01998 | All rights reserved.
+ * Copyright (C) 2014 - 2020 | Alexander01998 | Jakiki6 | All rights reserved.
  *
  * This source code is subject to the terms of the GNU General Public
  * License, version 3. If a copy of the GPL was not distributed with this
@@ -9,18 +9,35 @@ package net.wurstclient.hacks;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.entity.MovementType;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
+import net.wurstclient.settings.CheckboxSetting;
 
 @SearchTags({"BoatFlight", "boat fly", "boat flight"})
 public final class BoatFlyHack extends Hack implements UpdateListener
 {
+	public final SliderSetting speed_up =
+		new SliderSetting("Speed up", 1, 0.05, 4, 0.05, ValueDisplay.DECIMAL);
+	public final SliderSetting speed_dir =
+                new SliderSetting("Speed diagonal", 5, 0.05, 32, 0.05, ValueDisplay.DECIMAL);
+	public final SliderSetting speed_down =
+                new SliderSetting("Speed down", 0.0005, -1, 1, 0.0005, ValueDisplay.DECIMAL);
+	public final CheckboxSetting out_of_v =
+		new CheckboxSetting("Without boat", "Hack without any vehicle", false);
+
 	public BoatFlyHack()
 	{
 		super("BoatFly", "Allows you to fly with boats");
 		setCategory(Category.MOVEMENT);
+		addSetting(speed_up);
+		addSetting(speed_dir);
+		addSetting(speed_down);
+		addSetting(out_of_v);
 	}
 	
 	@Override
@@ -39,13 +56,20 @@ public final class BoatFlyHack extends Hack implements UpdateListener
 	public void onUpdate()
 	{
 		// check if riding
-		if(!MC.player.hasVehicle())
+		if(!MC.player.hasVehicle() && !out_of_v.isChecked())
 			return;
-		
 		// fly
 		Entity vehicle = MC.player.getVehicle();
-		Vec3d velocity = vehicle.getVelocity();
-		double motionY = MC.options.keyJump.isPressed() ? 0.3 : 0;
-		vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+		if (vehicle == null)
+			vehicle = MC.player;
+		double maxSpeed = speed_dir.getValue();
+		Vec3d v = vehicle.getVelocity();
+
+		double add = MC.player.age % 2 == 0 ? v.y: -v.y;
+		v = new Vec3d(v.x * maxSpeed, MC.options.keyJump.isPressed() ? speed_up.getValue() : (MC.player.age % 2 == 0 ? speed_down.getValue() : -speed_down.getValue()), v.z * maxSpeed);
+		vehicle.move(MovementType.SELF, v);
+		v = vehicle.getVelocity();
+		vehicle.setVelocity(new Vec3d(v.x, 0, v.z));
+
 	}
 }
