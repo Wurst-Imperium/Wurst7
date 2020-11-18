dfebb6bf9f820c5d101c156a91b67f0ebe1341d9
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2020-11-17 23:32:09 -0800

    Added NoFog hack

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -116,6 +116,7 @@ public final class HackList implements UpdateListener
 	public final NoClipHack noClipHack = new NoClipHack();
 	public final NoFallHack noFallHack = new NoFallHack();
 	public final NoFireOverlayHack noFireOverlayHack = new NoFireOverlayHack();
+	public final NoFogHack noFogHack = new NoFogHack();
 	public final NoHurtcamHack noHurtcamHack = new NoHurtcamHack();
 	public final NoOverlayHack noOverlayHack = new NoOverlayHack();
 	public final NoPumpkinHack noPumpkinHack = new NoPumpkinHack();
--- src/main/java/net/wurstclient/mixin/BackgroundRendererMixin.java
+++ src/main/java/net/wurstclient/mixin/BackgroundRendererMixin.java
@@ -9,9 +9,12 @@ package net.wurstclient.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
@@ -48,4 +51,16 @@ public class BackgroundRendererMixin
 		
 		return entity.hasStatusEffect(effect);
 	}
+	
+	@Inject(at = {@At("HEAD")},
+		method = {
+			"applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer$FogType;FZ)V"},
+		cancellable = true)
+	private static void changeLookDirection(Camera c,
+		BackgroundRenderer.FogType ft, float f, boolean b, CallbackInfo ci)
+	{
+		if(WurstClient.INSTANCE.getHax().noFogHack.isEnabled())
+			ci.cancel();
+	}
+	
 }
