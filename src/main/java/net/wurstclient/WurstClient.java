78a1e5960d46f83365c8f3774f7ec425b366aa13
Alexander01998 <contact.wurstimperium@gmail.com>
2020-11-27 11:38:13 +0100

    Change version to 7.9

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -58,7 +58,7 @@ public enum WurstClient
 	public static final MinecraftClient MC = MinecraftClient.getInstance();
 	public static final IMinecraftClient IMC = (IMinecraftClient)MC;
 	
-	public static final String VERSION = "7.8";
+	public static final String VERSION = "7.9";
 	public static final String MC_VERSION = "1.16.4";
 	
 	private WurstAnalytics analytics;
