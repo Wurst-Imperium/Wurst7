e234f26d98d0e530987fbd49ddb3d7b2f4ccf0db
Alexander01998 <contact.wurstimperium@gmail.com>
2021-01-13 02:39:48 +0100

    Fix AutoSprintHack not working in rain

--- src/main/java/net/wurstclient/hacks/AutoSprintHack.java
+++ src/main/java/net/wurstclient/hacks/AutoSprintHack.java
@@ -42,7 +42,7 @@ public final class AutoSprintHack extends Hack implements UpdateListener
 		if(player.horizontalCollision || player.isSneaking())
 			return;
 		
-		if(player.isWet() || player.isSubmergedInWater())
+		if(player.isInsideWaterOrBubbleColumn() || player.isSubmergedInWater())
 			return;
 		
 		if(player.forwardSpeed > 0)
