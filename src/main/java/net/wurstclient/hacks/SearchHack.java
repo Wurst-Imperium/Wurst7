e314dc1aa05ade179f31d892fc9452f2da0d5079
Bram Ceulemans <bramceulemans@me.com>
2021-01-20 17:18:27 +0100

    Add BlockListSetting to SearchHack

--- src/main/java/net/wurstclient/hacks/SearchHack.java
+++ src/main/java/net/wurstclient/hacks/SearchHack.java
@@ -7,23 +7,6 @@
  */
 package net.wurstclient.hacks;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
-import java.util.stream.Collectors;
-
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.block.Block;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
@@ -41,174 +24,165 @@ import net.wurstclient.events.PacketInputListener;
 import net.wurstclient.events.RenderListener;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
-import net.wurstclient.settings.BlockSetting;
+import net.wurstclient.settings.BlockListSetting;
 import net.wurstclient.settings.EnumSetting;
 import net.wurstclient.settings.SliderSetting;
-import net.wurstclient.util.BlockVertexCompiler;
-import net.wurstclient.util.ChatUtils;
-import net.wurstclient.util.ChunkSearcher;
-import net.wurstclient.util.MinPriorityThreadFactory;
-import net.wurstclient.util.RenderUtils;
-import net.wurstclient.util.RotationUtils;
+import net.wurstclient.util.*;
+import org.lwjgl.opengl.GL11;
+
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public final class SearchHack extends Hack
-	implements UpdateListener, PacketInputListener, RenderListener
-{
-	private final BlockSetting block = new BlockSetting("Block",
-		"The type of block to search for.", "minecraft:diamond_ore", false);
-	
+		implements UpdateListener, PacketInputListener, RenderListener {
+	private final BlockListSetting block = new BlockListSetting("Block",
+			"The type of block to search for.",
+			"minecraft:diamond_ore", "minecraft:nether_portal");
+
 	private final EnumSetting<Area> area = new EnumSetting<>("Area",
-		"The area around the player to search in.\n"
-			+ "Higher values require a faster computer.",
-		Area.values(), Area.D11);
-	
+			"The area around the player to search in.\n"
+					+ "Higher values require a faster computer.",
+			Area.values(), Area.D11);
+
 	private final SliderSetting limit = new SliderSetting("Limit",
-		"The maximum number of blocks to display.\n"
-			+ "Higher values require a faster computer.",
-		4, 3, 6, 1,
-		v -> new DecimalFormat("##,###,###").format(Math.pow(10, v)));
+			"The maximum number of blocks to display.\n"
+					+ "Higher values require a faster computer.",
+			4, 3, 6, 1,
+			v -> new DecimalFormat("##,###,###").format(Math.pow(10, v)));
+	private final HashMap<Chunk, List<ChunkSearcher>> searchers = new HashMap<>();
+	private final Set<Chunk> chunksToUpdate =
+			Collections.synchronizedSet(new HashSet<>());
 	private int prevLimit;
 	private boolean notify;
-	
-	private final HashMap<Chunk, ChunkSearcher> searchers = new HashMap<>();
-	private final Set<Chunk> chunksToUpdate =
-		Collections.synchronizedSet(new HashSet<>());
 	private ExecutorService pool1;
-	
+
 	private ForkJoinPool pool2;
 	private ForkJoinTask<HashSet<BlockPos>> getMatchingBlocksTask;
 	private ForkJoinTask<ArrayList<int[]>> compileVerticesTask;
-	
+
 	private int displayList;
 	private boolean displayListUpToDate;
-	
-	public SearchHack()
-	{
+
+	public SearchHack() {
 		super("Search", "Helps you to find specific blocks by\n"
-			+ "highlighting them in rainbow color.");
+				+ "highlighting them in rainbow color.");
 		setCategory(Category.RENDER);
 		addSetting(block);
 		addSetting(area);
 		addSetting(limit);
 	}
-	
+
 	@Override
-	public String getRenderName()
-	{
-		return getName() + " [" + block.getBlockName().replace("minecraft:", "")
-			+ "]";
+	public String getRenderName() {
+		return getName();
 	}
-	
+
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		prevLimit = limit.getValueI();
 		notify = true;
-		
+
 		pool1 = MinPriorityThreadFactory.newFixedThreadPool();
 		pool2 = new ForkJoinPool();
-		
+
 		displayList = GL11.glGenLists(1);
 		displayListUpToDate = false;
-		
+
 		EVENTS.add(UpdateListener.class, this);
 		EVENTS.add(PacketInputListener.class, this);
 		EVENTS.add(RenderListener.class, this);
 	}
-	
+
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		EVENTS.remove(UpdateListener.class, this);
 		EVENTS.remove(PacketInputListener.class, this);
 		EVENTS.remove(RenderListener.class, this);
-		
+
 		stopPool2Tasks();
 		pool1.shutdownNow();
 		pool2.shutdownNow();
 		GL11.glDeleteLists(displayList, 1);
 		chunksToUpdate.clear();
 	}
-	
+
 	@Override
-	public void onReceivedPacket(PacketInputEvent event)
-	{
+	public void onReceivedPacket(PacketInputEvent event) {
 		ClientPlayerEntity player = MC.player;
 		ClientWorld world = MC.world;
-		if(player == null || world == null)
+		if (player == null || world == null)
 			return;
-		
+
 		Packet<?> packet = event.getPacket();
 		Chunk chunk;
-		
-		if(packet instanceof BlockUpdateS2CPacket)
-		{
-			BlockUpdateS2CPacket change = (BlockUpdateS2CPacket)packet;
+
+		if (packet instanceof BlockUpdateS2CPacket) {
+			BlockUpdateS2CPacket change = (BlockUpdateS2CPacket) packet;
 			BlockPos pos = change.getPos();
 			chunk = world.getChunk(pos);
-			
-		}else if(packet instanceof ChunkDeltaUpdateS2CPacket)
-		{
+
+		} else if (packet instanceof ChunkDeltaUpdateS2CPacket) {
 			ChunkDeltaUpdateS2CPacket change =
-				(ChunkDeltaUpdateS2CPacket)packet;
-			
+					(ChunkDeltaUpdateS2CPacket) packet;
+
 			ArrayList<BlockPos> changedBlocks = new ArrayList<>();
 			change.visitUpdates((pos, state) -> changedBlocks.add(pos));
-			if(changedBlocks.isEmpty())
+			if (changedBlocks.isEmpty())
 				return;
-			
+
 			chunk = world.getChunk(changedBlocks.get(0));
-			
-		}else if(packet instanceof ChunkDataS2CPacket)
-		{
-			ChunkDataS2CPacket chunkData = (ChunkDataS2CPacket)packet;
+
+		} else if (packet instanceof ChunkDataS2CPacket) {
+			ChunkDataS2CPacket chunkData = (ChunkDataS2CPacket) packet;
 			chunk = world.getChunk(chunkData.getX(), chunkData.getZ());
-			
-		}else
+
+		} else
 			return;
-		
+
 		chunksToUpdate.add(chunk);
 	}
-	
+
 	@Override
-	public void onUpdate()
-	{
-		Block currentBlock = block.getBlock();
+	public void onUpdate() {
+		List<Block> currentBlocks = block.getBlockNames().stream()
+				.map(BlockUtils::getBlockFromName)
+				.collect(Collectors.toList());
 		BlockPos eyesPos = new BlockPos(RotationUtils.getEyesPos());
-		
+
 		ChunkPos center = getPlayerChunkPos(eyesPos);
 		int range = area.getSelected().chunkRange;
 		int dimensionId = MC.world.getRegistryKey().toString().hashCode();
-		
-		addSearchersInRange(center, range, currentBlock, dimensionId);
+
+		addSearchersInRange(center, range, currentBlocks, dimensionId);
 		removeSearchersOutOfRange(center, range);
-		replaceSearchersWithDifferences(currentBlock, dimensionId);
-		replaceSearchersWithChunkUpdate(currentBlock, dimensionId);
-		
-		if(!areAllChunkSearchersDone())
+		replaceSearchersWithDifferences(center, range, currentBlocks, dimensionId);
+		replaceSearchersWithChunkUpdate(currentBlocks, dimensionId);
+
+		if (!areAllChunkSearchersDone())
 			return;
-		
+
 		checkIfLimitChanged();
-		
-		if(getMatchingBlocksTask == null)
+
+		if (getMatchingBlocksTask == null)
 			startGetMatchingBlocksTask(eyesPos);
-		
-		if(!getMatchingBlocksTask.isDone())
+
+		if (!getMatchingBlocksTask.isDone())
 			return;
-		
-		if(compileVerticesTask == null)
+
+		if (compileVerticesTask == null)
 			startCompileVerticesTask();
-		
-		if(!compileVerticesTask.isDone())
+
+		if (!compileVerticesTask.isDone())
 			return;
-		
-		if(!displayListUpToDate)
+
+		if (!displayListUpToDate)
 			setDisplayListFromTask();
 	}
-	
+
 	@Override
-	public void onRender(float partialTicks)
-	{
+	public void onRender(float partialTicks) {
 		// GL settings
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
@@ -218,25 +192,25 @@ public final class SearchHack extends Hack
 		GL11.glEnable(GL11.GL_CULL_FACE);
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
 		GL11.glDisable(GL11.GL_LIGHTING);
-		
+
 		GL11.glPushMatrix();
 		RenderUtils.applyRenderOffset();
-		
+
 		// generate rainbow color
 		float x = System.currentTimeMillis() % 2000 / 1000F;
-		float red = 0.5F + 0.5F * MathHelper.sin(x * (float)Math.PI);
+		float red = 0.5F + 0.5F * MathHelper.sin(x * (float) Math.PI);
 		float green =
-			0.5F + 0.5F * MathHelper.sin((x + 4F / 3F) * (float)Math.PI);
+				0.5F + 0.5F * MathHelper.sin((x + 4F / 3F) * (float) Math.PI);
 		float blue =
-			0.5F + 0.5F * MathHelper.sin((x + 8F / 3F) * (float)Math.PI);
-		
+				0.5F + 0.5F * MathHelper.sin((x + 8F / 3F) * (float) Math.PI);
+
 		GL11.glColor4f(red, green, blue, 0.5F);
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glCallList(displayList);
 		GL11.glEnd();
-		
+
 		GL11.glPopMatrix();
-		
+
 		// GL resets
 		GL11.glColor4f(1, 1, 1, 1);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
@@ -244,225 +218,222 @@ public final class SearchHack extends Hack
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
-	
-	private ChunkPos getPlayerChunkPos(BlockPos eyesPos)
-	{
+
+	private ChunkPos getPlayerChunkPos(BlockPos eyesPos) {
 		int chunkX = eyesPos.getX() >> 4;
 		int chunkZ = eyesPos.getZ() >> 4;
 		return MC.world.getChunk(chunkX, chunkZ).getPos();
 	}
-	
+
 	private void addSearchersInRange(ChunkPos center, int chunkRange,
-		Block block, int dimensionId)
-	{
-		ArrayList<Chunk> chunksInRange = getChunksInRange(center, chunkRange);
-		
-		for(Chunk chunk : chunksInRange)
-		{
-			if(searchers.containsKey(chunk))
+									 List<Block> blocks, int dimensionId) {
+		List<Chunk> chunksInRange = getChunksInRange(center, chunkRange);
+
+		for (Chunk chunk : chunksInRange) {
+			if (searchers.containsKey(chunk)) {
 				continue;
-			
-			addSearcher(chunk, block, dimensionId);
+			}
+			addSearcher(chunk, blocks, dimensionId);
 		}
 	}
-	
-	private ArrayList<Chunk> getChunksInRange(ChunkPos center, int chunkRange)
-	{
+
+	private ArrayList<Chunk> getChunksInRange(ChunkPos center, int chunkRange) {
 		ArrayList<Chunk> chunksInRange = new ArrayList<>();
-		
-		for(int x = center.x - chunkRange; x <= center.x + chunkRange; x++)
-			for(int z = center.z - chunkRange; z <= center.z + chunkRange; z++)
-			{
+
+		for (int x = center.x - chunkRange; x <= center.x + chunkRange; x++)
+			for (int z = center.z - chunkRange; z <= center.z + chunkRange; z++) {
 				Chunk chunk = MC.world.getChunk(x, z);
-				if(chunk instanceof EmptyChunk)
+				if (chunk instanceof EmptyChunk) {
 					continue;
-				
+				}
+
 				chunksInRange.add(chunk);
 			}
-		
+
 		return chunksInRange;
 	}
-	
-	private void removeSearchersOutOfRange(ChunkPos center, int chunkRange)
-	{
-		for(ChunkSearcher searcher : new ArrayList<>(searchers.values()))
-		{
-			ChunkPos searcherPos = searcher.getChunk().getPos();
-			
-			if(Math.abs(searcherPos.x - center.x) <= chunkRange
-				&& Math.abs(searcherPos.z - center.z) <= chunkRange)
-				continue;
-			
-			removeSearcher(searcher);
-		}
-	}
-	
-	private void replaceSearchersWithDifferences(Block currentBlock,
-		int dimensionId)
-	{
-		for(ChunkSearcher oldSearcher : new ArrayList<>(searchers.values()))
-		{
-			if(currentBlock.equals(oldSearcher.getBlock())
-				&& dimensionId == oldSearcher.getDimensionId())
-				continue;
-			
-			removeSearcher(oldSearcher);
-			addSearcher(oldSearcher.getChunk(), currentBlock, dimensionId);
-		}
-	}
-	
-	private void replaceSearchersWithChunkUpdate(Block currentBlock,
-		int dimensionId)
-	{
-		synchronized(chunksToUpdate)
-		{
-			if(chunksToUpdate.isEmpty())
-				return;
-			
-			for(Iterator<Chunk> itr = chunksToUpdate.iterator(); itr.hasNext();)
-			{
-				Chunk chunk = itr.next();
-				
-				ChunkSearcher oldSearcher = searchers.get(chunk);
-				if(oldSearcher == null)
+
+	private void removeSearchersOutOfRange(ChunkPos center, int chunkRange) {
+		for (List<ChunkSearcher> searchers : new ArrayList<>(searchers.values())) {
+			for (ChunkSearcher searcher : searchers) {
+				if (searchers.size() < 1) {
 					continue;
-				
-				removeSearcher(oldSearcher);
-				addSearcher(chunk, currentBlock, dimensionId);
+				}
+
+				ChunkPos searcherPos = searchers.get(0).getChunk().getPos();
+
+				if (Math.abs(searcherPos.x - center.x) <= chunkRange
+						&& Math.abs(searcherPos.z - center.z) <= chunkRange) {
+					continue;
+				}
+
+				removeSearcher(searcher);
+			}
+		}
+	}
+
+	private void replaceSearchersWithDifferences(ChunkPos center, int range, List<Block> currentBlocks, int dimensionId) {
+		for (List<ChunkSearcher> oldSearchers : new ArrayList<>(searchers.values())) {
+			Set<String> oldBlockNames = oldSearchers.stream()
+					.map(ChunkSearcher::getBlock)
+					.map(BlockUtils::getName)
+					.collect(Collectors.toSet());
+			Set<String> currentBlockNames = currentBlocks.stream()
+					.map(BlockUtils::getName)
+					.collect(Collectors.toSet());
+			if (oldBlockNames.equals(currentBlockNames)) {
+				continue;
+			}
+			if (oldSearchers.size() > 0) {
+				Chunk chunk = null;
+				for (ChunkSearcher oldSearcher : oldSearchers) {
+					if (chunk == null) {
+						chunk = oldSearcher.getChunk();
+					}
+					removeSearcher(oldSearcher);
+				}
+				addSearcher(chunk, currentBlocks, dimensionId);
+			}
+		}
+	}
+
+	private void replaceSearchersWithChunkUpdate(List<Block> currentBlocks, int dimensionId) {
+		synchronized (chunksToUpdate) {
+			if (chunksToUpdate.isEmpty())
+				return;
+
+			for (Iterator<Chunk> itr = chunksToUpdate.iterator(); itr.hasNext(); ) {
+				Chunk chunk = itr.next();
+
+				List<ChunkSearcher> oldSearchers = searchers.get(chunk);
+				if (oldSearchers == null) {
+					continue;
+				}
+				for (ChunkSearcher oldSearcher : oldSearchers) {
+					removeSearcher(oldSearcher);
+
+				}
+				addSearcher(chunk, currentBlocks, dimensionId);
 				itr.remove();
 			}
 		}
 	}
-	
-	private void addSearcher(Chunk chunk, Block block, int dimensionId)
-	{
+
+	private void addSearcher(Chunk chunk, List<Block> block, int dimensionId) {
 		stopPool2Tasks();
-		
-		ChunkSearcher searcher = new ChunkSearcher(chunk, block, dimensionId);
-		searchers.put(chunk, searcher);
-		searcher.startSearching(pool1);
+		List<ChunkSearcher> chunkSearchers = block.stream()
+				.map(b -> new ChunkSearcher(chunk, b, dimensionId))
+				.peek(searcher -> searcher.startSearching(pool1))
+				.collect(Collectors.toList());
+		searchers.put(chunk, chunkSearchers);
 	}
-	
-	private void removeSearcher(ChunkSearcher searcher)
-	{
+
+	private void removeSearcher(ChunkSearcher searcher) {
 		stopPool2Tasks();
-		
+
 		searchers.remove(searcher.getChunk());
 		searcher.cancelSearching();
 	}
-	
-	private void stopPool2Tasks()
-	{
-		if(getMatchingBlocksTask != null)
-		{
+
+	private void stopPool2Tasks() {
+		if (getMatchingBlocksTask != null) {
 			getMatchingBlocksTask.cancel(true);
 			getMatchingBlocksTask = null;
 		}
-		
-		if(compileVerticesTask != null)
-		{
+
+		if (compileVerticesTask != null) {
 			compileVerticesTask.cancel(true);
 			compileVerticesTask = null;
 		}
-		
+
 		displayListUpToDate = false;
 	}
-	
-	private boolean areAllChunkSearchersDone()
-	{
-		for(ChunkSearcher searcher : searchers.values())
-			if(searcher.getStatus() != ChunkSearcher.Status.DONE)
-				return false;
-			
+
+	private boolean areAllChunkSearchersDone() {
+		for (List<ChunkSearcher> searchers : searchers.values()) {
+			for (ChunkSearcher searcher : searchers) {
+				if (searcher.getStatus() != ChunkSearcher.Status.DONE) {
+					return false;
+				}
+			}
+		}
+
 		return true;
 	}
-	
-	private void checkIfLimitChanged()
-	{
-		if(limit.getValueI() != prevLimit)
-		{
+
+	private void checkIfLimitChanged() {
+		if (limit.getValueI() != prevLimit) {
 			stopPool2Tasks();
 			notify = true;
 			prevLimit = limit.getValueI();
 		}
 	}
-	
-	private void startGetMatchingBlocksTask(BlockPos eyesPos)
-	{
-		int maxBlocks = (int)Math.pow(10, limit.getValueI());
-		
+
+	private void startGetMatchingBlocksTask(BlockPos eyesPos) {
+		int maxBlocks = (int) Math.pow(10, limit.getValueI());
+
 		Callable<HashSet<BlockPos>> task =
-			() -> searchers.values().parallelStream()
-				.flatMap(searcher -> searcher.getMatchingBlocks().stream())
-				.sorted(Comparator
-					.comparingInt(pos -> eyesPos.getManhattanDistance(pos)))
-				.limit(maxBlocks)
-				.collect(Collectors.toCollection(() -> new HashSet<>()));
-		
+				() -> searchers.values().parallelStream()
+						.flatMap(Collection::parallelStream)
+						.flatMap(searcher -> searcher.getMatchingBlocks().stream())
+						.sorted(Comparator.comparingInt(eyesPos::getManhattanDistance))
+						.limit(maxBlocks)
+						.collect(Collectors.toCollection(HashSet::new));
+
 		getMatchingBlocksTask = pool2.submit(task);
 	}
-	
-	private HashSet<BlockPos> getMatchingBlocksFromTask()
-	{
-		HashSet<BlockPos> matchingBlocks = new HashSet<>();
-		
-		try
-		{
+
+	private HashSet<BlockPos> getMatchingBlocksFromTask() {
+		HashSet<BlockPos> matchingBlocks;
+
+		try {
 			matchingBlocks = getMatchingBlocksTask.get();
-			
-		}catch(InterruptedException | ExecutionException e)
-		{
+		} catch (InterruptedException | ExecutionException e) {
 			throw new RuntimeException(e);
 		}
-		
-		int maxBlocks = (int)Math.pow(10, limit.getValueI());
-		
-		if(matchingBlocks.size() < maxBlocks)
+
+		int maxBlocks = (int) Math.pow(10, limit.getValueI());
+
+		if (matchingBlocks.size() < maxBlocks)
 			notify = true;
-		else if(notify)
-		{
+		else if (notify) {
 			ChatUtils.warning("Search found \u00a7lA LOT\u00a7r of blocks!"
-				+ " To prevent lag, it will only show the closest \u00a76"
-				+ limit.getValueString() + "\u00a7r results.");
+					+ " To prevent lag, it will only show the closest \u00a76"
+					+ limit.getValueString() + "\u00a7r results.");
 			notify = false;
 		}
-		
+
 		return matchingBlocks;
 	}
-	
-	private void startCompileVerticesTask()
-	{
+
+	private void startCompileVerticesTask() {
 		HashSet<BlockPos> matchingBlocks = getMatchingBlocksFromTask();
-		
+
 		Callable<ArrayList<int[]>> task =
-			BlockVertexCompiler.createTask(matchingBlocks);
-		
+				BlockVertexCompiler.createTask(matchingBlocks);
+
 		compileVerticesTask = pool2.submit(task);
 	}
-	
-	private void setDisplayListFromTask()
-	{
+
+	private void setDisplayListFromTask() {
 		ArrayList<int[]> vertices;
-		
-		try
-		{
+
+		try {
 			vertices = compileVerticesTask.get();
-			
-		}catch(InterruptedException | ExecutionException e)
-		{
+
+		} catch (InterruptedException | ExecutionException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		GL11.glNewList(displayList, GL11.GL_COMPILE);
-		for(int[] vertex : vertices)
+		for (int[] vertex : vertices)
 			GL11.glVertex3d(vertex[0], vertex[1], vertex[2]);
 		GL11.glEndList();
-		
+
 		displayListUpToDate = true;
 	}
-	
-	private enum Area
-	{
+
+	private enum Area {
 		D3("3x3 chunks", 1),
 		D5("5x5 chunks", 2),
 		D7("7x7 chunks", 3),
@@ -479,19 +450,17 @@ public final class SearchHack extends Hack
 		D29("29x29 chunks", 14),
 		D31("31x31 chunks", 15),
 		D33("33x33 chunks", 16);
-		
+
 		private final String name;
 		private final int chunkRange;
-		
-		private Area(String name, int chunkRange)
-		{
+
+		Area(String name, int chunkRange) {
 			this.name = name;
 			this.chunkRange = chunkRange;
 		}
-		
+
 		@Override
-		public String toString()
-		{
+		public String toString() {
 			return name;
 		}
 	}
