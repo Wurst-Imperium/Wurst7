378d3786105aa164601f36b5f44c018d43f1451e
Alexander01998 <contact.wurstimperium@gmail.com>
2021-03-10 21:40:55 +0100

    Fix version

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -57,7 +57,7 @@ public enum WurstClient
 	public static final IMinecraftClient IMC = (IMinecraftClient)MC;
 	
 	public static final String VERSION = "7.13";
-	public static final String MC_VERSION = "21w08a";
+	public static final String MC_VERSION = "21w10a";
 	
 	private WurstAnalytics analytics;
 	private EventManager eventManager;
