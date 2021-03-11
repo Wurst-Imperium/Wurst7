a97df178f8890251951e3fc380c368daef64185c
Alexander01998 <contact.wurstimperium@gmail.com>
2021-03-11 19:44:47 +0100

    Fix CheckboxComponent shader

--- src/main/java/net/wurstclient/clickgui/components/CheckboxComponent.java
+++ src/main/java/net/wurstclient/clickgui/components/CheckboxComponent.java
@@ -14,6 +14,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.BufferRenderer;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
@@ -65,6 +66,8 @@ public final class CheckboxComponent extends Component
 		int y2 = y1 + getHeight();
 		
 		boolean hovering = isHovering(mouseX, mouseY, x1, x2, y1, y2);
+		
+		RenderSystem.setShader(GameRenderer::method_34539);
 		
 		if(hovering && mouseX >= x3)
 			setTooltip();
