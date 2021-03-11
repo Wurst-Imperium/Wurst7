9df81e5afb53391fad7ccc98e42c3db2fb7eafb7
Alexander01998 <contact.wurstimperium@gmail.com>
2021-03-11 19:33:00 +0100

    Fix CheckboxComponent*
    *Lines are still broken.

--- src/main/java/net/wurstclient/clickgui/components/CheckboxComponent.java
+++ src/main/java/net/wurstclient/clickgui/components/CheckboxComponent.java
@@ -12,7 +12,13 @@ import org.lwjgl.opengl.GL11;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.BufferRenderer;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
 import net.wurstclient.WurstClient;
 import net.wurstclient.clickgui.ClickGui;
 import net.wurstclient.clickgui.Component;
@@ -66,11 +72,11 @@ public final class CheckboxComponent extends Component
 		if(setting.isLocked())
 			hovering = false;
 		
-		drawBackground(x2, x3, y1, y2);
-		drawBox(x1, x3, y1, y2, hovering);
+		drawBackground(matrixStack, x2, x3, y1, y2);
+		drawBox(matrixStack, x1, x3, y1, y2, hovering);
 		
 		if(setting.isChecked())
-			drawCheck(x1, y1, hovering);
+			drawCheck(matrixStack, x1, y1, hovering);
 		
 		drawName(matrixStack, x3, y1);
 	}
@@ -99,84 +105,106 @@ public final class CheckboxComponent extends Component
 		GUI.setTooltip(tooltip);
 	}
 	
-	private void drawBackground(int x2, int x3, int y1, int y2)
+	private void drawBackground(MatrixStack matrixStack, int x2, int x3, int y1,
+		int y2)
 	{
 		float[] bgColor = GUI.getBgColor();
 		float opacity = GUI.getOpacity();
 		
+		Matrix4f matrix = matrixStack.peek().getModel();
+		BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+		
 		RenderSystem.setShaderColor(bgColor[0], bgColor[1], bgColor[2],
 			opacity);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2i(x3, y1);
-		GL11.glVertex2i(x3, y2);
-		GL11.glVertex2i(x2, y2);
-		GL11.glVertex2i(x2, y1);
-		GL11.glEnd();
+		bufferBuilder.begin(VertexFormat.DrawMode.QUADS,
+			VertexFormats.POSITION);
+		bufferBuilder.vertex(matrix, x3, y1, 0).next();
+		bufferBuilder.vertex(matrix, x3, y2, 0).next();
+		bufferBuilder.vertex(matrix, x2, y2, 0).next();
+		bufferBuilder.vertex(matrix, x2, y1, 0).next();
+		bufferBuilder.end();
+		BufferRenderer.draw(bufferBuilder);
 	}
 	
-	private void drawBox(int x1, int x3, int y1, int y2, boolean hovering)
+	private void drawBox(MatrixStack matrixStack, int x1, int x3, int y1,
+		int y2, boolean hovering)
 	{
 		float[] bgColor = GUI.getBgColor();
 		float[] acColor = GUI.getAcColor();
 		float opacity = GUI.getOpacity();
 		
+		Matrix4f matrix = matrixStack.peek().getModel();
+		BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+		
 		RenderSystem.setShaderColor(bgColor[0], bgColor[1], bgColor[2],
 			hovering ? opacity * 1.5F : opacity);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2i(x1, y1);
-		GL11.glVertex2i(x1, y2);
-		GL11.glVertex2i(x3, y2);
-		GL11.glVertex2i(x3, y1);
-		GL11.glEnd();
+		bufferBuilder.begin(VertexFormat.DrawMode.QUADS,
+			VertexFormats.POSITION);
+		bufferBuilder.vertex(matrix, x1, y1, 0).next();
+		bufferBuilder.vertex(matrix, x1, y2, 0).next();
+		bufferBuilder.vertex(matrix, x3, y2, 0).next();
+		bufferBuilder.vertex(matrix, x3, y1, 0).next();
+		bufferBuilder.end();
+		BufferRenderer.draw(bufferBuilder);
 		
 		RenderSystem.setShaderColor(acColor[0], acColor[1], acColor[2], 0.5F);
-		GL11.glBegin(GL11.GL_LINE_LOOP);
-		GL11.glVertex2i(x1, y1);
-		GL11.glVertex2i(x1, y2);
-		GL11.glVertex2i(x3, y2);
-		GL11.glVertex2i(x3, y1);
-		GL11.glEnd();
+		bufferBuilder.begin(VertexFormat.DrawMode.LINE_STRIP,
+			VertexFormats.POSITION);
+		bufferBuilder.vertex(matrix, x1, y1, 0).next();
+		bufferBuilder.vertex(matrix, x1, y2, 0).next();
+		bufferBuilder.vertex(matrix, x3, y2, 0).next();
+		bufferBuilder.vertex(matrix, x3, y1, 0).next();
+		bufferBuilder.end();
+		BufferRenderer.draw(bufferBuilder);
 	}
 	
-	private void drawCheck(int x1, int y1, boolean hovering)
+	private void drawCheck(MatrixStack matrixStack, int x1, int y1,
+		boolean hovering)
 	{
-		double xc1 = x1 + 2.5;
-		double xc2 = x1 + 3.5;
-		double xc3 = x1 + 4.5;
-		double xc4 = x1 + 7.5;
-		double xc5 = x1 + 8.5;
-		double yc1 = y1 + 2.5;
-		double yc2 = y1 + 3.5;
-		double yc3 = y1 + 5.5;
-		double yc4 = y1 + 6.5;
-		double yc5 = y1 + 8.5;
+		Matrix4f matrix = matrixStack.peek().getModel();
+		BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+		
+		float xc1 = x1 + 2.5F;
+		float xc2 = x1 + 3.5F;
+		float xc3 = x1 + 4.5F;
+		float xc4 = x1 + 7.5F;
+		float xc5 = x1 + 8.5F;
+		float yc1 = y1 + 2.5F;
+		float yc2 = y1 + 3.5F;
+		float yc3 = y1 + 5.5F;
+		float yc4 = y1 + 6.5F;
+		float yc5 = y1 + 8.5F;
 		
 		// check
 		if(setting.isLocked())
 			RenderSystem.setShaderColor(0.5F, 0.5F, 0.5F, 0.75F);
 		else
 			RenderSystem.setShaderColor(0, hovering ? 1 : 0.85F, 0, 1);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2d(xc2, yc3);
-		GL11.glVertex2d(xc3, yc4);
-		GL11.glVertex2d(xc3, yc5);
-		GL11.glVertex2d(xc1, yc4);
-		GL11.glVertex2d(xc4, yc1);
-		GL11.glVertex2d(xc5, yc2);
-		GL11.glVertex2d(xc3, yc5);
-		GL11.glVertex2d(xc3, yc4);
-		GL11.glEnd();
+		bufferBuilder.begin(VertexFormat.DrawMode.QUADS,
+			VertexFormats.POSITION);
+		bufferBuilder.vertex(matrix, xc2, yc3, 0).next();
+		bufferBuilder.vertex(matrix, xc3, yc4, 0).next();
+		bufferBuilder.vertex(matrix, xc3, yc5, 0).next();
+		bufferBuilder.vertex(matrix, xc1, yc4, 0).next();
+		bufferBuilder.vertex(matrix, xc4, yc1, 0).next();
+		bufferBuilder.vertex(matrix, xc5, yc2, 0).next();
+		bufferBuilder.vertex(matrix, xc3, yc5, 0).next();
+		bufferBuilder.vertex(matrix, xc3, yc4, 0).next();
+		bufferBuilder.end();
+		BufferRenderer.draw(bufferBuilder);
 		
 		// outline
 		RenderSystem.setShaderColor(0.0625F, 0.0625F, 0.0625F, 0.5F);
-		GL11.glBegin(GL11.GL_LINE_LOOP);
-		GL11.glVertex2d(xc2, yc3);
-		GL11.glVertex2d(xc3, yc4);
-		GL11.glVertex2d(xc4, yc1);
-		GL11.glVertex2d(xc5, yc2);
-		GL11.glVertex2d(xc3, yc5);
-		GL11.glVertex2d(xc1, yc4);
-		GL11.glEnd();
+		bufferBuilder.begin(VertexFormat.DrawMode.LINE_STRIP,
+			VertexFormats.POSITION);
+		bufferBuilder.vertex(matrix, xc2, yc3, 0).next();
+		bufferBuilder.vertex(matrix, xc3, yc4, 0).next();
+		bufferBuilder.vertex(matrix, xc4, yc1, 0).next();
+		bufferBuilder.vertex(matrix, xc5, yc2, 0).next();
+		bufferBuilder.vertex(matrix, xc3, yc5, 0).next();
+		bufferBuilder.vertex(matrix, xc1, yc4, 0).next();
+		bufferBuilder.end();
+		BufferRenderer.draw(bufferBuilder);
 	}
 	
 	private void drawName(MatrixStack matrixStack, int x3, int y1)
