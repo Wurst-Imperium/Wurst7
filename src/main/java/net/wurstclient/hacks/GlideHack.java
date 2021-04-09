02214380cfc526a59b15ed1fc43eaab9163be23c
Lamby777 <sheepareawesome777@gmail.com>
2021-04-09 14:43:56 -0400

    made it optional

--- src/main/java/net/wurstclient/hacks/GlideHack.java
+++ src/main/java/net/wurstclient/hacks/GlideHack.java
@@ -19,74 +19,69 @@ import net.minecraft.util.math.Vec3d;
 import net.wurstclient.Category;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.util.BlockUtils;
 
-public final class GlideHack extends Hack implements UpdateListener
-{
-	private final SliderSetting fallSpeed = new SliderSetting("Fall speed",
-		0.125, 0.005, 0.25, 0.005, ValueDisplay.DECIMAL);
+public final class GlideHack extends Hack implements UpdateListener {
+	private final CheckboxSetting doSneakFall = new CheckboxSetting("Temporary Disable",
+			"Temporarily disable this hack using your crouch button.\n"
+			+ "Turn off if you want to glide while crouching.",
+			true);
+	private final SliderSetting fallSpeed = new SliderSetting("Fall speed", 0.125, 0.005, 0.25, 0.005,
+			ValueDisplay.DECIMAL);
 	
-	private final SliderSetting moveSpeed =
-		new SliderSetting("Move speed", "Horizontal movement factor.", 1.2, 1,
-			5, 0.05, ValueDisplay.PERCENTAGE);
+	private final SliderSetting moveSpeed = new SliderSetting("Move speed", "Horizontal movement factor.", 1.2, 1, 5,
+			0.05, ValueDisplay.PERCENTAGE);
 	
 	private final SliderSetting minHeight = new SliderSetting("Min height",
-		"Won't glide when you are\n" + "too close to the ground.", 0, 0, 2,
-		0.01,
-		v -> v == 0 ? "disabled" : ValueDisplay.DECIMAL.getValueString(v));
+			"Won't glide when you are\n" + "too close to the ground.", 0, 0, 2, 0.01,
+			v -> v == 0 ? "disabled" : ValueDisplay.DECIMAL.getValueString(v));
 	
-	public GlideHack()
-	{
+	public GlideHack() {
 		super("Glide", "Makes you glide down slowly when falling.\n\n"
-			+ "\u00a7c\u00a7lWARNING:\u00a7r You will take fall damage if you don't use NoFall.");
+				+ "\u00a7c\u00a7lWARNING:\u00a7r You will take fall damage if you don't use NoFall.");
 		
 		setCategory(Category.MOVEMENT);
+		addSetting(doSneakFall);
 		addSetting(fallSpeed);
 		addSetting(moveSpeed);
 		addSetting(minHeight);
 	}
 	
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		EVENTS.add(UpdateListener.class, this);
 	}
 	
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		EVENTS.remove(UpdateListener.class, this);
 	}
 	
 	@Override
 	public void onUpdate() {
-		if (MC.options.keySneak.isPressed()) return;
+		if (doSneakFall.isChecked() && MC.options.keySneak.isPressed())
+			return;
 		ClientPlayerEntity player = MC.player;
 		Vec3d v = player.getVelocity();
 		
-		if(player.isOnGround() || player.isTouchingWater() || player.isInLava()
-			|| player.isClimbing() || v.y >= 0)
+		if (player.isOnGround() || player.isTouchingWater() || player.isInLava() || player.isClimbing() || v.y >= 0)
 			return;
 		
-		if(minHeight.getValue() > 0)
-		{
+		if (minHeight.getValue() > 0) {
 			Box box = player.getBoundingBox();
 			box = box.union(box.offset(0, -minHeight.getValue(), 0));
-			if(!MC.world.isSpaceEmpty(box))
+			if (!MC.world.isSpaceEmpty(box))
 				return;
 			
-			BlockPos min =
-				new BlockPos(new Vec3d(box.minX, box.minY, box.minZ));
-			BlockPos max =
-				new BlockPos(new Vec3d(box.maxX, box.maxY, box.maxZ));
-			Stream<BlockPos> stream = StreamSupport
-				.stream(BlockUtils.getAllInBox(min, max).spliterator(), true);
+			BlockPos min = new BlockPos(new Vec3d(box.minX, box.minY, box.minZ));
+			BlockPos max = new BlockPos(new Vec3d(box.maxX, box.maxY, box.maxZ));
+			Stream<BlockPos> stream = StreamSupport.stream(BlockUtils.getAllInBox(min, max).spliterator(), true);
 			
 			// manual collision check, since liquids don't have bounding boxes
-			if(stream.map(BlockUtils::getState).map(BlockState::getMaterial)
-				.anyMatch(Material::isLiquid))
+			if (stream.map(BlockUtils::getState).map(BlockState::getMaterial).anyMatch(Material::isLiquid))
 				return;
 		}
 		
