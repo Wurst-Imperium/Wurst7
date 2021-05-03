eab7c3f67e437b4191b5af4fc380de29414726ba
Suriel G <surielgoncalves@gmail.com>
2021-05-03 00:39:08 -0300

    addition of  the possibility to search for specific items

--- src/main/java/net/wurstclient/hacks/ItemEspHack.java
+++ src/main/java/net/wurstclient/hacks/ItemEspHack.java
@@ -23,50 +23,50 @@ import net.wurstclient.events.RenderListener;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.CheckboxSetting;
+import net.wurstclient.settings.ItemListSetting;
 import net.wurstclient.settings.EnumSetting;
 import net.wurstclient.util.RenderUtils;
 import net.wurstclient.util.RotationUtils;
+import net.wurstclient.util.SearchType;
 
-@SearchTags({"item esp", "ItemTracers", "item tracers"})
-public final class ItemEspHack extends Hack implements UpdateListener,
-	CameraTransformViewBobbingListener, RenderListener
-{
-	private final CheckboxSetting names = new CheckboxSetting("Show item names",
-		"Sorry, this is currently broken!\n"
-			+ "19w39a changed how nameplates work\n"
-			+ "and we haven't figured it out yet.",
-		true);
-	
-	private final EnumSetting<Style> style =
-		new EnumSetting<>("Style", Style.values(), Style.BOXES);
-	
+@SearchTags({ "item esp", "ItemTracers", "item tracers" })
+public final class ItemEspHack extends Hack
+		implements UpdateListener, CameraTransformViewBobbingListener, RenderListener {
+	private final CheckboxSetting names = new CheckboxSetting("Show item names", "Sorry, this is currently broken!\n"
+			+ "19w39a changed how nameplates work\n" + "and we haven't figured it out yet.", true);
+
+	private final EnumSetting<Style> style = new EnumSetting<>("Style", Style.values(), Style.BOXES);
+
 	private final EnumSetting<BoxSize> boxSize = new EnumSetting<>("Box size",
-		"\u00a7lAccurate\u00a7r mode shows the exact\n"
-			+ "hitbox of each item.\n"
-			+ "\u00a7lFancy\u00a7r mode shows larger boxes\n"
-			+ "that look better.",
-		BoxSize.values(), BoxSize.FANCY);
-	
+			"\u00a7lAccurate\u00a7r mode shows the exact\n" + "hitbox of each item.\n"
+					+ "\u00a7lFancy\u00a7r mode shows larger boxes\n" + "that look better.",
+			BoxSize.values(), BoxSize.FANCY);
+
+	private ItemListSetting itemSearch = new ItemListSetting("Search List",
+			"Use this to search for specific items!\n" + SearchType._DISABLED + ": Just marks everything\n"
+					+ SearchType._ITEMID + ": Consider the Id\n" + SearchType._NAME + ": Consider the name\n"
+					+ "(all are case sensitive)",
+			"");
+
 	private int itemBox;
 	private final ArrayList<ItemEntity> items = new ArrayList<>();
-	
-	public ItemEspHack()
-	{
+
+	public ItemEspHack() {
 		super("ItemESP", "Highlights nearby items.");
 		setCategory(Category.RENDER);
-		
+
 		addSetting(names);
 		addSetting(style);
 		addSetting(boxSize);
+		addSetting(itemSearch);
 	}
-	
+
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		EVENTS.add(UpdateListener.class, this);
 		EVENTS.add(CameraTransformViewBobbingListener.class, this);
 		EVENTS.add(RenderListener.class, this);
-		
+
 		itemBox = GL11.glGenLists(1);
 		GL11.glNewList(itemBox, GL11.GL_COMPILE);
 		GL11.glEnable(GL11.GL_BLEND);
@@ -77,57 +77,75 @@ public final class ItemEspHack extends Hack implements UpdateListener,
 		RenderUtils.drawOutlinedBox(new Box(-0.5, 0, -0.5, 0.5, 1, 0.5));
 		GL11.glEndList();
 	}
-	
+
 	@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		EVENTS.remove(UpdateListener.class, this);
 		EVENTS.remove(CameraTransformViewBobbingListener.class, this);
 		EVENTS.remove(RenderListener.class, this);
-		
+
 		GL11.glDeleteLists(itemBox, 1);
 	}
-	
+
 	@Override
-	public void onUpdate()
-	{
+	public void onUpdate() {
 		items.clear();
-		for(Entity entity : MC.world.getEntities())
-			if(entity instanceof ItemEntity)
-				items.add((ItemEntity)entity);
+		switch (itemSearch.getSearchType().getCurrent()) {
+			case SearchType._ITEMID:
+				for (Entity entity : MC.world.getEntities()) {
+					if (entity instanceof ItemEntity) {
+						if (itemSearch.containsItemId(((ItemEntity) entity).getStack().getItem())) {
+							items.add((ItemEntity) entity);
+						}
+					}
+				}
+				break;
+			case SearchType._NAME:
+				for (Entity entity : MC.world.getEntities()) {
+					if (entity instanceof ItemEntity) {
+						if (itemSearch.containsItemName(((ItemEntity) entity).getStack())) {
+							items.add((ItemEntity) entity);
+						}
+					}
+				}
+				break;
+			default:
+				for (Entity entity : MC.world.getEntities()) {
+					if (entity instanceof ItemEntity) {
+						items.add((ItemEntity) entity);
+					}
+				}
+		}
 	}
-	
+
 	@Override
-	public void onCameraTransformViewBobbing(
-		CameraTransformViewBobbingEvent event)
-	{
-		if(style.getSelected().lines)
+	public void onCameraTransformViewBobbing(CameraTransformViewBobbingEvent event) {
+		if (style.getSelected().lines)
 			event.cancel();
 	}
-	
+
 	@Override
-	public void onRender(float partialTicks)
-	{
+	public void onRender(float partialTicks) {
 		// GL settings
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		GL11.glEnable(GL11.GL_LINE_SMOOTH);
 		GL11.glLineWidth(2);
 		GL11.glDisable(GL11.GL_LIGHTING);
-		
+
 		GL11.glPushMatrix();
 		RenderUtils.applyRegionalRenderOffset();
-		
+
 		BlockPos camPos = RenderUtils.getCameraBlockPos();
 		int regionX = (camPos.getX() >> 9) * 512;
 		int regionZ = (camPos.getZ() >> 9) * 512;
-		
+
 		renderBoxes(partialTicks, regionX, regionZ);
-		
-		if(style.getSelected().lines)
+
+		if (style.getSelected().lines)
 			renderTracers(partialTicks, regionX, regionZ);
-		
+
 		GL11.glPopMatrix();
-		
+
 		// GL resets
 		GL11.glColor4f(1, 1, 1, 1);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
@@ -135,31 +153,25 @@ public final class ItemEspHack extends Hack implements UpdateListener,
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
-	
-	private void renderBoxes(double partialTicks, int regionX, int regionZ)
-	{
+
+	private void renderBoxes(double partialTicks, int regionX, int regionZ) {
 		double extraSize = boxSize.getSelected().extraSize;
-		
-		for(ItemEntity e : items)
-		{
+
+		for (ItemEntity e : items) {
 			GL11.glPushMatrix();
-			
-			GL11.glTranslated(
-				e.prevX + (e.getX() - e.prevX) * partialTicks - regionX,
-				e.prevY + (e.getY() - e.prevY) * partialTicks,
-				e.prevZ + (e.getZ() - e.prevZ) * partialTicks - regionZ);
-			
-			if(style.getSelected().boxes)
-			{
+
+			GL11.glTranslated(e.prevX + (e.getX() - e.prevX) * partialTicks - regionX,
+					e.prevY + (e.getY() - e.prevY) * partialTicks,
+					e.prevZ + (e.getZ() - e.prevZ) * partialTicks - regionZ);
+
+			if (style.getSelected().boxes) {
 				GL11.glPushMatrix();
-				GL11.glScaled(e.getWidth() + extraSize,
-					e.getHeight() + extraSize, e.getWidth() + extraSize);
+				GL11.glScaled(e.getWidth() + extraSize, e.getHeight() + extraSize, e.getWidth() + extraSize);
 				GL11.glCallList(itemBox);
 				GL11.glPopMatrix();
 			}
-			
-			if(names.isChecked())
-			{
+
+			if (names.isChecked()) {
 				// ItemStack stack = e.getStack();
 				// GameRenderer.renderFloatingText(MC.textRenderer,
 				// stack.getCount() + "x "
@@ -168,76 +180,63 @@ public final class ItemEspHack extends Hack implements UpdateListener,
 				// MC.getEntityRenderManager().cameraPitch, false);
 				// GL11.glDisable(GL11.GL_LIGHTING);
 			}
-			
+
 			GL11.glPopMatrix();
 		}
 	}
-	
-	private void renderTracers(double partialTicks, int regionX, int regionZ)
-	{
+
+	private void renderTracers(double partialTicks, int regionX, int regionZ) {
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
 		GL11.glColor4f(1, 1, 0, 0.5F);
-		
-		Vec3d start =
-			RotationUtils.getClientLookVec().add(RenderUtils.getCameraPos());
-		
+
+		Vec3d start = RotationUtils.getClientLookVec().add(RenderUtils.getCameraPos());
+
 		GL11.glBegin(GL11.GL_LINES);
-		for(ItemEntity e : items)
-		{
-			Vec3d end = e.getBoundingBox().getCenter()
-				.subtract(new Vec3d(e.getX(), e.getY(), e.getZ())
-					.subtract(e.prevX, e.prevY, e.prevZ)
-					.multiply(1 - partialTicks));
-			
+		for (ItemEntity e : items) {
+			Vec3d end = e.getBoundingBox().getCenter().subtract(new Vec3d(e.getX(), e.getY(), e.getZ())
+					.subtract(e.prevX, e.prevY, e.prevZ).multiply(1 - partialTicks));
+
 			GL11.glVertex3d(start.x - regionX, start.y, start.z - regionZ);
 			GL11.glVertex3d(end.x - regionX, end.y, end.z - regionZ);
 		}
 		GL11.glEnd();
 	}
-	
-	private enum Style
-	{
-		BOXES("Boxes only", true, false),
-		LINES("Lines only", false, true),
+
+	private enum Style {
+		BOXES("Boxes only", true, false), LINES("Lines only", false, true),
 		LINES_AND_BOXES("Lines and boxes", true, true);
-		
+
 		private final String name;
 		private final boolean boxes;
 		private final boolean lines;
-		
-		private Style(String name, boolean boxes, boolean lines)
-		{
+
+		private Style(String name, boolean boxes, boolean lines) {
 			this.name = name;
 			this.boxes = boxes;
 			this.lines = lines;
 		}
-		
+
 		@Override
-		public String toString()
-		{
+		public String toString() {
 			return name;
 		}
 	}
-	
-	private enum BoxSize
-	{
-		ACCURATE("Accurate", 0),
-		FANCY("Fancy", 0.1);
-		
+
+	private enum BoxSize {
+		ACCURATE("Accurate", 0), FANCY("Fancy", 0.1);
+
 		private final String name;
 		private final double extraSize;
-		
-		private BoxSize(String name, double extraSize)
-		{
+
+		private BoxSize(String name, double extraSize) {
 			this.name = name;
 			this.extraSize = extraSize;
 		}
-		
+
 		@Override
-		public String toString()
-		{
+		public String toString() {
 			return name;
 		}
 	}
