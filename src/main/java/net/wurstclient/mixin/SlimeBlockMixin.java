c016187060a6c20968d1f50b7db3a8c7f3bc16de
TheWienerMaster <61437644+TheWienerMaster@users.noreply.github.com>
2021-06-22 06:17:46 +0300

    Forgot to update imports

--- src/main/java/net/wurstclient/mixin/SlimeBlockMixin.java
+++ src/main/java/net/wurstclient/mixin/SlimeBlockMixin.java
@@ -19,6 +19,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.wurstclient.WurstClient;
+import net.wurstclient.hacks.NoSlowdownHack;
 
 @Mixin(SlimeBlock.class)
 public abstract class SlimeBlockMixin extends TransparentBlock
