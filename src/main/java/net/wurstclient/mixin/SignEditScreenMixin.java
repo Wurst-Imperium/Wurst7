16512991b203d8eb6c911e700e7ae29c72b070c7
TheWienerMaster <61437644+TheWienerMaster@users.noreply.github.com>
2021-06-29 18:02:24 +0300

    AutoSign Crash Fix

--- src/main/java/net/wurstclient/mixin/SignEditScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/SignEditScreenMixin.java
@@ -9,6 +9,7 @@ package net.wurstclient.mixin;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,6 +26,7 @@ public abstract class SignEditScreenMixin extends Screen
 {
 	@Shadow
 	@Final
+	@Mutable
 	private String[] text;
 	
 	private SignEditScreenMixin(WurstClient wurst, Text text_1)
