bd63884e796448bccc465a09959ddc7e148cc4d5
Alexander01998 <contact.wurstimperium@gmail.com>
2021-08-21 18:44:03 +0200

    Add "Potion type" settings

--- src/main/java/net/wurstclient/hacks/KillPotionHack.java
+++ src/main/java/net/wurstclient/hacks/KillPotionHack.java
@@ -7,6 +7,7 @@
  */
 package net.wurstclient.hacks;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
@@ -16,11 +17,17 @@ import net.minecraft.text.LiteralText;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.EnumSetting;
 import net.wurstclient.util.ChatUtils;
 
-@SearchTags({"kill potion", "KillerPotion", "killer potion"})
+@SearchTags({"kill potion", "KillerPotion", "killer potion", "KillingPotion",
+	"killing potion", "InstantDeathPotion", "instant death potion"})
 public final class KillPotionHack extends Hack
 {
+	private final EnumSetting<PotionType> potionType =
+		new EnumSetting<>("Potion type", "The type of potion to generate.",
+			PotionType.values(), PotionType.SPLASH);
+	
 	public KillPotionHack()
 	{
 		super("KillPotion",
@@ -30,6 +37,7 @@ public final class KillPotionHack extends Hack
 				+ "Requires Creative mode.");
 		
 		setCategory(Category.ITEMS);
+		addSetting(potionType);
 	}
 	
 	@Override
@@ -44,18 +52,7 @@ public final class KillPotionHack extends Hack
 		}
 		
 		// generate potion
-		ItemStack stack = new ItemStack(Items.SPLASH_POTION);
-		NbtCompound effect = new NbtCompound();
-		effect.putInt("Amplifier", 125);
-		effect.putInt("Duration", 2000);
-		effect.putInt("Id", 6);
-		NbtList effects = new NbtList();
-		effects.add(effect);
-		NbtCompound nbt = new NbtCompound();
-		nbt.put("CustomPotionEffects", effects);
-		stack.setNbt(nbt);
-		String name = "\u00a7rSplash Potion of \u00a74\u00a7lINSTANT DEATH";
-		stack.setCustomName(new LiteralText(name));
+		ItemStack stack = potionType.getSelected().createPotionStack();
 		
 		// give potion
 		if(placeStackInHotbar(stack))
@@ -79,5 +76,57 @@ public final class KillPotionHack extends Hack
 		}
 		
 		return false;
+	}
+	
+	private enum PotionType
+	{
+		NORMAL("Normal", "Potion", Items.POTION),
+		
+		SPLASH("Splash", "Splash Potion", Items.SPLASH_POTION),
+		
+		LINGERING("Lingering", "Lingering Potion", Items.LINGERING_POTION);
+		
+		// does not work
+		// ARROW("Arrow", "Arrow", Items.TIPPED_ARROW);
+		
+		private final String name;
+		private final String itemName;
+		private final Item item;
+		
+		private PotionType(String name, String itemName, Item item)
+		{
+			this.name = name;
+			this.itemName = itemName;
+			this.item = item;
+		}
+		
+		@Override
+		public String toString()
+		{
+			return name;
+		}
+		
+		public ItemStack createPotionStack()
+		{
+			ItemStack stack = new ItemStack(item);
+			
+			NbtCompound effect = new NbtCompound();
+			effect.putInt("Amplifier", 125);
+			effect.putInt("Duration", 2000);
+			effect.putInt("Id", 6);
+			
+			NbtList effects = new NbtList();
+			effects.add(effect);
+			
+			NbtCompound nbt = new NbtCompound();
+			nbt.put("CustomPotionEffects", effects);
+			stack.setNbt(nbt);
+			
+			String name =
+				"\u00a7f" + itemName + " of \u00a74\u00a7lINSTANT DEATH";
+			stack.setCustomName(new LiteralText(name));
+			
+			return stack;
+		}
 	}
 }
