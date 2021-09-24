8e78c4a2f02bca864a02100fea2277d1386d2355
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2021-09-24 18:26:55 +0800

    Update JumpCmd.java

--- src/main/java/net/wurstclient/commands/JumpCmd.java
+++ src/main/java/net/wurstclient/commands/JumpCmd.java
@@ -16,7 +16,7 @@ public final class JumpCmd extends Command
 {
 	public JumpCmd()
 	{
-		super("jump", "Makes you jump.");
+		super("jump", "使你跳一下.");
 	}
 	
 	@Override
@@ -26,7 +26,7 @@ public final class JumpCmd extends Command
 			throw new CmdSyntaxError();
 		
 		if(!MC.player.isOnGround() && !WURST.getHax().jetpackHack.isEnabled())
-			throw new CmdError("Can't jump in mid-air.");
+			throw new CmdError("不能在空中跳跃.");
 		
 		MC.player.jump();
 	}
