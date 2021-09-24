af8afb0baa2dd4a0df8814a449ec4bab7b093152
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2021-09-24 18:30:04 +0800

    Update LastServerOtf.java

--- src/main/java/net/wurstclient/other_features/LastServerOtf.java
+++ src/main/java/net/wurstclient/other_features/LastServerOtf.java
@@ -18,9 +18,7 @@ public final class LastServerOtf extends OtherFeature
 	public LastServerOtf()
 	{
 		super("LastServer",
-			"Wurst adds a \"Last Server\" button to the server selection\n"
-				+ "screen that automatically brings you back to the last\n"
-				+ "server you played on.\n\n"
-				+ "Useful when you get kicked and/or have a lot of servers.");
+			"Wurst 在服务器选择屏幕上添加了一个“上一个服务器”按钮，该按钮会自动将您带回上次玩的服务器.\n\n"
+				+ "当你被踢和/或有很多服务器时很有用.");
 	}
 }
