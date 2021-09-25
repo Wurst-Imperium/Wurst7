9081e84c0645baeb5496fc0589070b550ee7c6a9
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2021-09-25 10:58:37 +0800

    Update SpeedNukerHack.java

--- src/main/java/net/wurstclient/hacks/SpeedNukerHack.java
+++ src/main/java/net/wurstclient/hacks/SpeedNukerHack.java
@@ -74,8 +74,7 @@ public final class SpeedNukerHack extends Hack
 	
 	public SpeedNukerHack()
 	{
-		super("SpeedNuker",
-			"Faster version of Nuker that cannot bypass NoCheat+.");
+		super("矿井+", "更快的的Nuker,无法绕过NoCheat+.");
 		
 		setCategory(Category.BLOCKS);
 		addSetting(range);
