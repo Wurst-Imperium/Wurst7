898205f021a2d58222e0900ad8d5b594a4dae165
Lamby777 <sheepareawesome777@gmail.com>
2021-11-28 01:44:02 -0500

    OIF command actually does something

--- src/main/java/net/wurstclient/commands/OnInvFullCmd.java
+++ src/main/java/net/wurstclient/commands/OnInvFullCmd.java
@@ -10,76 +10,21 @@ package net.wurstclient.commands;
 import net.wurstclient.command.CmdException;
 import net.wurstclient.command.CmdSyntaxError;
 import net.wurstclient.command.Command;
-import net.wurstclient.events.UpdateListener;
+import net.wurstclient.hacks.OnInvFullHack;
 
-public final class OnInvFullCmd extends Command implements UpdateListener
-{
-	private int slowModeTimer;
-	private int slowModeSlotCounter;
-	
-	public OnInvFullCmd()
-	{
-		super("oninvfull", "Writes message when your inventory becomes full.", ".oninvfull",
+public final class OnInvFullCmd extends Command {
+	public OnInvFullCmd() {
+		super("oninvfull", "Writes message when your inventory becomes full.", ".oninvfull <action>",
 			"Example: .oninvfull /sellall",
 			"Runs /sellall when inventory becomes full.",
 			"Compatible with Wurst \"dot\" commands");
 	}
 	
 	@Override
-	public void call(String[] args) throws CmdException
-	{
-		if(args.length > 1)
-			throw new CmdSyntaxError();
-		
+	public void call(String[] args) throws CmdException {
 		if(args.length < 1)
-		{
-			dropAllItems();
-			return;
-		}
-		
-		if(!args[0].equalsIgnoreCase("slow"))
 			throw new CmdSyntaxError();
 		
-		slowModeTimer = 5;
-		slowModeSlotCounter = 9;
-		EVENTS.add(UpdateListener.class, this);
-	}
-	
-	private void dropAllItems()
-	{
-		for(int i = 9; i < 45; i++)
-			IMC.getInteractionManager().windowClick_THROW(i);
-	}
-	
-	@Override
-	public void onUpdate()
-	{
-		slowModeTimer--;
-		if(slowModeTimer > 0)
-			return;
-		
-		skipEmptySlots();
-		IMC.getInteractionManager().windowClick_THROW(slowModeSlotCounter);
-		
-		slowModeSlotCounter++;
-		slowModeTimer = 5;
-		
-		if(slowModeSlotCounter >= 45)
-			EVENTS.remove(UpdateListener.class, this);
-	}
-	
-	private void skipEmptySlots()
-	{
-		while(slowModeSlotCounter < 45)
-		{
-			int adjustedSlot = slowModeSlotCounter;
-			if(adjustedSlot >= 36)
-				adjustedSlot -= 36;
-			
-			if(!MC.player.getInventory().getStack(adjustedSlot).isEmpty())
-				break;
-			
-			slowModeSlotCounter++;
-		}
+		OnInvFullHack.action = String.join(" ", args);
 	}
 }
