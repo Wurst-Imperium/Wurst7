9174968b6bc5ec1a170bb813a0ac78c964fb31b9
yuanlu <2573580691@qq.com>
2021-12-06 14:02:51 +0800

    Increase speed customization of BoatFlyHack
    Signed-off-by: yuanlu <2573580691@qq.com>

--- src/main/java/net/wurstclient/hacks/BoatFlyHack.java
+++ src/main/java/net/wurstclient/hacks/BoatFlyHack.java
@@ -13,14 +13,24 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"BoatFlight", "boat fly", "boat flight"})
 public final class BoatFlyHack extends Hack implements UpdateListener
 {
+	private final SliderSetting moveSpeed=new SliderSetting("Move speed", "Provides an additional horizontal speed to increase movement speed"//
+			+"\n提供一个额外的水平速度，提高移动速度"//Chinese description, can be deleted
+			, 0, 0, 2, 0.01, ValueDisplay.DECIMAL);
+	private final SliderSetting flySpeed=new SliderSetting("Fly speed", "Set vertical speed"//
+			+"\n设定垂直速度"//Chinese description, can be deleted
+			, 0.3, 0, 2, 0.01, null);
 	public BoatFlyHack()
 	{
 		super("BoatFly");
 		setCategory(Category.MOVEMENT);
+		addSetting(moveSpeed);
+		addSetting(flySpeed);
 	}
 	
 	@Override
@@ -45,7 +55,9 @@ public final class BoatFlyHack extends Hack implements UpdateListener
 		// fly
 		Entity vehicle = MC.player.getVehicle();
 		Vec3d velocity = vehicle.getVelocity();
-		double motionY = MC.options.keyJump.isPressed() ? 0.3 : 0;
-		vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+		double motionY = MC.options.keyJump.isPressed() ? this.flySpeed.getValue() : 0;
+		double speed   = velocity.horizontalLength();
+		speed = (speed + this.moveSpeed.getValue()) / speed;
+		vehicle.setVelocity(new Vec3d(velocity.x * speed, motionY, velocity.z * speed));
 	}
 }
