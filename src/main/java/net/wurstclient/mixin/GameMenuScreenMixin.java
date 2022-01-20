86069eea87a7f78f2e70f0b556f3ead20a1e5e00
Kadeluxe <63540878+Kadeluxe@users.noreply.github.com>
2022-01-20 13:42:36 +0300

    Fix injection of Wurst options button

--- src/main/java/net/wurstclient/mixin/GameMenuScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/GameMenuScreenMixin.java
@@ -15,17 +15,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import java.util.List;
+
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.wurstclient.WurstClient;
-import net.wurstclient.mixinterface.IScreen;
 import net.wurstclient.options.WurstOptionsScreen;
 
 @Mixin(GameMenuScreen.class)
@@ -48,41 +50,45 @@ public abstract class GameMenuScreenMixin extends Screen
 			return;
 		
 		addWurstOptionsButton();
-		removeFeedbackAndBugReportButtons();
 	}
 	
 	private void addWurstOptionsButton()
 	{
-		wurstOptionsButton = new ButtonWidget(width / 2 - 102, height / 4 + 56,
-			204, 20, new LiteralText("            Options"),
-			b -> openWurstOptions());
+		List<ClickableWidget> buttons = Screens.getButtons(this);
 		
-		addDrawableChild(wurstOptionsButton);
+		int buttonWidth = 204;
+		int buttonHeight = 20;
+		
+		int buttonX = this.width / 2 - 102;
+		int buttonY = 0;
+		
+		int idx = 0;
+		
+		for (int i = 0; i < buttons.size(); ++i)
+		{
+			ClickableWidget button = buttons.get(i);
+			
+			// insert Wurst button in place of game options row
+			if (button.visible && buttonHasText(button, "menu.options"))
+			{
+				buttonY = button.y;
+				idx = i;
+			}
+			
+			// shift next buttons down
+			if (buttonY != 0)
+			{
+				button.y += buttonHeight + 4;
+			}
+		}
+		
+		wurstOptionsButton = new ButtonWidget(buttonX, buttonY, buttonWidth, buttonHeight, new LiteralText("            Options"), b -> openWurstOptions());
+		buttons.add(idx, wurstOptionsButton);
 	}
 	
 	private void openWurstOptions()
 	{
 		client.setScreen(new WurstOptionsScreen(this));
-	}
-	
-	private void removeFeedbackAndBugReportButtons()
-	{
-		((IScreen)this).getButtons()
-			.removeIf(this::isFeedbackOrBugReportButton);
-		children().removeIf(this::isFeedbackOrBugReportButton);
-	}
-	
-	private boolean isFeedbackOrBugReportButton(Object element)
-	{
-		if(element == null || !(element instanceof ClickableWidget))
-			return false;
-		
-		ClickableWidget button = (ClickableWidget)element;
-		String message = button.getMessage().getString();
-		
-		return message != null
-			&& (message.equals(I18n.translate("menu.sendFeedback"))
-				|| message.equals(I18n.translate("menu.reportBugs")));
 	}
 	
 	@Inject(at = {@At("TAIL")},
@@ -111,5 +117,11 @@ public abstract class GameMenuScreenMixin extends Screen
 		float u = 0;
 		float v = 0;
 		drawTexture(matrixStack, x, y, u, v, w, h, fw, fh);
+	}
+	
+	private static boolean buttonHasText(ClickableWidget button, String translationKey)
+	{
+		Text text = button.getMessage();
+		return text instanceof TranslatableText && ((TranslatableText) text).getKey().equals(translationKey);
 	}
 }
