fb57b36cb758f0b4e522200e9c43d4087db4f61d
Jerry <isjerryxiao@outlook.com>
2022-01-26 19:27:21 +0800

    update BoatFlyHack: increase boat speed

--- src/main/java/net/wurstclient/hacks/BoatFlyHack.java
+++ src/main/java/net/wurstclient/hacks/BoatFlyHack.java
@@ -8,19 +8,32 @@
 package net.wurstclient.hacks;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
+import net.wurstclient.settings.SliderSetting;
 
 @SearchTags({"BoatFlight", "boat fly", "boat flight"})
 public final class BoatFlyHack extends Hack implements UpdateListener
 {
+	private final CheckboxSetting changeforwardSpeed =
+			new CheckboxSetting("Change Forward Speed", "Allows Forward Speed to be changed, disables smooth acceleration.", false);
+	public final SliderSetting forwardSpeed =
+			new SliderSetting("Forward Speed", 1, 0.05, 5, 0.05, SliderSetting.ValueDisplay.DECIMAL);
+	public final SliderSetting upwardSpeed =
+			new SliderSetting("Upward Speed", 0.3, 0, 5, 0.05, SliderSetting.ValueDisplay.DECIMAL);
 	public BoatFlyHack()
 	{
 		super("BoatFly");
 		setCategory(Category.MOVEMENT);
+		addSetting(changeforwardSpeed);
+		addSetting(forwardSpeed);
+		addSetting(upwardSpeed);
 	}
 	
 	@Override
@@ -41,11 +54,19 @@ public final class BoatFlyHack extends Hack implements UpdateListener
 		// check if riding
 		if(!MC.player.hasVehicle())
 			return;
-		
-		// fly
+		// check is boat
 		Entity vehicle = MC.player.getVehicle();
+		if (!(vehicle instanceof BoatEntity)) return;
+
+		// fly
 		Vec3d velocity = vehicle.getVelocity();
-		double motionY = MC.options.jumpKey.isPressed() ? 0.3 : 0;
-		vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+		double motionY = MC.options.jumpKey.isPressed() ? upwardSpeed.getValue() : 0;
+		if (changeforwardSpeed.isChecked() && MC.options.forwardKey.isPressed()) {
+			double f = forwardSpeed.getValue();
+			vehicle.setVelocity(MathHelper.sin(-vehicle.getYaw() * ((float)Math.PI / 180)) * f, motionY, MathHelper.cos(vehicle.getYaw() * ((float)Math.PI / 180)) * f);
+		}
+		else {
+			vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+		}
 	}
 }
