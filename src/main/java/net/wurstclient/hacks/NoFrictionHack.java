6e9c14babbae1561469c310e07312620b370856d
HAHALOSAH <67280050+HAHALOSAH@users.noreply.github.com>
2022-06-20 14:23:52 -0700

    Add settings for NoFriction

--- src/main/java/net/wurstclient/hacks/NoFrictionHack.java
+++ src/main/java/net/wurstclient/hacks/NoFrictionHack.java
@@ -3,13 +3,18 @@ package net.wurstclient.hacks;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"friction", "no friction", "slippery", "slipperiness"})
 public final class NoFrictionHack extends Hack
 {
+	public final SliderSetting friction =
+                new SliderSetting("Friction/Slipperiness", 0.989, 0.8, 1.1, 0.001, ValueDisplay.DECIMAL);
 	public NoFrictionHack()
 	{
 		super("NoFriction");
 		setCategory(Category.MOVEMENT);
+		addSetting(friction);
 	}
 }
