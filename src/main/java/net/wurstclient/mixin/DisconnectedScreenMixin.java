54cc1bd6bf97fa51893403bdf1361392f1d1a5e7
Alexander01998 <contact.wurstimperium@gmail.com>
2022-07-06 10:41:51 +0200

    Fix NPE in DisconnectedScreenMixin
    Fixes #678

--- src/main/java/net/wurstclient/mixin/DisconnectedScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/DisconnectedScreenMixin.java
@@ -77,6 +77,9 @@ public class DisconnectedScreenMixin extends Screen
 	@Override
 	public void tick()
 	{
+		if(!WurstClient.INSTANCE.isEnabled() || autoReconnectButton == null)
+			return;
+		
 		AutoReconnectHack autoReconnect =
 			WurstClient.INSTANCE.getHax().autoReconnectHack;
 		
