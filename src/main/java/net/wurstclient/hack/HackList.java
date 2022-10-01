0c42eaa4fe1716a391a96943a36cbfe29f79aacf
Beta-51 <65312404+Beta-51@users.noreply.github.com>
2022-10-01 15:16:49 -0400

    Update HackList.java

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -109,6 +109,7 @@ public final class HackList implements UpdateListener
 	public final KillauraLegitHack killauraLegitHack = new KillauraLegitHack();
 	public final KillauraHack killauraHack = new KillauraHack();
 	public final KillPotionHack killPotionHack = new KillPotionHack();
+	public final KnockbackPlusHack knockbackPlusHack = new KnockbackPlusHack();
 	public final LiquidsHack liquidsHack = new LiquidsHack();
 	public final LsdHack lsdHack = new LsdHack();
 	public final MassTpaHack massTpaHack = new MassTpaHack();
