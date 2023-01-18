6c57f9cfb70463f7c4149b1fa3cb51c1f2871fbf
Alexander01998 <contact.wurstimperium@gmail.com>
2023-01-18 23:08:30 +0100

    [Wurst-Bot] Update to 23w03a

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -57,7 +57,7 @@ public enum WurstClient
 	public static IMinecraftClient IMC;
 	
 	public static final String VERSION = "7.31";
-	public static final String MC_VERSION = "1.19.3";
+	public static final String MC_VERSION = "23w03a";
 	
 	private WurstAnalytics analytics;
 	private EventManager eventManager;
