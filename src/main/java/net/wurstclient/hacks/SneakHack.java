a747e0f31bc213433edf6d4004bb10c346409910
Hassinator00 <109218095+Hassinator00@users.noreply.github.com>
2023-03-22 17:29:48 +0100

    SneakHack fly detection
    Added the possibility to fly while Sneak is active

--- src/main/java/net/wurstclient/hacks/SneakHack.java
+++ src/main/java/net/wurstclient/hacks/SneakHack.java
@@ -7,17 +7,23 @@
  */
 package net.wurstclient.hacks;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket.Mode;
+import net.minecraft.util.math.BlockPos;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.PostMotionListener;
 import net.wurstclient.events.PreMotionListener;
+import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
 import net.wurstclient.mixinterface.IKeyBinding;
 import net.wurstclient.settings.EnumSetting;
+
+import java.util.ArrayList;
 
 @SearchTags({"AutoSneaking"})
 public final class SneakHack extends Hack
@@ -70,30 +76,85 @@ public final class SneakHack extends Hack
 	@Override
 	public void onPreMotion()
 	{
+
+		boolean isFlying = checkFly();
+
 		KeyBinding sneakKey = MC.options.sneakKey;
-		
-		switch(mode.getSelected())
-		{
+
+		switch(mode.getSelected()) {
 			case LEGIT:
-			sneakKey.setPressed(true);
-			break;
-			
+				if(!isFlying){
+					sneakKey.setPressed(true);
+				}else if(sneakKey.isPressed() && !isOnGround()){
+					sneakKey.setPressed(true);
+				}
+				else{
+					sneakKey.setPressed(false);
+				}
+				break;
+
 			case PACKET:
-			((IKeyBinding)sneakKey).resetPressedState();
-			sendSneakPacket(Mode.PRESS_SHIFT_KEY);
-			sendSneakPacket(Mode.RELEASE_SHIFT_KEY);
-			break;
+				((IKeyBinding) sneakKey).resetPressedState();
+				sendSneakPacket(Mode.PRESS_SHIFT_KEY);
+				sendSneakPacket(Mode.RELEASE_SHIFT_KEY);
+				break;
 		}
 	}
 	
 	@Override
-	public void onPostMotion()
-	{
-		if(mode.getSelected() != SneakMode.PACKET)
+	public void onPostMotion() {
+
+		if (mode.getSelected() != SneakMode.PACKET)
 			return;
-		
-		sendSneakPacket(Mode.RELEASE_SHIFT_KEY);
-		sendSneakPacket(Mode.PRESS_SHIFT_KEY);
+
+			sendSneakPacket(Mode.RELEASE_SHIFT_KEY);
+			sendSneakPacket(Mode.PRESS_SHIFT_KEY);
+	}
+
+	private boolean checkFly(){
+
+		boolean ground = isOnGround();
+
+		ClientPlayerEntity player = MC.player;
+		int height = (int)player.getPos().y - 1;
+		BlockState blockStateBelow = player.world.getBlockState(new BlockPos((int)player.getPos().x, height, (int)player.getPos().z));
+
+		if(player.world.getBlockState(new BlockPos((int)player.getPos().x, height-1, (int)player.getPos().z)).getMaterial() != Material.AIR && !MC.options.jumpKey.isPressed()){
+			MC.options.sneakKey.setPressed(true);
+		}
+
+		if(player.getAbilities().flying){
+			return true;
+		}else if(blockStateBelow.getMaterial() == Material.AIR && !ground){
+			return true;
+		}else if(MC.options.jumpKey.isPressed()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	private boolean isOnGround() {
+		ClientPlayerEntity player = MC.player;
+		ArrayList<BlockState> blocks = new ArrayList<>();
+
+		for(int x = 2; x >= -2; x--){
+			for(int z = 2; z >= -2; z--){
+				BlockPos block = new BlockPos((int)player.getPos().x + x, (int)player.getPos().y-1, (int)player.getPos().z + z);
+				blocks.add(player.world.getBlockState(block));
+			}
+		}
+
+		for(BlockState block : blocks){
+			if(block.getMaterial() != Material.AIR){
+				System.out.println(block.getMaterial());
+				return true;
+			}
+		}
+
+		return false;
+
 	}
 	
 	private void sendSneakPacket(Mode mode)
