70a69247b23766dde7ac13f5069b042f9a60346f
Hassinator00 <109218095+Hassinator00@users.noreply.github.com>
2023-03-22 19:09:27 +0100

    Update SneakHack.java

--- src/main/java/net/wurstclient/hacks/SneakHack.java
+++ src/main/java/net/wurstclient/hacks/SneakHack.java
@@ -105,7 +105,7 @@ public final class SneakHack extends Hack
 	public void onPostMotion()
 	{
 
-		if (mode.getSelected() != SneakMode.PACKET)
+		if(mode.getSelected() != SneakMode.PACKET)
 			return;
 
 	sendSneakPacket(Mode.RELEASE_SHIFT_KEY);
