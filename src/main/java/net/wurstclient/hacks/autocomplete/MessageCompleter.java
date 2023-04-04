c27fb79d258f7801ca3a7e100e994b55b6bb6445
Alexander01998 <contact.wurstimperium@gmail.com>
2023-04-04 20:40:41 +0200

    Move OpenAI-specific methods to OpenAiMessageCompleter

--- src/main/java/net/wurstclient/hacks/autocomplete/MessageCompleter.java
+++ src/main/java/net/wurstclient/hacks/autocomplete/MessageCompleter.java
@@ -8,9 +8,6 @@
 package net.wurstclient.hacks.autocomplete;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
 import com.google.gson.JsonObject;
@@ -20,7 +17,6 @@ import net.minecraft.client.gui.hud.ChatHudLine;
 import net.wurstclient.WurstClient;
 import net.wurstclient.util.ChatUtils;
 import net.wurstclient.util.json.JsonException;
-import net.wurstclient.util.json.JsonUtils;
 import net.wurstclient.util.json.WsonObject;
 
 public abstract class MessageCompleter
@@ -92,54 +88,11 @@ public abstract class MessageCompleter
 		return prompt;
 	}
 	
-	protected JsonObject buildParams(String prompt)
-	{
-		JsonObject params = new JsonObject();
-		params.addProperty("prompt", prompt);
-		params.addProperty("stop", "\n<");
-		params.addProperty("model", "code-davinci-002");
-		params.addProperty("max_tokens", modelSettings.maxTokens.getValueI());
-		params.addProperty("temperature", modelSettings.temperature.getValue());
-		params.addProperty("top_p", modelSettings.topP.getValue());
-		params.addProperty("presence_penalty",
-			modelSettings.presencePenalty.getValue());
-		params.addProperty("frequency_penalty",
-			modelSettings.frequencyPenalty.getValue());
-		return params;
-	}
+	protected abstract JsonObject buildParams(String prompt);
 	
-	protected WsonObject requestCompletion(JsonObject parameters)
-		throws IOException, JsonException
-	{
-		// set up the API request
-		URL url = new URL("https://api.openai.com/v1/completions");
-		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("Content-Type", "application/json");
-		conn.setRequestProperty("Authorization",
-			"Bearer " + System.getenv("WURST_OPENAI_KEY"));
-		
-		// set the request body
-		conn.setDoOutput(true);
-		try(OutputStream os = conn.getOutputStream())
-		{
-			os.write(JsonUtils.GSON.toJson(parameters).getBytes());
-			os.flush();
-		}
-		
-		// parse the response
-		return JsonUtils.parseConnectionToObject(conn);
-	}
+	protected abstract WsonObject requestCompletion(JsonObject parameters)
+		throws IOException, JsonException;
 	
-	protected String extractCompletion(WsonObject response) throws JsonException
-	{
-		// extract completion from response
-		String completion =
-			response.getArray("choices").getObject(0).getString("text");
-		
-		// remove newlines
-		completion = completion.replace("\n", " ");
-		
-		return completion;
-	}
+	protected abstract String extractCompletion(WsonObject response)
+		throws JsonException;
 }
