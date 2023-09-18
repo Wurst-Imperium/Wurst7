486c98b9e610620830ddf370ac64b52033e2a6b9
Alexander01998 <contact.wurstimperium@gmail.com>
2023-09-18 20:36:22 +0200

    Use ChunkSearcherCoordinator in PortalESP

--- src/main/java/net/wurstclient/hacks/PortalEspHack.java
+++ src/main/java/net/wurstclient/hacks/PortalEspHack.java
@@ -8,14 +8,8 @@
 package net.wurstclient.hacks;
 
 import java.awt.Color;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
 import java.util.function.BiPredicate;
 
 import org.lwjgl.opengl.GL11;
@@ -27,9 +21,6 @@ import net.minecraft.block.Blocks;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.dimension.DimensionType;
 import net.wurstclient.Category;
 import net.wurstclient.events.CameraTransformViewBobbingListener;
 import net.wurstclient.events.PacketInputListener;
@@ -42,14 +33,12 @@ import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.ChunkAreaSetting;
 import net.wurstclient.settings.ColorSetting;
 import net.wurstclient.settings.EspStyleSetting;
-import net.wurstclient.util.ChunkSearcher;
 import net.wurstclient.util.ChunkSearcher.Result;
-import net.wurstclient.util.ChunkUtils;
-import net.wurstclient.util.MinPriorityThreadFactory;
+import net.wurstclient.util.ChunkSearcherCoordinator;
 import net.wurstclient.util.RenderUtils;
 
 public final class PortalEspHack extends Hack implements UpdateListener,
-	PacketInputListener, CameraTransformViewBobbingListener, RenderListener
+	CameraTransformViewBobbingListener, RenderListener
 {
 	private final EspStyleSetting style = new EspStyleSetting();
 	
@@ -84,16 +73,14 @@ public final class PortalEspHack extends Hack implements UpdateListener,
 		"The area around the player to search in.\n"
 			+ "Higher values require a faster computer.");
 	
-	private final HashMap<ChunkPos, ChunkSearcher> searchers = new HashMap<>();
 	private final BiPredicate<BlockPos, BlockState> query =
 		(pos, state) -> state.getBlock() == Blocks.NETHER_PORTAL
 			|| state.getBlock() == Blocks.END_PORTAL
 			|| state.getBlock() == Blocks.END_PORTAL_FRAME
 			|| state.getBlock() == Blocks.END_GATEWAY;
 	
-	private final Set<ChunkPos> chunksToUpdate =
-		Collections.synchronizedSet(new HashSet<>());
-	private ExecutorService threadPool;
+	private final ChunkSearcherCoordinator coordinator =
+		new ChunkSearcherCoordinator(query, area);
 	
 	private boolean groupsUpToDate;
 	
@@ -111,11 +98,10 @@ public final class PortalEspHack extends Hack implements UpdateListener,
 	@Override
 	public void onEnable()
 	{
-		threadPool = MinPriorityThreadFactory.newFixedThreadPool();
 		groupsUpToDate = false;
 		
 		EVENTS.add(UpdateListener.class, this);
-		EVENTS.add(PacketInputListener.class, this);
+		EVENTS.add(PacketInputListener.class, coordinator);
 		EVENTS.add(CameraTransformViewBobbingListener.class, this);
 		EVENTS.add(RenderListener.class, this);
 		
@@ -126,23 +112,13 @@ public final class PortalEspHack extends Hack implements UpdateListener,
 	public void onDisable()
 	{
 		EVENTS.remove(UpdateListener.class, this);
-		EVENTS.remove(PacketInputListener.class, this);
+		EVENTS.remove(PacketInputListener.class, coordinator);
 		EVENTS.remove(CameraTransformViewBobbingListener.class, this);
 		EVENTS.remove(RenderListener.class, this);
 		
-		threadPool.shutdownNow();
-		chunksToUpdate.clear();
+		coordinator.reset();
 		groups.forEach(PortalEspBlockGroup::clear);
 		PortalEspRenderer.closeBuffers();
-	}
-	
-	@Override
-	public void onReceivedPacket(PacketInputEvent event)
-	{
-		ChunkPos chunkPos = ChunkUtils.getAffectedChunk(event.getPacket());
-		
-		if(chunkPos != null)
-			chunksToUpdate.add(chunkPos);
 	}
 	
 	@Override
@@ -156,52 +132,11 @@ public final class PortalEspHack extends Hack implements UpdateListener,
 	@Override
 	public void onUpdate()
 	{
-		DimensionType dimension = MC.world.getDimension();
-		HashSet<ChunkPos> chunkUpdates = clearChunksToUpdate();
-		
-		// remove outdated ChunkSearchers
-		for(ChunkSearcher searcher : new ArrayList<>(searchers.values()))
-		{
-			boolean remove = false;
-			ChunkPos searcherPos = searcher.getPos();
-			
-			// wrong dimension
-			if(dimension != searcher.getDimension())
-				remove = true;
-			
-			// out of range
-			else if(!area.isInRange(searcherPos))
-				remove = true;
-			
-			// chunk update
-			else if(chunkUpdates.contains(searcherPos))
-				remove = true;
-			
-			if(remove)
-			{
-				searchers.remove(searcherPos);
-				searcher.cancel();
-				groupsUpToDate = false;
-			}
-		}
-		
-		// add new ChunkSearchers
-		for(Chunk chunk : area.getChunksInRange())
-		{
-			ChunkPos chunkPos = chunk.getPos();
-			if(searchers.containsKey(chunkPos))
-				continue;
-			
-			ChunkSearcher searcher = new ChunkSearcher(query, chunk, dimension);
-			searchers.put(chunkPos, searcher);
-			searcher.start(threadPool);
+		boolean searchersChanged = coordinator.update();
+		if(searchersChanged)
 			groupsUpToDate = false;
-		}
 		
-		if(!searchers.values().stream().allMatch(ChunkSearcher::isDone))
-			return;
-		
-		if(!groupsUpToDate)
+		if(coordinator.isDone() && !groupsUpToDate)
 			updateGroupBoxes();
 	}
 	
@@ -241,22 +176,10 @@ public final class PortalEspHack extends Hack implements UpdateListener,
 		GL11.glDisable(GL11.GL_BLEND);
 	}
 	
-	private HashSet<ChunkPos> clearChunksToUpdate()
-	{
-		synchronized(chunksToUpdate)
-		{
-			HashSet<ChunkPos> chunks = new HashSet<>(chunksToUpdate);
-			chunksToUpdate.clear();
-			return chunks;
-		}
-	}
-	
 	private void updateGroupBoxes()
 	{
 		groups.forEach(PortalEspBlockGroup::clear);
-		searchers.values().stream().flatMap(ChunkSearcher::getMatches)
-			.forEachOrdered(this::addToGroupBoxes);
-		
+		coordinator.getMatches().forEach(this::addToGroupBoxes);
 		groupsUpToDate = true;
 	}
 	
