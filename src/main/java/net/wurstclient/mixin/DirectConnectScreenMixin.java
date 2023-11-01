ee781b23b240fc693f50d9198fbedcd92444486c
Alexander01998 <contact.wurstimperium@gmail.com>
2023-11-01 19:37:10 +0100

    Update to 23w44a

--- src/main/java/net/wurstclient/mixin/DirectConnectScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/DirectConnectScreenMixin.java
@@ -13,6 +13,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.DirectConnectScreen;
 import net.minecraft.client.network.ServerInfo;
