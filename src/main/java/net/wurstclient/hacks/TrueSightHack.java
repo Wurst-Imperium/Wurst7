f79f10525aae5f76199c7431c724145ac47e362b
Alexander01998 <contact.wurstimperium@gmail.com>
2023-11-03 16:03:22 +0100

    Add filters to TrueSightHack

--- src/main/java/net/wurstclient/hacks/TrueSightHack.java
+++ src/main/java/net/wurstclient/hacks/TrueSightHack.java
@@ -11,19 +11,45 @@ import net.minecraft.entity.Entity;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.filterlists.EntityFilterList;
+import net.wurstclient.settings.filters.*;
 
 @SearchTags({"true sight"})
 public final class TrueSightHack extends Hack
 {
+	private final EntityFilterList entityFilters =
+		new EntityFilterList(FilterHostileSetting.genericVision(false),
+			FilterNeutralSetting
+				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
+			FilterPassiveSetting.genericVision(false),
+			FilterPassiveWaterSetting.genericVision(false),
+			FilterBatsSetting.genericVision(false),
+			FilterSlimesSetting.genericVision(false),
+			FilterPetsSetting.genericVision(false),
+			FilterVillagersSetting.genericVision(false),
+			FilterZombieVillagersSetting.genericVision(false),
+			FilterGolemsSetting.genericVision(false),
+			FilterPiglinsSetting
+				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
+			FilterZombiePiglinsSetting
+				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
+			FilterEndermenSetting
+				.genericVision(AttackDetectingEntityFilter.Mode.OFF),
+			FilterShulkersSetting.genericVision(false),
+			FilterAllaysSetting.genericVision(false),
+			FilterNamedSetting.genericVision(false),
+			FilterArmorStandsSetting.genericVision(false));
+	
 	public TrueSightHack()
 	{
 		super("TrueSight");
 		setCategory(Category.RENDER);
+		entityFilters.forEach(this::addSetting);
 	}
 	
 	public boolean shouldBeVisible(Entity entity)
 	{
-		return isEnabled();
+		return isEnabled() && entityFilters.testOne(entity);
 	}
 	
 	// See EntityMixin.onIsInvisibleTo()
