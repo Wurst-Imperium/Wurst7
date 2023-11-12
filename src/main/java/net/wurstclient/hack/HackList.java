16e30f3756aa71e2fb0bd3ae1102b7e366a9e71f
mdenials <46415607+mdenials@users.noreply.github.com>
2023-11-12 20:12:12 +0500

    Update HackList.java

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -133,6 +133,7 @@ public final class HackList implements UpdateListener
 	public final NoClipHack noClipHack = new NoClipHack();
 	public final NoFallHack noFallHack = new NoFallHack();
 	public final NoFireOverlayHack noFireOverlayHack = new NoFireOverlayHack();
+	public final NoFrictionHack noFrictionHack = new NoFrictionHack();
 	public final NoHurtcamHack noHurtcamHack = new NoHurtcamHack();
 	public final NoLevitationHack noLevitationHack = new NoLevitationHack();
 	public final NoOverlayHack noOverlayHack = new NoOverlayHack();
