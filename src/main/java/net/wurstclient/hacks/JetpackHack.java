e2f855e077233aa71a89a048d4cadbcd5a501ac4
mdenials <46415607+mdenials@users.noreply.github.com>
2023-11-12 20:34:52 +0500

    Update JetpackHack.java

--- src/main/java/net/wurstclient/hacks/JetpackHack.java
+++ src/main/java/net/wurstclient/hacks/JetpackHack.java
@@ -11,15 +11,30 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"jet pack", "AirJump", "air jump"})
 public final class JetpackHack extends Hack implements UpdateListener
 {
+	public final EnumSetting<Mode> mode = new EnumSetting<>("Mode",
+		"\u00a7lJump\u00a7r mode is default jetpack setting \n\n",
+		"\u00a7lVelocity\u00a7r mode uses velocity mechanic and help consumes less food\n",
+		Mode.values(), Mode.JUMP);
+	
+	public final SliderSetting verticalSpeed = new SliderSetting(
+		"Vertical Speed",
+		"The default 0.42 is vanilla jump speed",
+		0.42, 0.05, 4, 0.01, ValueDisplay.DECIMAL);
+	
 	public JetpackHack()
 	{
 		super("Jetpack");
-		
 		setCategory(Category.MOVEMENT);
+		addSetting(mode);
+		addSetting(verticalSpeed);
 	}
 	
 	@Override
@@ -37,10 +52,39 @@ public final class JetpackHack extends Hack implements UpdateListener
 		EVENTS.remove(UpdateListener.class, this);
 	}
 	
-	@Override
+	@Overridew
 	public void onUpdate()
 	{
+		if (mode.getSelected() == Mode.VELOCITY) {
+			
+		ClientPlayerEntity player = MC.player;
+		Vec3d velocity = player.getVelocity();
+		if((player.isFallFlying() || velocity.y != 0) && MC.options.jumpKey.isPressed())
+		player.setVelocity(velocity.x, verticalSpeed.getValue(), velocity.z);
+			
+		}
+		else {
 		if(MC.options.jumpKey.isPressed())
 			MC.player.jump();
+		}
+	}
+	
+	public static enum Mode
+	{
+		JUMP("Jump"),
+		VELOCITY("Velocity");
+
+		private final String name;
+
+		private Mode(String name)
+		{
+			this.name = name;
+		}
+
+		@Override
+		public String toString()
+		{
+			return name;
+		}
 	}
 }
