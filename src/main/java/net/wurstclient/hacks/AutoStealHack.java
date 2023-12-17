80493694dbdb14861d631c57f95caebe42b4b6d2
mdenials <46415607+mdenials@users.noreply.github.com>
2023-12-17 13:21:50 +0500

    Update AutoStealHack.java

--- src/main/java/net/wurstclient/hacks/AutoStealHack.java
+++ src/main/java/net/wurstclient/hacks/AutoStealHack.java
@@ -13,30 +13,53 @@ import net.wurstclient.hack.Hack;
 import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
+import net.wurstclient.settings.ItemListSetting;
 
 @SearchTags({"auto steal", "ChestStealer", "chest stealer",
 	"steal store buttons", "Steal/Store buttons"})
 public final class AutoStealHack extends Hack
 {
-	private final SliderSetting delay = new SliderSetting("Delay",
+
+private final SliderSetting delay = new SliderSetting("Delay",
 		"Delay between moving stacks of items.\n"
 			+ "Should be at least 70ms for NoCheat+ servers.",
 		100, 0, 500, 10, ValueDisplay.INTEGER.withSuffix("ms"));
 	
+	private ItemListSetting items = new ItemListSetting("Items",
+		"Unwanted items that will be dropped.", "minecraft:allium",
+		"minecraft:azure_bluet", "minecraft:blue_orchid",
+		"minecraft:cornflower", "minecraft:dandelion", "minecraft:lilac",
+		"minecraft:lily_of_the_valley", "minecraft:orange_tulip",
+		"minecraft:oxeye_daisy", "minecraft:peony", "minecraft:pink_tulip",
+		"minecraft:poisonous_potato", "minecraft:poppy", "minecraft:red_tulip",
+		"minecraft:rose_bush", "minecraft:rotten_flesh", "minecraft:sunflower",
+		"minecraft:wheat_seeds", "minecraft:white_tulip");
+
 	private final CheckboxSetting buttons =
 		new CheckboxSetting("Steal/Store buttons", true);
+
+	private final CheckboxSetting filter =
+		new CheckboxSetting("Enable filter", true);
 	
 	public AutoStealHack()
 	{
 		super("AutoSteal");
 		setCategory(Category.ITEMS);
-		addSetting(buttons);
 		addSetting(delay);
+		addSetting(items);
+		addSetting(buttons);
+		addSetting(filter);
+		
 	}
 	
 	public boolean areButtonsVisible()
 	{
 		return buttons.isChecked();
+	}
+	
+	public boolean areFilterEnabled()
+	{
+		return filter.isChecked();
 	}
 	
 	public long getDelay()
