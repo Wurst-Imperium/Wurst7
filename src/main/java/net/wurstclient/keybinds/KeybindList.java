9ce0766636eb5aab2bcc97a6779d3cfa468d78ed
Alexander01998 <contact.wurstimperium@gmail.com>
2024-01-05 14:07:42 +0100

    Remove duplicate keybind

--- src/main/java/net/wurstclient/keybinds/KeybindList.java
+++ src/main/java/net/wurstclient/keybinds/KeybindList.java
@@ -117,7 +117,6 @@ public final class KeybindList
 	{
 		Set<Keybind> set = new LinkedHashSet<>();
 		addKB(set, "b", "fastplace;fastbreak");
-		addKB(set, "b", "fastplace;fastbreak");
 		addKB(set, "c", "fullbright");
 		addKB(set, "g", "flight");
 		addKB(set, "semicolon", "speednuker");
