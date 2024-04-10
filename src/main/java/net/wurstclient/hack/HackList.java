39f7daa945108d3961e08e86abba45961edb0e89
mdenials <46415607+mdenials@users.noreply.github.com>
2024-04-10 18:00:50 +0500

    Update HackList.java

--- src/main/java/net/wurstclient/hack/HackList.java
+++ src/main/java/net/wurstclient/hack/HackList.java
@@ -34,8 +34,7 @@ public final class HackList implements UpdateListener
 	public final AntiAfkHack antiAfkHack = new AntiAfkHack();
 	public final AntiBlindHack antiBlindHack = new AntiBlindHack();
 	public final AntiCactusHack antiCactusHack = new AntiCactusHack();
-	public final AntiEntityPushHack antiEntityPushHack =
-		new AntiEntityPushHack();
+	public final AntiEntityPushHack antiEntityPushHack = new AntiEntityPushHack();
 	public final AntiHungerHack antiHungerHack = new AntiHungerHack();
 	public final AntiKnockbackHack antiKnockbackHack = new AntiKnockbackHack();
 	public final AntiSpamHack antiSpamHack = new AntiSpamHack();
