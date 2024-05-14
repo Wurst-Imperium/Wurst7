92161a8ca9d1d1a506b06673519dec43560a0e68
mdenials <46415607+mdenials@users.noreply.github.com>
2024-05-14 20:42:15 +0500

    Tactical retreat for FightBotHack
    An improvement that allows the combat bot not to die from a horde of mobs

--- src/main/java/net/wurstclient/hacks/FightBotHack.java
+++ src/main/java/net/wurstclient/hacks/FightBotHack.java
@@ -53,6 +53,10 @@ public final class FightBotHack extends Hack
 			+ "This should be set to a lower value than Range.",
 		3, 1, 6, 0.05, ValueDisplay.DECIMAL);
 	
+  	private final SliderSetting safedistance = new SliderSetting("Safe Distance",
+		"At what distance to retreat from the target.",
+		3, 0, 6, 0.01, ValueDisplay.DECIMAL);
+	
 	private final CheckboxSetting useAi =
 		new CheckboxSetting("Use AI (experimental)", false);
 	
@@ -74,6 +78,7 @@ public final class FightBotHack extends Hack
 		addSetting(range);
 		addSetting(speed);
 		addSetting(distance);
+		addSetting(safedistance);
 		addSetting(useAi);
 		addSetting(pauseOnContainers);
 		
@@ -196,6 +201,8 @@ public final class FightBotHack extends Hack
 			// follow entity
 			MC.options.forwardKey.setPressed(
 				MC.player.distanceTo(entity) > distance.getValueF());
+			MC.options.backKey.setPressed(
+				MC.player.distanceTo(entity) < safedistance.getValueF());
 			WURST.getRotationFaker()
 				.faceVectorClient(entity.getBoundingBox().getCenter());
 		}
