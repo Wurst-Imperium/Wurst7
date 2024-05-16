3a03086e4c6799c7e4f3a0e4266a809edae679bb
Alexander01998 <contact.wurstimperium@gmail.com>
2024-05-16 21:00:02 +0200

    [Wurst-Bot] Update to 24w20a

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -59,7 +59,7 @@ public enum WurstClient
 	public static IMinecraftClient IMC;
 	
 	public static final String VERSION = "7.41.2";
-	public static final String MC_VERSION = "24w19b";
+	public static final String MC_VERSION = "24w20a";
 	
 	private WurstAnalytics analytics;
 	private EventManager eventManager;
