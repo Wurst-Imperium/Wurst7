404306a1c3798e597179cfdb0df8864a8a9d191b
Alexander01998 <contact.wurstimperium@gmail.com>
2024-08-07 15:46:18 +0200

    Merge v7.44pre1 into 1.20.6

--- src/main/java/net/wurstclient/settings/filterlists/FollowFilterList.java
+++ src/main/java/net/wurstclient/settings/filterlists/FollowFilterList.java
@@ -23,70 +23,72 @@ public final class FollowFilterList extends EntityFilterList
 	{
 		ArrayList<EntityFilter> builder = new ArrayList<>();
 		
-		builder.add(
-			new FilterPlayersSetting("Won't follow other players.", false));
+		builder.add(new FilterPlayersSetting(
+			"description.wurst.setting.follow.filter_players", false));
 		
-		builder.add(
-			new FilterSleepingSetting("Won't follow sleeping players.", false));
+		builder.add(new FilterSleepingSetting(
+			"description.wurst.setting.follow.filter_sleeping", false));
 		
 		builder.add(new FilterFlyingSetting(
-			"Won't follow players that are at least the given distance above ground.",
-			0));
+			"description.wurst.setting.follow.filter_flying", 0));
 		
 		builder.add(new FilterHostileSetting(
 			"Won't follow hostile mobs like zombies and creepers.", true));
 		
 		builder.add(FilterNeutralSetting.onOffOnly(
-			"Won't follow neutral mobs like endermen and wolves.", true));
+			"description.wurst.setting.follow.filter_neutral", true));
 		
-		builder.add(new FilterPassiveSetting("Won't follow animals like pigs"
-			+ " and cows, ambient mobs like bats, and water mobs like"
-			+ " fish, squid and dolphins.", true));
+		builder.add(new FilterPassiveSetting(
+			"Won't follow animals like pigs and cows, ambient mobs like bats, and water mobs like fish, squid and dolphins.",
+			true));
 		
-		builder.add(new FilterPassiveWaterSetting("Won't follow passive water"
-			+ " mobs like fish, squid, dolphins and axolotls.", true));
+		builder.add(new FilterPassiveWaterSetting(
+			"Won't follow passive water mobs like fish, squid, dolphins and axolotls.",
+			true));
 		
 		builder.add(new FilterBabiesSetting(
 			"Won't follow baby pigs, baby villagers, etc.", true));
 		
-		builder.add(new FilterBatsSetting("Won't follow bats and any other"
-			+ " \"ambient\" mobs that might be added by mods.", true));
+		builder.add(new FilterBatsSetting(
+			"description.wurst.setting.follow.filter_bats", true));
 		
 		builder.add(new FilterSlimesSetting("Won't follow slimes.", true));
 		
 		builder.add(new FilterPetsSetting(
-			"Won't follow tamed wolves, tamed horses, etc.", true));
+			"description.wurst.setting.follow.filter_pets", true));
 		
 		builder.add(new FilterVillagersSetting(
-			"Won't follow villagers and wandering traders.", true));
+			"description.wurst.setting.follow.filter_villagers", true));
 		
 		builder.add(new FilterZombieVillagersSetting(
-			"Won't follow zombified villagers.", true));
+			"description.wurst.setting.follow.filter_zombie_villagers", true));
 		
 		builder.add(new FilterGolemsSetting(
-			"Won't follow iron golems and snow golems.", true));
+			"description.wurst.setting.follow.filter_golems", true));
 		
 		builder
 			.add(FilterPiglinsSetting.onOffOnly("Won't follow piglins.", true));
 		
-		builder.add(FilterZombiePiglinsSetting
-			.onOffOnly("Won't follow zombified piglins.", true));
+		builder.add(FilterZombiePiglinsSetting.onOffOnly(
+			"description.wurst.setting.follow.filter_zombie_piglins", true));
 		
-		builder.add(
-			FilterEndermenSetting.onOffOnly("Won't follow endermen.", true));
+		builder.add(FilterEndermenSetting.onOffOnly(
+			"description.wurst.setting.follow.filter_endermen", true));
 		
-		builder.add(new FilterShulkersSetting("Won't follow shulkers.", true));
+		builder.add(new FilterShulkersSetting(
+			"description.wurst.setting.follow.filter_shulkers", true));
 		
-		builder.add(new FilterAllaysSetting("Won't follow allays.", true));
+		builder.add(new FilterAllaysSetting(
+			"description.wurst.setting.follow.filter_allays", true));
 		
 		builder.add(new FilterInvisibleSetting(
-			"Won't follow invisible entities.", false));
+			"description.wurst.setting.follow.filter_invisible", false));
 		
-		builder.add(
-			new FilterArmorStandsSetting("Won't follow armor stands.", true));
+		builder.add(new FilterArmorStandsSetting(
+			"description.wurst.setting.follow.filter_armor_stands", true));
 		
-		builder
-			.add(new FilterMinecartsSetting("Won't follow minecarts.", true));
+		builder.add(new FilterMinecartsSetting(
+			"description.wurst.setting.follow.filter_minecarts", true));
 		
 		return new FollowFilterList(builder);
 	}
