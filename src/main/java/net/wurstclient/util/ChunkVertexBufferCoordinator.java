07e35df17ac973137f5e4c5091fa8aa7653321ac
Alexander01998 <contact.wurstimperium@gmail.com>
2024-09-10 16:21:28 +0200

    Add abstract parent class for ChunkSearcherCoordinator and ChunkVertexBufferCoordinator

--- src/main/java/net/wurstclient/util/ChunkVertexBufferCoordinator.java
+++ src/main/java/net/wurstclient/util/ChunkVertexBufferCoordinator.java
@@ -7,47 +7,33 @@
  */
 package net.wurstclient.util;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.gl.VertexBuffer;
 import net.minecraft.client.render.BuiltBuffer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.dimension.DimensionType;
-import net.wurstclient.WurstClient;
-import net.wurstclient.events.PacketInputListener;
 import net.wurstclient.settings.ChunkAreaSetting;
 import net.wurstclient.util.ChunkSearcher.Result;
 
-public final class ChunkVertexBufferCoordinator implements PacketInputListener
+public final class ChunkVertexBufferCoordinator extends AbstractChunkCoordinator
 {
-	private final HashMap<ChunkPos, ChunkSearcher> searchers = new HashMap<>();
 	private final HashMap<ChunkPos, VertexBuffer> buffers = new HashMap<>();
-	private final ChunkAreaSetting area;
-	private BiPredicate<BlockPos, BlockState> query;
 	private final BiFunction<ChunkSearcher, Iterable<Result>, BuiltBuffer> renderer;
-	
-	private final Set<ChunkPos> chunksToUpdate =
-		Collections.synchronizedSet(new HashSet<>());
 	
 	public ChunkVertexBufferCoordinator(BiPredicate<BlockPos, BlockState> query,
 		BiFunction<ChunkSearcher, Iterable<Result>, BuiltBuffer> renderer,
 		ChunkAreaSetting area)
 	{
-		this.query = Objects.requireNonNull(query);
+		super(query, area);
 		this.renderer = Objects.requireNonNull(renderer);
-		this.area = Objects.requireNonNull(area);
 	}
 	
 	@Override
@@ -62,69 +48,19 @@ public final class ChunkVertexBufferCoordinator implements PacketInputListener
 				chunksToUpdate.add(new ChunkPos(x, z));
 	}
 	
-	public boolean update()
+	@Override
+	protected void onRemove(ChunkSearcher searcher)
 	{
-		DimensionType dimension = WurstClient.MC.world.getDimension();
-		HashSet<ChunkPos> chunkUpdates = clearChunksToUpdate();
-		boolean searchersChanged = false;
-		
-		// remove outdated ChunkSearchers
-		for(ChunkSearcher searcher : new ArrayList<>(searchers.values()))
-		{
-			boolean remove = false;
-			ChunkPos searcherPos = searcher.getPos();
-			
-			// wrong dimension
-			if(dimension != searcher.getDimension())
-				remove = true;
-			
-			// out of range
-			else if(!area.isInRange(searcherPos))
-				remove = true;
-			
-			// chunk update
-			else if(chunkUpdates.contains(searcherPos))
-				remove = true;
-			
-			if(remove)
-			{
-				searchers.remove(searcherPos);
-				searcher.cancel();
-				VertexBuffer buffer = buffers.remove(searcherPos);
-				if(buffer != null)
-					buffer.close();
-				searchersChanged = true;
-			}
-		}
-		
-		// add new ChunkSearchers
-		for(Chunk chunk : area.getChunksInRange())
-		{
-			ChunkPos chunkPos = chunk.getPos();
-			if(searchers.containsKey(chunkPos))
-				continue;
-			
-			ChunkSearcher searcher = new ChunkSearcher(query, chunk, dimension);
-			searchers.put(chunkPos, searcher);
-			searcher.start();
-			searchersChanged = true;
-		}
-		
-		return searchersChanged;
+		VertexBuffer buffer = buffers.remove(searcher.getPos());
+		if(buffer != null)
+			buffer.close();
 	}
 	
 	public void reset()
 	{
-		searchers.values().forEach(ChunkSearcher::cancel);
-		searchers.clear();
+		super.reset();
 		buffers.values().forEach(VertexBuffer::close);
 		buffers.clear();
-		chunksToUpdate.clear();
-	}
-	
-	public boolean isDone()
-	{
-		return searchers.values().stream().allMatch(ChunkSearcher::isDone);
 	}
 	
 	public Set<Entry<ChunkPos, VertexBuffer>> getBuffers()
@@ -150,27 +86,5 @@ public final class ChunkVertexBufferCoordinator implements PacketInputListener
 		vertexBuffer.upload(buffer);
 		VertexBuffer.unbind();
 		buffers.put(searcher.getPos(), vertexBuffer);
-	}
-	
-	public void setTargetBlock(Block block)
-	{
-		setQuery((pos, state) -> block == state.getBlock());
-	}
-	
-	public void setQuery(BiPredicate<BlockPos, BlockState> query)
-	{
-		this.query = Objects.requireNonNull(query);
-		searchers.values().forEach(ChunkSearcher::cancel);
-		searchers.clear();
-	}
-	
-	private HashSet<ChunkPos> clearChunksToUpdate()
-	{
-		synchronized(chunksToUpdate)
-		{
-			HashSet<ChunkPos> chunks = new HashSet<>(chunksToUpdate);
-			chunksToUpdate.clear();
-			return chunks;
-		}
 	}
 }
