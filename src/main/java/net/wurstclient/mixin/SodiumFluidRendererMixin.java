02e19a239fc8159ce8cfd3d13c3596ef011b2bb6
Alexander01998 <contact.wurstimperium@gmail.com>
2024-09-24 09:06:26 +0200

    Merge v7.45.1 into 1.21

--- src/main/java/net/wurstclient/mixin/SodiumFluidRendererMixin.java
+++ src/main/java/net/wurstclient/mixin/SodiumFluidRendererMixin.java
@@ -9,11 +9,13 @@ package net.wurstclient.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
@@ -22,26 +24,29 @@ import net.wurstclient.events.ShouldDrawSideListener.ShouldDrawSideEvent;
 
 @Pseudo
 @Mixin(targets = {
-	// current target
-	"me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.FluidRenderer",
-	// < Sodium 0.4.9
-	"me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer"},
+	"net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.DefaultFluidRenderer"},
 	remap = false)
 public class SodiumFluidRendererMixin
 {
+	@Unique
+	private ThreadLocal<BlockPos.Mutable> mutablePosForExposedCheck =
+		ThreadLocal.withInitial(BlockPos.Mutable::new);
+	
 	/**
 	 * This mixin hides and shows fluids when using X-Ray with Sodium installed.
+	 * Last updated for Sodium 0.6.0-beta.1+mc1.21.
 	 */
-	@Inject(at = @At("HEAD"), method = "isSideExposed", cancellable = true)
-	private void isSideExposed(BlockRenderView world, int x, int y, int z,
-		Direction dir, float height, CallbackInfoReturnable<Boolean> cir)
+	@Inject(at = @At("HEAD"), method = "isFluidOccluded", cancellable = true)
+	private void onIsFluidOccluded(BlockRenderView world, int x, int y, int z,
+		Direction dir, BlockState state, Fluid fluid,
+		CallbackInfoReturnable<Boolean> cir)
 	{
-		BlockPos pos = new BlockPos(x, y, z);
-		BlockState state = world.getBlockState(pos);
+		BlockPos.Mutable pos = mutablePosForExposedCheck.get();
+		pos.set(x, y, z);
 		ShouldDrawSideEvent event = new ShouldDrawSideEvent(state, pos);
 		EventManager.fire(event);
 		
 		if(event.isRendered() != null)
-			cir.setReturnValue(event.isRendered());
+			cir.setReturnValue(!event.isRendered());
 	}
 }
