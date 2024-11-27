8d0d39d1d5d777608c02cc55d438f606ef5cc69f
Alexander01998 <contact.wurstimperium@gmail.com>
2024-11-27 10:25:23 +0100

    Also assert that .copyitem won't remove the helmet from the head slot

--- src/main/java/net/wurstclient/test/CopyItemCmdTest.java
+++ src/main/java/net/wurstclient/test/CopyItemCmdTest.java
@@ -24,14 +24,15 @@ public enum CopyItemCmdTest
 		// Put on a golden helmet
 		runChatCommand("item replace entity @s armor.head with golden_helmet");
 		takeScreenshot("copyitem_command_setup");
-		assertOneItemInSlot(39, Items.GOLDEN_HELMET);
 		assertNoItemInSlot(0);
+		assertOneItemInSlot(39, Items.GOLDEN_HELMET);
 		
 		// .copyitem the helmet
 		String playerName = submitAndGet(mc -> mc.player.getName().getString());
 		runWurstCommand("copyitem " + playerName + " head");
 		takeScreenshot("copyitem_command_result");
 		assertOneItemInSlot(0, Items.GOLDEN_HELMET);
+		assertOneItemInSlot(39, Items.GOLDEN_HELMET);
 		
 		// Clean up
 		setPerspective(Perspective.FIRST_PERSON);
