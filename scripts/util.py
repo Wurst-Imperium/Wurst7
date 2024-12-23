853d3f3fbca2b51a30ba3e455ad17ff487744e13
Alexander01998 <contact.wurstimperium@gmail.com>
2024-12-23 17:52:07 +0100

    Print GitHub summary directly if running locally

--- scripts/util.py
+++ scripts/util.py
@@ -11,7 +11,7 @@ def read_json_file(path: Path) -> dict:
 def add_github_summary(summary: str):
 	"""Add a line to the GitHub Actions summary for the current step."""
 	if "GITHUB_STEP_SUMMARY" not in os.environ:
-		print(f"Not running on GHA, would have set summary: {summary}")
+		print(summary)
 		return
 	with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as summary_file:
 		print(summary, file=summary_file)
