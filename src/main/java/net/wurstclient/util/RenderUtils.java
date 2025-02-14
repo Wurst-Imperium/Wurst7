c0d7480fcf0ef675e85e2deac17c2da65b6b94c9
Alexander01998 <contact.wurstimperium@gmail.com>
2025-02-14 14:39:01 +0100

    Refactor TabGUI to use DrawContext and RenderLayers

--- src/main/java/net/wurstclient/util/RenderUtils.java
+++ src/main/java/net/wurstclient/util/RenderUtils.java
@@ -914,4 +914,62 @@ public enum RenderUtils
 				.color(color);
 		});
 	}
+	
+	/**
+	 * Draws a box shadow around the given rectangle.
+	 */
+	public static void drawBoxShadow2D(DrawContext context, int x1, int y1,
+		int x2, int y2)
+	{
+		float[] acColor = WurstClient.INSTANCE.getGui().getAcColor();
+		
+		// outline
+		float xo1 = x1 - 0.1F;
+		float xo2 = x2 + 0.1F;
+		float yo1 = y1 - 0.1F;
+		float yo2 = y2 + 0.1F;
+		
+		int outlineColor = toIntColor(acColor, 0.5F);
+		drawBorder2D(context, xo1, yo1, xo2, yo2, outlineColor);
+		
+		// shadow
+		float xs1 = x1 - 1;
+		float xs2 = x2 + 1;
+		float ys1 = y1 - 1;
+		float ys2 = y2 + 1;
+		
+		int shadowColor1 = toIntColor(acColor, 0.75F);
+		int shadowColor2 = 0x00000000;
+		
+		MatrixStack matrixStack = context.getMatrices();
+		Matrix4f matrix = matrixStack.peek().getPositionMatrix();
+		
+		context.draw(consumers -> {
+			VertexConsumer buffer = consumers.getBuffer(RenderLayer.getGui());
+			
+			// top
+			buffer.vertex(matrix, x1, y1, 0).color(shadowColor1);
+			buffer.vertex(matrix, x2, y1, 0).color(shadowColor1);
+			buffer.vertex(matrix, xs2, ys1, 0).color(shadowColor2);
+			buffer.vertex(matrix, xs1, ys1, 0).color(shadowColor2);
+			
+			// left
+			buffer.vertex(matrix, xs1, ys1, 0).color(shadowColor2);
+			buffer.vertex(matrix, xs1, ys2, 0).color(shadowColor2);
+			buffer.vertex(matrix, x1, y2, 0).color(shadowColor1);
+			buffer.vertex(matrix, x1, y1, 0).color(shadowColor1);
+			
+			// right
+			buffer.vertex(matrix, x2, y1, 0).color(shadowColor1);
+			buffer.vertex(matrix, x2, y2, 0).color(shadowColor1);
+			buffer.vertex(matrix, xs2, ys2, 0).color(shadowColor2);
+			buffer.vertex(matrix, xs2, ys1, 0).color(shadowColor2);
+			
+			// bottom
+			buffer.vertex(matrix, x2, y2, 0).color(shadowColor1);
+			buffer.vertex(matrix, x1, y2, 0).color(shadowColor1);
+			buffer.vertex(matrix, xs1, ys2, 0).color(shadowColor2);
+			buffer.vertex(matrix, xs2, ys2, 0).color(shadowColor2);
+		});
+	}
 }
