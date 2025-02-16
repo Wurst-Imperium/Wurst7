a30312482a5b15470cbe491c632cbc89a57b0c3c
Alexander01998 <contact.wurstimperium@gmail.com>
2025-02-16 12:53:03 +0100

    Update Yarn mappings

--- src/main/java/net/wurstclient/WurstPipelines.java
+++ src/main/java/net/wurstclient/WurstPipelines.java
@@ -7,10 +7,10 @@
  */
 package net.wurstclient;
 
-import net.minecraft.class_10784;
-import net.minecraft.class_10785;
-import net.minecraft.class_10798;
-import net.minecraft.class_10799;
+import net.minecraft.client.gl.BlendPrograms;
+import net.minecraft.client.gl.ShaderProgramLayer;
+import net.minecraft.client.gl.ShaderProgramLayers;
+import net.minecraft.client.render.DepthTestState;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
 
@@ -19,39 +19,39 @@ public enum WurstPipelines
 	;
 	
 	/**
-	 * Similar to the DEBUG_LINE_STIP pipeline (class_10799.field_56836), but as
-	 * a non-srip version with support for transparency.
+	 * Similar to the DEBUG_LINE_STIP pipeline, but as a non-srip version with
+	 * support for transparency.
 	 */
-	public static final class_10785 ONE_PIXEL_LINES = class_10799
-		.method_67887(class_10785.method_67729(class_10799.field_56849)
-			.method_67748("pipeline/wurst_1px_lines")
-			.method_67762("core/position_color")
-			.method_67757("core/position_color")
-			.method_67744(class_10784.field_56701).method_67753(false)
-			.method_67746(VertexFormats.POSITION_COLOR,
+	public static final ShaderProgramLayer ONE_PIXEL_LINES =
+		ShaderProgramLayers.addProgram(ShaderProgramLayer
+			.create(ShaderProgramLayers.MATRICIES_COLOR)
+			.setName("pipeline/wurst_1px_lines")
+			.setVertex("core/position_color").setShard("core/position_color")
+			.addBlend(BlendPrograms.TRANSLUCENT).addCulling(false)
+			.addFormat(VertexFormats.POSITION_COLOR,
 				VertexFormat.DrawMode.DEBUG_LINES)
-			.method_67760());
+			.create());
 	
 	/**
-	 * Similar to the DEBUG_LINE_STIP pipeline (class_10799.field_56836), but
-	 * with support for transparency.
+	 * Similar to the DEBUG_LINE_STIP pipeline, but with support for
+	 * transparency.
 	 */
-	public static final class_10785 ONE_PIXEL_LINE_STRIP = class_10799
-		.method_67887(class_10785.method_67729(class_10799.field_56849)
-			.method_67748("pipeline/wurst_1px_line_strip")
-			.method_67762("core/position_color")
-			.method_67757("core/position_color")
-			.method_67744(class_10784.field_56701).method_67753(false)
-			.method_67746(VertexFormats.POSITION_COLOR,
+	public static final ShaderProgramLayer ONE_PIXEL_LINE_STRIP =
+		ShaderProgramLayers.addProgram(ShaderProgramLayer
+			.create(ShaderProgramLayers.MATRICIES_COLOR)
+			.setName("pipeline/wurst_1px_line_strip")
+			.setVertex("core/position_color").setShard("core/position_color")
+			.addBlend(BlendPrograms.TRANSLUCENT).addCulling(false)
+			.addFormat(VertexFormats.POSITION_COLOR,
 				VertexFormat.DrawMode.DEBUG_LINE_STRIP)
-			.method_67760());
+			.create());
 	
 	/**
-	 * Similar to the LINES pipeline (class_10799.field_56833), but with no
-	 * depth test.
+	 * Similar to the LINES pipeline, but with no depth test.
 	 */
-	public static final class_10785 ESP_LINES = class_10799
-		.method_67887(class_10785.method_67729(class_10799.field_56859)
-			.method_67748("pipeline/wurst_esp_lines")
-			.method_67747(class_10798.NO_DEPTH_TEST).method_67760());
+	public static final ShaderProgramLayer ESP_LINES =
+		ShaderProgramLayers.addProgram(
+			ShaderProgramLayer.create(ShaderProgramLayers.RENDERTYPE_LINES)
+				.setName("pipeline/wurst_esp_lines")
+				.AddDepthProgram(DepthTestState.NO_DEPTH_TEST).create());
 }
