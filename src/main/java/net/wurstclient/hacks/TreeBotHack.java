c5e4436d7d9f6c5e66734ba963105e81f8d5a47b
Alexander01998 <contact.wurstimperium@gmail.com>
2025-02-28 14:06:10 +0100

    Remove TreeBot's VertexBuffer

--- src/main/java/net/wurstclient/hacks/TreeBotHack.java
+++ src/main/java/net/wurstclient/hacks/TreeBotHack.java
@@ -120,12 +120,7 @@ public final class TreeBotHack extends Hack
 		treeFinder = null;
 		angleFinder = null;
 		processor = null;
-		
-		if(tree != null)
-		{
-			tree.close();
-			tree = null;
-		}
+		tree = null;
 		
 		if(currentBlock != null)
 		{
@@ -153,11 +148,9 @@ public final class TreeBotHack extends Hack
 		}
 		
 		tree.getLogs().removeIf(Predicate.not(TreeBotUtils::isLog));
-		tree.compileBuffer();
 		
 		if(tree.getLogs().isEmpty())
 		{
-			tree.close();
 			tree = null;
 			return;
 		}
