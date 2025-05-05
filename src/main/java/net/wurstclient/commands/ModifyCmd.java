0df48c2db2fb3ff06eedfe6b93bfeea9b5144c3d
Alexander01998 <contact.wurstimperium@gmail.com>
2025-05-05 15:26:28 +0200

    Merge v7.48 into 1.21.1

--- src/main/java/net/wurstclient/commands/ModifyCmd.java
+++ src/main/java/net/wurstclient/commands/ModifyCmd.java
@@ -9,15 +9,19 @@ package net.wurstclient.commands;
 
 import java.util.Arrays;
 
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
 
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.NbtComponent;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.ComponentType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import net.wurstclient.command.CmdError;
 import net.wurstclient.command.CmdException;
 import net.wurstclient.command.CmdSyntaxError;
@@ -28,11 +32,10 @@ public final class ModifyCmd extends Command
 {
 	public ModifyCmd()
 	{
-		super("modify", "Allows you to modify NBT data of items.",
-			".modify add <nbt_data>", ".modify set <nbt_data>",
-			".modify remove <nbt_path>", "Use $ for colors, use $$ for $.", "",
-			"Example:",
-			".modify add {display:{Name:'{\"text\":\"$cRed Name\"}'}}",
+		super("modify", "Allows you to modify component data of items.",
+			".modify set <type> <value>", ".modify remove <type>",
+			"Use $ for colors, use $$ for $.", "", "Example:",
+			".modify set custom_name \"\\\"$cRed Name\\\"\"",
 			"(changes the item's name to \u00a7cRed Name\u00a7r)");
 	}
 	
@@ -40,24 +43,17 @@ public final class ModifyCmd extends Command
 	public void call(String[] args) throws CmdException
 	{
 		ClientPlayerEntity player = MC.player;
-		
 		if(!player.getAbilities().creativeMode)
 			throw new CmdError("Creative mode only.");
-		
 		if(args.length < 2)
 			throw new CmdSyntaxError();
 		
 		ItemStack stack = player.getInventory().getMainHandStack();
-		
 		if(stack == null)
 			throw new CmdError("You must hold an item in your main hand.");
 		
 		switch(args[0].toLowerCase())
 		{
-			case "add":
-			add(stack, args);
-			break;
-			
 			case "set":
 			set(stack, args);
 			break;
@@ -77,44 +73,25 @@ public final class ModifyCmd extends Command
 		ChatUtils.message("Item modified.");
 	}
 	
-	private void add(ItemStack stack, String[] args) throws CmdError
+	private void set(ItemStack stack, String[] args) throws CmdException
 	{
-		String nbtString =
-			String.join(" ", Arrays.copyOfRange(args, 1, args.length))
+		if(args.length < 3)
+			throw new CmdSyntaxError();
+		
+		ComponentType<?> type =
+			Registries.DATA_COMPONENT_TYPE.get(Identifier.tryParse(args[1]));
+		
+		String valueString =
+			String.join(" ", Arrays.copyOfRange(args, 2, args.length))
 				.replace("$", "\u00a7").replace("\u00a7\u00a7", "$");
+		JsonElement valueJson = parseJson(valueString);
+		DataResult<?> valueResult =
+			type.getCodec().parse(JsonOps.INSTANCE, valueJson);
+		Object value = valueResult.resultOrPartial().orElse(null);
 		
-		NbtCompound itemNbt = stack
-			.getOrDefault(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT)
-			.copyNbt();
-		
-		try
-		{
-			NbtCompound parsedNbt = StringNbtReader.parse(nbtString);
-			itemNbt.copyFrom(parsedNbt);
-			stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(itemNbt));
-			
-		}catch(CommandSyntaxException e)
-		{
-			ChatUtils.message(e.getMessage());
-			throw new CmdError("NBT data is invalid.");
-		}
-	}
-	
-	private void set(ItemStack stack, String[] args) throws CmdError
-	{
-		String nbt = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
-		nbt = nbt.replace("$", "\u00a7").replace("\u00a7\u00a7", "$");
-		
-		try
-		{
-			NbtCompound tag = StringNbtReader.parse(nbt);
-			stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(tag));
-			
-		}catch(CommandSyntaxException e)
-		{
-			ChatUtils.message(e.getMessage());
-			throw new CmdError("NBT data is invalid.");
-		}
+		ComponentMap.Builder builder = ComponentMap.builder();
+		builder.put(type, value);
+		stack.applyComponentsFrom(builder.build());
 	}
 	
 	private void remove(ItemStack stack, String[] args) throws CmdException
@@ -122,50 +99,27 @@ public final class ModifyCmd extends Command
 		if(args.length > 2)
 			throw new CmdSyntaxError();
 		
-		NbtPath path = parseNbtPath(stack
-			.getOrDefault(DataComponentTypes.CUSTOM_DATA, NbtComponent.DEFAULT)
-			.copyNbt(), args[1]);
+		ComponentType<?> type =
+			Registries.DATA_COMPONENT_TYPE.get(Identifier.tryParse(args[1]));
 		
-		if(path == null)
-			throw new CmdError("The path does not exist.");
+		if(type == null)
+			throw new CmdError(
+				"Component type \"" + args[1] + "\" does not exist.");
 		
-		path.base.remove(path.key);
-		stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(path.base));
+		stack.set(type, null);
 	}
 	
-	private NbtPath parseNbtPath(NbtCompound tag, String path)
+	private JsonElement parseJson(String jsonString) throws CmdError
 	{
-		String[] parts = path.split("\\.");
-		
-		NbtCompound base = tag;
-		if(base == null)
-			return null;
-		
-		for(int i = 0; i < parts.length - 1; i++)
+		try
 		{
-			String part = parts[i];
+			return JsonParser.parseString(jsonString);
 			
-			if(!base.contains(part) || !(base.get(part) instanceof NbtCompound))
-				return null;
-			
-			base = base.getCompound(part);
-		}
-		
-		if(!base.contains(parts[parts.length - 1]))
-			return null;
-		
-		return new NbtPath(base, parts[parts.length - 1]);
-	}
-	
-	private static class NbtPath
-	{
-		public NbtCompound base;
-		public String key;
-		
-		public NbtPath(NbtCompound base, String key)
+		}catch(JsonParseException e)
 		{
-			this.base = base;
-			this.key = key;
+			if(e.getCause() != null)
+				throw new CmdError(e.getCause().getMessage());
+			throw new CmdError(e.getMessage());
 		}
 	}
 }
