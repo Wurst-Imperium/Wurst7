618dca962198a18a66746d4c91b4276c1a138a57
Alexander01998 <contact.wurstimperium@gmail.com>
2025-06-10 14:12:57 +0200

    Revert "Temporarily remove FRAPI mixins"
    This reverts commit 4e3e8bf843d68e218141be724a623d2cf2140724.

---
+++ src/main/java/net/wurstclient/mixin/AbstractTerrainRenderContextMixin.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2014-2025 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.fabricmc.fabric.impl.client.indigo.renderer.mesh.MutableQuadViewImpl;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.AbstractTerrainRenderContext;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderInfo;
+import net.wurstclient.WurstClient;
+import net.wurstclient.hacks.XRayHack;
+
+@Mixin(value = AbstractTerrainRenderContext.class, remap = false)
+public abstract class AbstractTerrainRenderContextMixin
+{
+	@Shadow
+	@Final
+	private BlockRenderInfo blockInfo;
+	
+	/**
+	 * Applies X-Ray's opacity mask to the block color after all the normal
+	 * coloring and shading is done, if Indigo is running.
+	 */
+	@Inject(at = @At("RETURN"),
+		method = "shadeQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;ZZZ)V")
+	private void onShadeQuad(MutableQuadViewImpl quad, boolean ao,
+		boolean emissive, boolean vanillaShade, CallbackInfo ci)
+	{
+		XRayHack xray = WurstClient.INSTANCE.getHax().xRayHack;
+		if(!xray.isOpacityMode() || xray
+			.isVisible(blockInfo.blockState.getBlock(), blockInfo.blockPos))
+			return;
+		
+		for(int i = 0; i < 4; i++)
+			quad.color(i, quad.color(i) & xray.getOpacityColorMask());
+	}
+}
