fd343c8578078bad0c48307d301a002baac6b1b5
Théodore <theodore.reboul@gmail.com>
2025-06-18 12:59:05 +0200

    Update NewChunksHack.java

--- src/main/java/net/wurstclient/hacks/NewChunksHack.java
+++ src/main/java/net/wurstclient/hacks/NewChunksHack.java
@@ -159,7 +159,8 @@ public final class NewChunksHack extends Hack
 		{
 			renderer.updateBuffer(0, chunkRenderer.getLayer(),
 			// FIX: Copy the set to avoid ConcurrentModificationException
-            			buffer -> chunkRenderer.buildBuffer(buffer, new HashSet<>(newChunks), dd));
+            			buffer -> chunkRenderer.buildBuffer(buffer,
+            				new HashSet<>(newChunks), dd));
 			
 			if(showReasons.isChecked())
 				renderer.updateBuffer(1, reasonsRenderer.getLayer(),
@@ -171,7 +172,8 @@ public final class NewChunksHack extends Hack
 		{
 			renderer.updateBuffer(2, chunkRenderer.getLayer(),
 			// FIX: Copy the set to avoid ConcurrentModificationException
-            			buffer -> chunkRenderer.buildBuffer(buffer, new HashSet<>(oldChunks), dd));
+            			 buffer -> chunkRenderer.buildBuffer(buffer,
+            				new HashSet<>(oldChunks), dd));
 			
 			if(showReasons.isChecked())
 				renderer.updateBuffer(3, reasonsRenderer.getLayer(),
