809ee8f4b218a6e7b699002494aae78c2034a5b9
ZANX3Y <74610540+ZANX3Y@users.noreply.github.com>
2025-07-02 15:31:48 +0200

    Fix X-Ray - Sodium compat & opacity mode

--- src/main/java/net/wurstclient/mixin/SodiumAbstractBlockRenderContextMixin.java
+++ src/main/java/net/wurstclient/mixin/SodiumAbstractBlockRenderContextMixin.java
@@ -7,8 +7,10 @@
  */
 package net.wurstclient.mixin;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -16,28 +18,39 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockView;
 import net.wurstclient.event.EventManager;
 import net.wurstclient.events.ShouldDrawSideListener.ShouldDrawSideEvent;
 
+/**
+ * Last updated for <a href=
+ * "https://github.com/CaffeineMC/sodium/tree/02253db283e4679228ba5fbc30cfc851d17123c8">Sodium
+ * 0.6.13+mc1.21.6</a>
+ */
 @Pseudo
 @Mixin(targets = {
-	"net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache"},
-	remap = false)
-public class SodiumBlockOcclusionCacheMixin
+	"net.caffeinemc.mods.sodium.client.render.frapi.render.AbstractBlockRenderContext"})
+public class SodiumAbstractBlockRenderContextMixin
 {
+	@Shadow
+	protected BlockState state;
+	
+	@Shadow
+	protected BlockPos pos;
+	
 	/**
-	 * This mixin hides and shows regular full blocks when using X-Ray with
-	 * Sodium installed. Last updated for Sodium 0.6.0-beta.1+mc1.21.
+	 * Hides and shows blocks when using X-Ray with Sodium installed.
 	 */
-	@Inject(at = @At("HEAD"), method = "shouldDrawSide", cancellable = true)
-	public void shouldDrawSide(BlockState state, BlockView world, BlockPos pos,
-		Direction side, CallbackInfoReturnable<Boolean> cir)
+	@Inject(at = @At("HEAD"),
+		method = "isFaceCulled(Lnet/minecraft/util/math/Direction;)Z",
+		cancellable = true,
+		require = 0)
+	private void onIsFaceCulled(@Nullable Direction face,
+		CallbackInfoReturnable<Boolean> cir)
 	{
 		ShouldDrawSideEvent event = new ShouldDrawSideEvent(state, pos);
 		EventManager.fire(event);
 		
 		if(event.isRendered() != null)
-			cir.setReturnValue(event.isRendered());
+			cir.setReturnValue(!event.isRendered());
 	}
 }
