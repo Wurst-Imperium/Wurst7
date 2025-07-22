eb9e1e73ffed00cf138918534013ecf553dbf6f1
mdenials0000 <mdenials128@gmail.com>
2025-07-22 13:21:28 +0700

    More than health tag

--- src/main/java/net/wurstclient/hacks/HealthTagsHack.java
+++ src/main/java/net/wurstclient/hacks/HealthTagsHack.java
@@ -14,26 +14,44 @@ import net.minecraft.util.Formatting;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
 
 @SearchTags({"health tags"})
 public final class HealthTagsHack extends Hack
 {
+	public final CheckboxSetting entityHealth = new CheckboxSetting("Health", false);
+	public final CheckboxSetting entityArmor = new CheckboxSetting("Armor", false);
+	public final CheckboxSetting entityDistance = new CheckboxSetting("Distance", false);
+
 	public HealthTagsHack()
 	{
 		super("HealthTags");
 		setCategory(Category.RENDER);
+		addSetting(entityHealth);
+		addSetting(entityArmor);
+		addSetting(entityDistance);
 	}
 	
 	public Text addHealth(LivingEntity entity, MutableText nametag)
 	{
-		if(!isEnabled())
-			return nametag;
-		
 		int health = (int)entity.getHealth();
+		int armor = (int)entity.getArmor();
+		int distance = (int)MC.player.distanceTo(entity); 
 		
-		MutableText formattedHealth = Text.literal(" ")
-			.append(Integer.toString(health)).formatted(getColor(health));
-		return nametag.append(formattedHealth);
+		MutableText formattedHealth = Text.literal(" ").append(Integer.toString(health)).formatted(getColor(health));
+		MutableText formattedArmor = Text.literal(" ").append(Integer.toString(armor)).formatted(getColor(armor));
+		MutableText formattedDistance = Text.literal(" ").append(Integer.toString(distance)).formatted(getColor(distance));
+
+		if (entityHealth.isChecked())
+			nametag.append(formattedHealth);
+
+		if(entityArmor.isChecked())
+			nametag.append(formattedArmor);
+
+		if(entityDistance.isChecked())
+			nametag.append(formattedDistance);
+		
+		return nametag;
 	}
 	
 	private Formatting getColor(int health)
