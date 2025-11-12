42556b08b194d9027d77ba150b5ab4bd3726cd99
Alexander01998 <contact.wurstimperium@gmail.com>
2025-11-12 18:53:44 +0100

    Make Indigo mixins semi-optional

--- src/main/java/net/wurstclient/mixin/AbstractTerrainRenderContextMixin.java
+++ src/main/java/net/wurstclient/mixin/AbstractTerrainRenderContextMixin.java
@@ -9,18 +9,21 @@ package net.wurstclient.mixin;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.fabricmc.fabric.impl.client.indigo.renderer.mesh.MutableQuadViewImpl;
-import net.fabricmc.fabric.impl.client.indigo.renderer.render.AbstractTerrainRenderContext;
 import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderInfo;
 import net.wurstclient.WurstClient;
 import net.wurstclient.hacks.XRayHack;
 
-@Mixin(value = AbstractTerrainRenderContext.class, remap = false)
+@Pseudo
+@Mixin(
+	targets = "net.fabricmc.fabric.impl.client.indigo.renderer.render.AbstractTerrainRenderContext",
+	remap = false)
 public abstract class AbstractTerrainRenderContextMixin
 {
 	@Shadow
