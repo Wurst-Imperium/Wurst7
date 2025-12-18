14401b5e7e2d4a339f386ced19b0371f0dec1e27
Alexander01998 <contact.wurstimperium@gmail.com>
2025-12-18 10:52:05 +0100

    Change version to 7.51.4

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -50,7 +50,7 @@ public enum WurstClient
 	public static Minecraft MC;
 	public static IMinecraftClient IMC;
 	
-	public static final String VERSION = "7.51.3";
+	public static final String VERSION = "7.51.4";
 	public static final String MC_VERSION = "1.21.11";
 	
 	private PlausibleAnalytics plausible;
