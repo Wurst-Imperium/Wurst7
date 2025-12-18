4f0b15b2869c87ed5f4cfdcf7c12f7baf3202c41
Alexander01998 <contact.wurstimperium@gmail.com>
2025-12-18 10:51:44 +0100

    Update to latest Fabric API and Sodium

--- src/main/java/net/wurstclient/mixin/indigo/AbstractTerrainRenderContextMixin.java
+++ src/main/java/net/wurstclient/mixin/indigo/AbstractTerrainRenderContextMixin.java
@@ -7,15 +7,18 @@
  */
 package net.wurstclient.mixin.indigo;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-// import
-// net.fabricmc.fabric.impl.client.indigo.renderer.mesh.MutableQuadViewImpl;
-// import
-// net.fabricmc.fabric.impl.client.indigo.renderer.render.AbstractTerrainRenderContext;
-// import
-// net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderInfo;
+import net.fabricmc.fabric.impl.client.indigo.renderer.mesh.MutableQuadViewImpl;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderInfo;
+import net.wurstclient.WurstClient;
+import net.wurstclient.hacks.XRayHack;
 
 @Pseudo
 @Mixin(
@@ -23,27 +26,26 @@ import org.spongepowered.asm.mixin.Pseudo;
 	remap = false)
 public abstract class AbstractTerrainRenderContextMixin
 {
-	// @Shadow
-	// @Final
-	// private BlockRenderInfo blockInfo;
+	@Shadow
+	@Final
+	private BlockRenderInfo blockInfo;
 	
-	// /**
-	// * Applies X-Ray's opacity mask to the block color after all the normal
-	// * coloring and shading is done, if Indigo is running.
-	// */
-	// @Inject(at = @At("RETURN"),
-	// method =
-	// "shadeQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;ZZZ)V",
-	// require = 0)
-	// private void onShadeQuad(MutableQuadViewImpl quad, boolean ao,
-	// boolean emissive, boolean vanillaShade, CallbackInfo ci)
-	// {
-	// XRayHack xray = WurstClient.INSTANCE.getHax().xRayHack;
-	// if(!xray.isOpacityMode() || xray
-	// .isVisible(blockInfo.blockState.getBlock(), blockInfo.blockPos))
-	// return;
-	
-	// for(int i = 0; i < 4; i++)
-	// quad.color(i, quad.color(i) & xray.getOpacityColorMask());
-	// }
+	/**
+	 * Applies X-Ray's opacity mask to the block color after all the normal
+	 * coloring and shading is done, if Indigo is running.
+	 */
+	@Inject(at = @At("RETURN"),
+		method = "shadeQuad(Lnet/fabricmc/fabric/impl/client/indigo/renderer/mesh/MutableQuadViewImpl;ZZZ)V",
+		require = 0)
+	private void onShadeQuad(MutableQuadViewImpl quad, boolean ao,
+		boolean emissive, boolean vanillaShade, CallbackInfo ci)
+	{
+		XRayHack xray = WurstClient.INSTANCE.getHax().xRayHack;
+		if(!xray.isOpacityMode() || xray
+			.isVisible(blockInfo.blockState.getBlock(), blockInfo.blockPos))
+			return;
+		
+		for(int i = 0; i < 4; i++)
+			quad.color(i, quad.color(i) & xray.getOpacityColorMask());
+	}
 }
