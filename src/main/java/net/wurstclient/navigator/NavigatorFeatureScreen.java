08713453364d39a7be6224374a7f6e8a2eba8f14
Alexander01998 <contact.wurstimperium@gmail.com>
2026-03-15 15:11:08 +0100

    Fix popups staying open when their owner is out of view
    See https://wurstforum.net/d/1520

--- src/main/java/net/wurstclient/navigator/NavigatorFeatureScreen.java
+++ src/main/java/net/wurstclient/navigator/NavigatorFeatureScreen.java
@@ -249,8 +249,7 @@ public final class NavigatorFeatureScreen extends NavigatorScreen
 		int button = context.button();
 		
 		// popups
-		if(WurstClient.INSTANCE.getGui().handleNavigatorPopupClick(x, y,
-			button))
+		if(WurstClient.INSTANCE.getGui().handlePopupMouseClick(x, y, button))
 			return;
 		
 		// back button
@@ -485,6 +484,7 @@ public final class NavigatorFeatureScreen extends NavigatorScreen
 		}
 		
 		// popups & tooltip
+		gui.closePopupsOutsideArea(window, bgx1, bgy1, bgx2, bgy3);
 		gui.renderPopups(context, mouseX, mouseY);
 		gui.renderTooltip(context, mouseX, mouseY);
 	}
