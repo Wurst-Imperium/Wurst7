ad53911bdb019b3a66c868bce07fa4fb0cb5dd99
Alexander01998 <contact.wurstimperium@gmail.com>
2026-08-17 20:02:24 +0200

    Update to 26.3-snapshot-9

--- src/main/java/net/wurstclient/settings/filters/FilterEndermenSetting.java
+++ src/main/java/net/wurstclient/settings/filters/FilterEndermenSetting.java
@@ -8,7 +8,7 @@
 package net.wurstclient.settings.filters;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.monster.Enderman;
 
 public final class FilterEndermenSetting extends AttackDetectingEntityFilter
 {
@@ -26,13 +26,13 @@ public final class FilterEndermenSetting extends AttackDetectingEntityFilter
 	@Override
 	public boolean onTest(Entity e)
 	{
-		return !(e instanceof EnderMan);
+		return !(e instanceof Enderman);
 	}
 	
 	@Override
 	public boolean ifCalmTest(Entity e)
 	{
-		return !(e instanceof EnderMan ee) || ee.isAggressive();
+		return !(e instanceof Enderman ee) || ee.isAggressive();
 	}
 	
 	public static FilterEndermenSetting genericCombat(Mode selected)
